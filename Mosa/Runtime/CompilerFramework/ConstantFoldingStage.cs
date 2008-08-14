e40565d716221a28708826e023ede2c430d0800a
Simon Wollwage <rootnode@mosa-project.org>
2008-08-14 02:03:16 +0000

    - Extended ConstantFoldingStage to fold binary logic operations
    git-svn-id: svn://localhost/mosa@32 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Mosa/Runtime/CompilerFramework/ConstantFoldingStage.cs
+++ Mosa/Runtime/CompilerFramework/ConstantFoldingStage.cs
@@ -79,16 +79,38 @@ namespace Mosa.Runtime.CompilerFramework
                                 nothing_left = false;
                             }
                         }
+                        else if (instruction is IL.BinaryLogicInstruction)
+                        {
+                            Operand first = instruction.Operands[0];
+                            Operand second = instruction.Operands[1];
+
+                            // To fold, we need an arithmetic instruction operating
+                            // on 2 constants.
+                            if (first is ConstantOperand && second is ConstantOperand)
+                            {
+                                // Check for type of instruction
+                                if (instruction is IR.LogicalAndInstruction)
+                                    op = And(first as ConstantOperand, second as ConstantOperand, instruction.Results[0].StackType);
+                                else if (instruction is IR.LogicalOrInstruction)
+                                    op = Or(first as ConstantOperand, second as ConstantOperand, instruction.Results[0].StackType);
+                                else if (instruction is IR.LogicalXorInstruction)
+                                    op = Xor(first as ConstantOperand, second as ConstantOperand, instruction.Results[0].StackType);
+
+                                // We folded, so replace the current instruction
+                                remove = true;
+
+                                // We folded, so check the block again
+                                nothing_left = false;
+                            }
+                        }
+
 
                         // Shall we remove this instruction?
                         if (true == remove)
                         {
                             // Remove the arithmetic instruction and replace it
                             // by a store instruction
-                            Instruction new_instruction = new IL.StlocInstruction(Mosa.Runtime.CompilerFramework.IL.OpCode.Stloc);
-                            new_instruction.Results = new Operand[1];
-                            new_instruction.Results[0] = instruction.Results[0];
-                            new_instruction.Operands[0] = op;
+                            Instruction new_instruction = new IR.MoveInstruction(instruction.Results[0], op);
                             instructions[i] = new_instruction;
 
                             // Reset flag
@@ -103,6 +125,7 @@ namespace Mosa.Runtime.CompilerFramework
 
         #region Internals
 
+        #region Arithmetics
         /// <summary>
         /// Fold 2 constants by adding them
         /// </summary>
@@ -114,13 +137,13 @@ namespace Mosa.Runtime.CompilerFramework
             switch (type)
             {
                 case StackTypeCode.N:
-                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.Internal), (int)first.Value + (int)second.Value);
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.Internal), Convert.ToInt32(first.Value) + (int)second.Value);
                 case StackTypeCode.Int32:
-                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I4), (Int32)first.Value + (Int32)second.Value);
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I4), Convert.ToInt32(first.Value) + Convert.ToInt32(second.Value));
                 case StackTypeCode.Int64:
-                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I8), (Int64)first.Value + (Int64)second.Value);
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I8), Convert.ToInt64(first.Value) + Convert.ToInt32(second.Value));
                 case StackTypeCode.F:
-                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.R8), (double)first.Value + (double)second.Value);
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.R8), Convert.ToDouble(first.Value) + Convert.ToDouble(second.Value));
                 default:
                     throw new NotSupportedException();
             }
@@ -137,13 +160,13 @@ namespace Mosa.Runtime.CompilerFramework
             switch (type)
             {
                 case StackTypeCode.N:
-                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.Internal), (int)first.Value - (int)second.Value);
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.Internal), Convert.ToInt32(first.Value) - (int)second.Value);
                 case StackTypeCode.Int32:
-                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I4), (Int32)first.Value - (Int32)second.Value);
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I4), Convert.ToInt32(first.Value) - Convert.ToInt32(second.Value));
                 case StackTypeCode.Int64:
-                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I8), (Int64)first.Value - (Int64)second.Value);
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I8), Convert.ToInt64(first.Value) - Convert.ToInt32(second.Value));
                 case StackTypeCode.F:
-                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.R8), (double)first.Value - (double)second.Value);
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.R8), Convert.ToDouble(first.Value) - Convert.ToDouble(second.Value));
                 default:
                     throw new NotSupportedException();
             }
@@ -160,13 +183,13 @@ namespace Mosa.Runtime.CompilerFramework
             switch (type)
             {
                 case StackTypeCode.N:
-                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.Internal), (int)first.Value * (int)second.Value);
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.Internal), Convert.ToInt32(first.Value) * (int)second.Value);
                 case StackTypeCode.Int32:
-                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I4), (Int32)first.Value * (Int32)second.Value);
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I4), Convert.ToInt32(first.Value) * Convert.ToInt32(second.Value));
                 case StackTypeCode.Int64:
-                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I8), (Int64)first.Value * (Int64)second.Value);
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I8), Convert.ToInt64(first.Value) * Convert.ToInt32(second.Value));
                 case StackTypeCode.F:
-                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.R8), (double)first.Value * (double)second.Value);
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.R8), Convert.ToDouble(first.Value) * Convert.ToDouble(second.Value));
                 default:
                     throw new NotSupportedException();
             }
@@ -183,17 +206,83 @@ namespace Mosa.Runtime.CompilerFramework
             switch (type)
             {
                 case StackTypeCode.N:
-                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.Internal), (int)first.Value / (int)second.Value);
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.Internal), Convert.ToInt32(first.Value) / (int)second.Value);
                 case StackTypeCode.Int32:
-                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I4), (Int32)first.Value / (Int32)second.Value);
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I4), Convert.ToInt32(first.Value) / Convert.ToInt32(second.Value));
                 case StackTypeCode.Int64:
-                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I8), (Int64)first.Value / (Int64)second.Value);
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I8), Convert.ToInt64(first.Value) / Convert.ToInt32(second.Value));
                 case StackTypeCode.F:
-                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.R8), (double)first.Value / (double)second.Value);
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.R8), Convert.ToDouble(first.Value) / Convert.ToDouble(second.Value));
                 default:
                     throw new NotSupportedException();
             }
         }
+        #endregion // Arithmetics
+
+        #region Logical operations
+        /// <summary>
+        /// Fold 2 constants by multiplying them
+        /// </summary>
+        /// <param name="first">First constant to fold</param>
+        /// <param name="second">Second constant to fold</param>
+        /// <param name="type">Stacktype for the result</param>
+        private Operand And(ConstantOperand first, ConstantOperand second, StackTypeCode type)
+        {
+            switch (type)
+            {
+                case StackTypeCode.N:
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.Internal), Convert.ToInt32(first.Value) & (int)second.Value);
+                case StackTypeCode.Int32:
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I4), Convert.ToInt32(first.Value) & Convert.ToInt32(second.Value));
+                case StackTypeCode.Int64:
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I8), Convert.ToInt64(first.Value) & Convert.ToInt32(second.Value));
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        /// <summary>
+        /// Fold 2 constants by using binary or
+        /// </summary>
+        /// <param name="first">First constant to fold</param>
+        /// <param name="second">Second constant to fold</param>
+        /// <param name="type">Stacktype for the result</param>
+        private Operand Or(ConstantOperand first, ConstantOperand second, StackTypeCode type)
+        {
+            switch (type)
+            {
+                case StackTypeCode.N:
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.Internal), Convert.ToInt32(first.Value) | (int)second.Value);
+                case StackTypeCode.Int32:
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I4), Convert.ToInt32(first.Value) | Convert.ToInt32(second.Value));
+                case StackTypeCode.Int64:
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I8), Convert.ToInt64(first.Value) | Convert.ToInt32(second.Value));
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        /// <summary>
+        /// Fold 2 constants by using xor
+        /// </summary>
+        /// <param name="first">First constant to fold</param>
+        /// <param name="second">Second constant to fold</param>
+        /// <param name="type">Stacktype for the result</param>
+        private Operand Xor(ConstantOperand first, ConstantOperand second, StackTypeCode type)
+        {
+            switch (type)
+            {
+                case StackTypeCode.N:
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.Internal), Convert.ToInt32(first.Value) ^ (int)second.Value);
+                case StackTypeCode.Int32:
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I4), Convert.ToInt32(first.Value) ^ Convert.ToInt32(second.Value));
+                case StackTypeCode.Int64:
+                    return new ConstantOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.I8), Convert.ToInt64(first.Value) ^ Convert.ToInt32(second.Value));
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+        #endregion // Logical Operations
         #endregion // Internals
     }
 }
