75e1ce2a2ff4a89e0b6f46098f07d2c4d205c1c0
Simon Wollwage <rootnode@mosa-project.org>
2008-08-18 23:08:12 +0000

    - Created switch testcase. Not working yet
    git-svn-id: svn://localhost/mosa@38 45b04e34-05f1-4d87-a130-40a8491d5c8b

---
+++ Test/Mosa/Runtime/CompilerFramework/Tests/OpSwitch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.Mosa.Runtime.CompilerFramework.Tests
+{
+    /// <summary>
+    /// Holds test cases for the bitwise and operation.
+    /// </summary>
+    public static class OpSwitch
+    {
+        public static int Test_Switch()
+        {
+            int x = 0;
+            int j = 4;
+
+            switch (j)
+            {
+                case 1:
+                    x = 1;
+                    break;
+                case 2:
+                    x = 2;
+                    break;
+                case 3:
+                    x = 3;
+                    break;
+                case 4:
+                    x = 4;
+                    break;
+                case 5:
+                    x = 5;
+                    break;
+            }
+
+            return (x == 4 ? 1 : 0);
+        }
+    }
+}
