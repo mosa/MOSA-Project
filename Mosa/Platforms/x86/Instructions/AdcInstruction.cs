bcd8fdc07f9e7149490e6994fa8d18c54709701e
Simon Wollwage <rootnode@mosa-project.org>
2008-09-10 04:45:14 +0000

    - Forgot to add AdcInstruction.cs
    git-svn-id: svn://localhost/mosa@120 45b04e34-05f1-4d87-a130-40a8491d5c8b

---
+++ Mosa/Platforms/x86/Instructions/AdcInstruction.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * (c) 2008 MOSA - The Managed Operating System Alliance
+ *
+ * Licensed under the terms of the New BSD License.
+ *
+ * Authors:
+ *  Simon Wollwage (<mailto:rootnode@mosa-project.org>)
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Mosa.Runtime.CompilerFramework;
+using IL = Mosa.Runtime.CompilerFramework.IL;
+using System.Diagnostics;
+
+namespace Mosa.Platforms.x86
+{
+    sealed class AdcInstruction : Instruction
+    {
+        #region Construction
+        
+        public AdcInstruction(IL.OpCode code) :
+            base(2, 1)
+        {
+        }
+
+        public AdcInstruction(IL.OpCode code, Operand destination, Operand source) :
+            base(2, 1)
+        {
+        }
+
+        #endregion // Construction
+
+        /// <summary>
+        /// Allows visitor based dispatch for this instruction object.
+        /// </summary>
+        /// <param name="visitor">The visitor object.</param>
+        /// <param name="arg">A visitor specific context argument.</param>
+        /// <typeparam name="ArgType">An additional visitor context argument.</typeparam>
+        public override void Visit<ArgType>(IInstructionVisitor<ArgType> visitor, ArgType arg)
+        {
+            IX86InstructionVisitor<ArgType> x86 = visitor as IX86InstructionVisitor<ArgType>;
+            Debug.Assert(null != x86);
+            if (null != x86)
+                x86.Adc(this, arg);
+        }
+    }
+}
