c1b59fb86a17b8038f65901117b20c7dec300e76
Phil Garcia <phil@thinkedge.com>
2008-09-15 11:01:17 +0000

    - Fixed IOPort interfaces
    git-svn-id: svn://localhost/mosa@162 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Mosa/DeviceDrivers/IIOPorts.cs
+++ Mosa/DeviceDrivers/IIOPorts.cs
@@ -9,102 +9,67 @@
 
 namespace Mosa.DeviceDrivers
 {
-    /// <summary>
-    /// 
-    /// </summary>
-	public interface IReadWriteIOPort : IReadOnlyIOPort, IWriteOnlyIOPort
-	{
-        /// <summary>
-        /// Gets the address.
-        /// </summary>
-        /// <value>The address.</value>
-		new ushort Address { get; }
 
-        /// <summary>
-        /// Read8s this instance.
-        /// </summary>
-        /// <returns></returns>
-		new byte Read8();
-        /// <summary>
-        /// Write8s the specified data.
-        /// </summary>
-        /// <param name="data">The data.</param>
-        new void Write8(byte data);
-        /// <summary>
-        /// Read16s this instance.
-        /// </summary>
-        /// <returns></returns>
-        new ushort Read16();
-        /// <summary>
-        /// Write16s the specified data.
-        /// </summary>
-        /// <param name="data">The data.</param>
-        new void Write16(ushort data);
-        /// <summary>
-        /// Read32s this instance.
-        /// </summary>
-        /// <returns></returns>
-        new uint Read32();
-        /// <summary>
-        /// Write32s the specified data.
-        /// </summary>
-        /// <param name="data">The data.</param>
-        new void Write32(uint data);
+	/// <summary>
+	/// Interface to IOPort without any read/write permissions
+	/// </summary>
+	public interface IBaseIOPort
+	{
+		/// <summary>
+		/// Gets the address.
+		/// </summary>
+		/// <value>The address.</value>
+		ushort Address { get; }
 	}
 
-    /// <summary>
-    /// 
-    /// </summary>
-	public interface IReadOnlyIOPort
+	/// <summary>
+	/// Interface to IOPort with read only permission
+	/// </summary>
+	public interface IReadOnlyIOPort : IBaseIOPort
 	{
-        /// <summary>
-        /// Gets the address.
-        /// </summary>
-        /// <value>The address.</value>
-        ushort Address { get; }
-        /// <summary>
-        /// Read8s this instance.
-        /// </summary>
-        /// <returns></returns>
-        byte Read8();
-        /// <summary>
-        /// Read16s this instance.
-        /// </summary>
-        /// <returns></returns>
-        ushort Read16();
-        /// <summary>
-        /// Read32s this instance.
-        /// </summary>
-        /// <returns></returns>
-        uint Read32();
+		/// <summary>
+		/// Read8s this instance.
+		/// </summary>
+		/// <returns></returns>
+		byte Read8();
+		/// <summary>
+		/// Read16s this instance.
+		/// </summary>
+		/// <returns></returns>
+		ushort Read16();
+		/// <summary>
+		/// Read32s this instance.
+		/// </summary>
+		/// <returns></returns>
+		uint Read32();
 	}
 
-    /// <summary>
-    /// 
-    /// </summary>
-	public interface IWriteOnlyIOPort
+	/// <summary>
+	/// Interface to IOPort with write only permission
+	/// </summary>
+	public interface IWriteOnlyIOPort : IBaseIOPort
 	{
-        /// <summary>
-        /// Gets the address.
-        /// </summary>
-        /// <value>The address.</value>
-        ushort Address { get; }
-        /// <summary>
-        /// Write8s the specified data.
-        /// </summary>
-        /// <param name="data">The data.</param>
-        void Write8(byte data);
-        /// <summary>
-        /// Write16s the specified data.
-        /// </summary>
-        /// <param name="data">The data.</param>
-        void Write16(ushort data);
-        /// <summary>
-        /// Write32s the specified data.
-        /// </summary>
-        /// <param name="data">The data.</param>
-        void Write32(uint data);
+		/// <summary>
+		/// Write8s the specified data.
+		/// </summary>
+		/// <param name="data">The data.</param>
+		void Write8(byte data);
+		/// <summary>
+		/// Write16s the specified data.
+		/// </summary>
+		/// <param name="data">The data.</param>
+		void Write16(ushort data);
+		/// <summary>
+		/// Write32s the specified data.
+		/// </summary>
+		/// <param name="data">The data.</param>
+		void Write32(uint data);
 	}
 
-
+	/// <summary>
+	/// Interface to IOPort with full read/write permissions
+	/// </summary>
+	public interface IReadWriteIOPort : IBaseIOPort, IReadOnlyIOPort, IWriteOnlyIOPort
+	{
+	}
 }
