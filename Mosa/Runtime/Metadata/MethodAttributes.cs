dcd72914a1fd7371fc064c609eceaac452076296
Michael Fröhlich <sharpos@michaelruck.de>
2008-09-16 03:56:35 +0000

    - Fixed copyright header to use new MOSA style - Some parts changed with permission from alyman
    git-svn-id: svn://localhost/mosa@174 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Mosa/Runtime/Metadata/MethodAttributes.cs
+++ Mosa/Runtime/Metadata/MethodAttributes.cs
@@ -1,12 +1,12 @@
 /*
  * (c) 2008 MOSA - The Managed Operating System Alliance
  *
- * Licensed under the terms of the GNU GPL v3, with Classpath Linking Exception
- * Licensed under the terms of the New BSD License for exclusive use by the Ensemble OS Project
+ * Licensed under the terms of the New BSD License.
  *
  * Authors:
  *  Michael Ruck (<mailto:sharpos@michaelruck.de>)
  */
+
 
 using System;
 
