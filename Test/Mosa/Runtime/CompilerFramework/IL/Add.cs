09e5ed1b6e05829e6dc5ce690916f41e9e5c4168
Simon Wollwage <rootnode@mosa-project.org>
2008-10-08 05:27:38 +0000

    - Added unsigned testcases to Add
    git-svn-id: svn://localhost/mosa@245 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Test/Mosa/Runtime/CompilerFramework/IL/Add.cs
+++ Test/Mosa/Runtime/CompilerFramework/IL/Add.cs
@@ -91,6 +91,55 @@ namespace Test.Mosa.Runtime.CompilerFramework.IL
             CodeSource = "static class Test { static bool AddI1(int expect, sbyte a, sbyte b) { return expect == (a + b); } }";
             Assert.IsTrue((bool)Run<I4_I1_I1>("", "Test", "AddI1", a + b, a, b));
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expect"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        delegate bool U4_U1_U1(uint expect, byte a, byte b);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        [Row(1, 2)]
+        [Row(23, 21)]
+        [Row(0, 0)]
+        // And reverse
+        [Row(2, 1)]
+        [Row(21, 23)]
+        // (MinValue, X) Cases
+        [Row(byte.MinValue, 0)]
+        [Row(byte.MinValue, 1)]
+        [Row(byte.MinValue, 17)]
+        [Row(byte.MinValue, 123)]
+        // (MaxValue, X) Cases
+        [Row(byte.MaxValue, 0)]
+        [Row(byte.MaxValue, 1)]
+        [Row(byte.MaxValue, 17)]
+        [Row(byte.MaxValue, 123)]
+        // (X, MinValue) Cases
+        [Row(0, byte.MinValue)]
+        [Row(1, byte.MinValue)]
+        [Row(17, byte.MinValue)]
+        [Row(123, byte.MinValue)]
+        // (X, MaxValue) Cases
+        [Row(0, byte.MaxValue)]
+        [Row(1, byte.MaxValue)]
+        [Row(17, byte.MaxValue)]
+        [Row(123, byte.MaxValue)]
+        // Extremvaluecases
+        [Row(byte.MinValue, byte.MaxValue)]
+        [Test, Author("alyman", "mail.alex.lyman@gmail.com")]
+        public void AddU1(byte a, byte b)
+        {
+            CodeSource = "static class Test { static bool AddU1(uint expect, byte a, byte b) { return expect == (a + b); } }";
+            Assert.IsTrue((bool)Run<U4_U1_U1>("", "Test", "AddU1", a + b, a, b));
+        }
         
         delegate bool I4_I2_I2(int expect, short a, short b);
         /// <summary>
@@ -153,6 +202,55 @@ namespace Test.Mosa.Runtime.CompilerFramework.IL
         {
             CodeSource = "static class Test { static bool AddI2(int expect, short a, short b) { return expect == (a + b); } }";
             Assert.IsTrue((bool)Run<I4_I2_I2>("", "Test", "AddI2", (a + b), a, b));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expect"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        delegate bool U4_U2_U2(uint expect, short a, short b);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        [Row(1, 2)]
+        [Row(23, 21)]
+        [Row(0, 0)]
+        // And reverse
+        [Row(2, 1)]
+        [Row(21, 23)]
+        // (MinValue, X) Cases
+        [Row(Int16.MinValue, 0)]
+        [Row(Int16.MinValue, 1)]
+        [Row(Int16.MinValue, 17)]
+        [Row(Int16.MinValue, 123)]
+        // (MaxValue, X) Cases
+        [Row(Int16.MaxValue, 0)]
+        [Row(Int16.MaxValue, 1)]
+        [Row(Int16.MaxValue, 17)]
+        [Row(Int16.MaxValue, 123)]
+        // (X, MinValue) Cases
+        [Row(0, Int16.MinValue)]
+        [Row(1, Int16.MinValue)]
+        [Row(17, Int16.MinValue)]
+        [Row(123, Int16.MinValue)]
+        // (X, MaxValue) Cases
+        [Row(0, Int16.MaxValue)]
+        [Row(1, Int16.MaxValue)]
+        [Row(17, Int16.MaxValue)]
+        [Row(123, Int16.MaxValue)]
+        // Extremvaluecases
+        [Row(Int16.MinValue, Int16.MaxValue)]
+        [Test, Author("alyman", "mail.alex.lyman@gmail.com")]
+        public void AddU2(short a, short b)
+        {
+            CodeSource = "static class Test { static bool AddU2(uint expect, short a, short b) { return expect == (a + b); } }";
+            Assert.IsTrue((bool)Run<U4_U2_U2>("", "Test", "AddU2", a + b, a, b));
         }
 
         /// <summary>
@@ -233,6 +331,55 @@ namespace Test.Mosa.Runtime.CompilerFramework.IL
         /// <param name="a"></param>
         /// <param name="b"></param>
         /// <returns></returns>
+        delegate bool U4_U4_U4(uint expect, uint a, uint b);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        [Row(1, 2)]
+        [Row(23, 21)]
+        [Row(0, 0)]
+        // And reverse
+        [Row(2, 1)]
+        [Row(21, 23)]
+        // (MinValue, X) Cases
+        [Row(uint.MinValue, 0)]
+        [Row(uint.MinValue, 1)]
+        [Row(uint.MinValue, 17)]
+        [Row(uint.MinValue, 123)]
+        // (MaxValue, X) Cases
+        [Row(uint.MaxValue, 0)]
+        [Row(uint.MaxValue, 1)]
+        [Row(uint.MaxValue, 17)]
+        [Row(uint.MaxValue, 123)]
+        // (X, MinValue) Cases
+        [Row(0, uint.MinValue)]
+        [Row(1, uint.MinValue)]
+        [Row(17, uint.MinValue)]
+        [Row(123, uint.MinValue)]
+        // (X, MaxValue) Cases
+        [Row(0, uint.MaxValue)]
+        [Row(1, uint.MaxValue)]
+        [Row(17, uint.MaxValue)]
+        [Row(123, uint.MaxValue)]
+        // Extremvaluecases
+        [Row(uint.MinValue, uint.MaxValue)]
+        [Test, Author("alyman", "mail.alex.lyman@gmail.com")]
+        public void AddU4(uint a, uint b)
+        {
+            CodeSource = "static class Test { static bool AddU4(uint expect, uint a, uint b) { return expect == (a + b); } }";
+            Assert.IsTrue((bool)Run<U4_U4_U4>("", "Test", "AddU4", a + b, a, b));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expect"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         delegate bool I8_I8_I8(long expect, long a, long b);
         /// <summary>
         /// 
@@ -295,6 +442,55 @@ namespace Test.Mosa.Runtime.CompilerFramework.IL
         {
             CodeSource = "static class Test { static bool AddI8(long expect, long a, long b) { return expect == (a + b); } }";
             Assert.IsTrue((bool)Run<I8_I8_I8>("", "Test", "AddI8", (a + b), a, b));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expect"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        delegate bool U8_U8_U8(ulong expect, ulong a, ulong b);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        [Row(1, 2)]
+        [Row(23, 21)]
+        [Row(0, 0)]
+        // And reverse
+        [Row(2, 1)]
+        [Row(21, 23)]
+        // (MinValue, X) Cases
+        [Row(ulong.MinValue, 0)]
+        [Row(ulong.MinValue, 1)]
+        [Row(ulong.MinValue, 17)]
+        [Row(ulong.MinValue, 123)]
+        // (MaxValue, X) Cases
+        [Row(ulong.MaxValue, 0)]
+        [Row(ulong.MaxValue, 1)]
+        [Row(ulong.MaxValue, 17)]
+        [Row(ulong.MaxValue, 123)]
+        // (X, MinValue) Cases
+        [Row(0, ulong.MinValue)]
+        [Row(1, ulong.MinValue)]
+        [Row(17, ulong.MinValue)]
+        [Row(123, ulong.MinValue)]
+        // (X, MaxValue) Cases
+        [Row(0, ulong.MaxValue)]
+        [Row(1, ulong.MaxValue)]
+        [Row(17, ulong.MaxValue)]
+        [Row(123, ulong.MaxValue)]
+        // Extremvaluecases
+        [Row(ulong.MinValue, ulong.MaxValue)]
+        [Test, Author("alyman", "mail.alex.lyman@gmail.com")]
+        public void AddU8(ulong a, ulong b)
+        {
+            CodeSource = "static class Test { static bool AddU8(ulong expect, ulong a, ulong b) { return expect == (a + b); } }";
+            Assert.IsTrue((bool)Run<U8_U8_U8>("", "Test", "AddU8", a + b, a, b));
         }
         
         /// <summary>
