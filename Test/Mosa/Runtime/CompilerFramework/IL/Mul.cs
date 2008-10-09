f001b76d8cfaac77b085f7cc4a12d65b80f39c4e
Simon Wollwage <rootnode@mosa-project.org>
2008-10-09 20:15:21 +0000

    - Added unsigned testcases to Mul
    git-svn-id: svn://localhost/mosa@248 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Test/Mosa/Runtime/CompilerFramework/IL/Mul.cs
+++ Test/Mosa/Runtime/CompilerFramework/IL/Mul.cs
@@ -99,6 +99,55 @@ namespace Test.Mosa.Runtime.CompilerFramework.IL
         /// <param name="a"></param>
         /// <param name="b"></param>
         /// <returns></returns>
+        delegate bool U4_U1_U1(uint expect, byte a, byte b);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        [Row(1, 2)]
+        [Row(23, 21)]
+        [Row(0, 0)]
+        // And reverse
+        [Row(2, 1)]
+        [Row(21, 23)]
+        // (MinValue, X) Cases
+        [Row(byte.MinValue, 0)]
+        [Row(byte.MinValue, 1)]
+        [Row(byte.MinValue, 17)]
+        [Row(byte.MinValue, 123)]
+        // (MaxValue, X) Cases
+        [Row(byte.MaxValue, 0)]
+        [Row(byte.MaxValue, 1)]
+        [Row(byte.MaxValue, 17)]
+        [Row(byte.MaxValue, 123)]
+        // (X, MinValue) Cases
+        [Row(0, byte.MinValue)]
+        [Row(1, byte.MinValue)]
+        [Row(17, byte.MinValue)]
+        [Row(123, byte.MinValue)]
+        // (X, MaxValue) Cases
+        [Row(0, byte.MaxValue)]
+        [Row(1, byte.MaxValue)]
+        [Row(17, byte.MaxValue)]
+        [Row(123, byte.MaxValue)]
+        // Extremvaluecases
+        [Row(byte.MinValue, byte.MaxValue)]
+        [Row(byte.MaxValue, byte.MinValue)]
+        [Test, Author("alyman", "mail.alex.lyman@gmail.com")]
+        public void MulU1(byte a, byte b)
+        {
+            CodeSource = "static class Test { static bool MulU1(uint expect, byte a, byte b) { return expect == (a * b); } }";
+            Assert.IsTrue((bool)Run<U4_U1_U1>("", "Test", "MulU1", a * b, a, b));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expect"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         delegate bool I4_I2_I2(int expect, short a, short b);
         /// <summary>
         /// 
@@ -161,6 +210,55 @@ namespace Test.Mosa.Runtime.CompilerFramework.IL
         {
             CodeSource = "static class Test { static bool MulI2(int expect, short a, short b) { return expect == (a * b); } }";
             Assert.IsTrue((bool)Run<I4_I2_I2>("", "Test", "MulI2", (a * b), a, b));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expect"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        delegate bool U4_U2_U2(uint expect, ushort a, ushort b);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        [Row(1, 2)]
+        [Row(23, 21)]
+        [Row(0, 0)]
+        // And reverse
+        [Row(2, 1)]
+        [Row(21, 23)]
+        // (MinValue, X) Cases
+        [Row(ushort.MinValue, 0)]
+        [Row(ushort.MinValue, 1)]
+        [Row(ushort.MinValue, 17)]
+        [Row(ushort.MinValue, 123)]
+        // (MaxValue, X) Cases
+        [Row(ushort.MaxValue, 0)]
+        [Row(ushort.MaxValue, 1)]
+        [Row(ushort.MaxValue, 17)]
+        [Row(ushort.MaxValue, 123)]
+        // (X, MinValue) Cases
+        [Row(0, ushort.MinValue)]
+        [Row(1, ushort.MinValue)]
+        [Row(17, ushort.MinValue)]
+        [Row(123, ushort.MinValue)]
+        // (X, MaxValue) Cases
+        [Row(0, ushort.MaxValue)]
+        [Row(1, ushort.MaxValue)]
+        [Row(17, ushort.MaxValue)]
+        [Row(123, ushort.MaxValue)]
+        // Extremvaluecases
+        [Row(ushort.MinValue, ushort.MaxValue)]
+        [Row(ushort.MaxValue, ushort.MinValue)]
+        [Test, Author("alyman", "mail.alex.lyman@gmail.com")]
+        public void MulU2(ushort a, ushort b)
+        {
+            CodeSource = "static class Test { static bool MulU2(uint expect, ushort a, ushort b) { return expect == (a * b); } }";
+            Assert.IsTrue((bool)Run<U4_U2_U2>("", "Test", "MulU2", (a * b), a, b));
         }
 
         /// <summary>
@@ -233,6 +331,55 @@ namespace Test.Mosa.Runtime.CompilerFramework.IL
             CodeSource = "static class Test { static bool MulI4(int expect, int a, int b) { return expect == (a * b); } }";
             Assert.IsTrue((bool)Run<I4_I4_I4>("", "Test", "MulI4", (a * b), a, b));
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expect"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        delegate bool U4_U4_U4(uint expect, uint a, uint b);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        [Row(1, 2)]
+        [Row(23, 21)]
+        [Row(0, 0)]
+        // And reverse
+        [Row(2, 1)]
+        [Row(21, 23)]
+        // (MinValue, X) Cases
+        [Row(uint.MinValue, 0)]
+        [Row(uint.MinValue, 1)]
+        [Row(uint.MinValue, 17)]
+        [Row(uint.MinValue, 123)]
+        // (MaxValue, X) Cases
+        [Row(uint.MaxValue, 0)]
+        [Row(uint.MaxValue, 1)]
+        [Row(uint.MaxValue, 17)]
+        [Row(uint.MaxValue, 123)]
+        // (X, MinValue) Cases
+        [Row(0, uint.MinValue)]
+        [Row(1, uint.MinValue)]
+        [Row(17, uint.MinValue)]
+        [Row(123, uint.MinValue)]
+        // (X, MaxValue) Cases
+        [Row(0, uint.MaxValue)]
+        [Row(1, uint.MaxValue)]
+        [Row(17, uint.MaxValue)]
+        [Row(123, uint.MaxValue)]
+        // Extremvaluecases
+        [Row(uint.MinValue, uint.MaxValue)]
+        [Row(uint.MaxValue, uint.MinValue)]
+        [Test, Author("alyman", "mail.alex.lyman@gmail.com")]
+        public void MulU4(uint a, uint b)
+        {
+            CodeSource = "static class Test { static bool MulU4(uint expect, uint a, uint b) { return expect == (a * b); } }";
+            Assert.IsTrue((bool)Run<U4_U4_U4>("", "Test", "MulU4", (a * b), a, b));
+        }
          
         /// <summary>
         /// 
@@ -300,6 +447,52 @@ namespace Test.Mosa.Runtime.CompilerFramework.IL
         {
             CodeSource = "static class Test { static bool MulI8(long expect, long a, long b) { return expect == (a * b); } }";
             Assert.IsTrue((bool)Run<I8_I8_I8>("", "Test", "MulI8", (a * b), a, b));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expect"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        delegate bool U8_U8_U8(ulong expect, ulong a, ulong b);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        [Row(1, 2)]
+        [Row(23, 21)]
+        [Row(0, 0)]
+        // And reverse
+        [Row(2, 1)]
+        [Row(21, 23)]
+        // (MinValue, X) Cases
+        [Row(ulong.MinValue, 0)]
+        [Row(ulong.MinValue, 1)]
+        [Row(ulong.MinValue, 17)]
+        [Row(ulong.MinValue, 123)]
+        // (MaxValue, X) Cases
+        [Row(ulong.MaxValue, 0)]
+        [Row(ulong.MaxValue, 1)]
+        [Row(ulong.MaxValue, 17)]
+        [Row(ulong.MaxValue, 123)]
+        // (X, MinValue) Cases
+        [Row(0, ulong.MinValue)]
+        [Row(1, ulong.MinValue)]
+        [Row(17, ulong.MinValue)]
+        [Row(123, ulong.MinValue)]
+        // (X, MaxValue) Cases
+        [Row(0, ulong.MaxValue)]
+        [Row(1, ulong.MaxValue)]
+        [Row(17, ulong.MaxValue)]
+        [Row(123, ulong.MaxValue)]
+        [Test, Author("alyman", "mail.alex.lyman@gmail.com")]
+        public void MulU8(ulong a, ulong b)
+        {
+            CodeSource = "static class Test { static bool MulU8(ulong expect, ulong a, ulong b) { return expect == (a * b); } }";
+            Assert.IsTrue((bool)Run<U8_U8_U8>("", "Test", "MulU8", (a * b), a, b));
         }
         
         /// <summary>
