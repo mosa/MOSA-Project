88d520baf4e485837e2c7cfdaadc891a3f28d0b5
Michael Fröhlich <sharpos@michaelruck.de>
2008-10-14 19:35:31 +0000

    Fixed using declarations
    git-svn-id: svn://localhost/mosa@268 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Mosa/System/Runtime/CompilerServices/CompilerGeneratedAttribute.cs
+++ Mosa/System/Runtime/CompilerServices/CompilerGeneratedAttribute.cs
@@ -7,10 +7,6 @@
  *  Michael Ruck (<mailto:sharpos@michaelruck.de>)
  */
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace System.Runtime.CompilerServices
 {
     /// <summary>
