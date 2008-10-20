be37a150bdc09bc284bb944f19c8cf484a16f387
Simon Wollwage <rootnode@mosa-project.org>
2008-10-20 06:42:35 +0000

    - Updated Not testcase
    git-svn-id: svn://localhost/mosa@284 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Test/Mosa/Runtime/CompilerFramework/IL/Not.cs
+++ Test/Mosa/Runtime/CompilerFramework/IL/Not.cs
@@ -80,7 +80,7 @@ namespace Test.Mosa.Runtime.CompilerFramework.IL
         [Test, Author("rootnode", "rootnode@mosa-project.org")]
         public void NotU1(byte a)
         {
-            CodeSource = "static class Test { static bool NotU1(uint expect, byte a) { return expect == (~a); } }";
+            CodeSource = "static class Test { static bool NotU1(byte expect, byte a) { return expect == (~a); } }";
             Assert.IsTrue((bool)Run<U1_U1>("", "Test", "NotU1", (byte)~(byte)a, a));
         }
 
@@ -109,13 +109,40 @@ namespace Test.Mosa.Runtime.CompilerFramework.IL
         [Row(-10)]
         [Row(-11)]
         [Row(-100)]
-        [Row(sbyte.MinValue)]
-        [Row(sbyte.MaxValue)]
+        [Row(short.MinValue)]
+        [Row(short.MaxValue)]
         [Test, Author("rootnode", "rootnode@mosa-project.org")]
         public void NotI2(short a)
         {
             CodeSource = "static class Test { static bool NotI2(int expect, short a) { return expect == (~a); } }";
             Assert.IsTrue((bool)Run<I4_I2>("", "Test", "NotI2", (short)~a, (short)a));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expect"></param>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        delegate bool U2_U2(ushort expect, ushort a);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        [Row(0)]
+        [Row(1)]
+        [Row(2)]
+        [Row(5)]
+        [Row(10)]
+        [Row(11)]
+        [Row(100)]
+        [Row(ushort.MinValue)]
+        [Row(ushort.MaxValue)]
+        [Test, Author("rootnode", "rootnode@mosa-project.org")]
+        public void NotU2(ushort a)
+        {
+            CodeSource = "static class Test { static bool NotU2(ushort expect, ushort a) { return expect == (~a); } }";
+            Assert.IsTrue((bool)Run<U2_U2>("", "Test", "NotU2", ~(ushort)a, (ushort)a));
         }
 
         /// <summary>
@@ -143,13 +170,40 @@ namespace Test.Mosa.Runtime.CompilerFramework.IL
         [Row(-10)]
         [Row(-11)]
         [Row(-100)]
-        [Row(sbyte.MinValue)]
-        [Row(sbyte.MaxValue)]
+        [Row(int.MinValue)]
+        [Row(int.MaxValue)]
         [Test, Author("rootnode", "rootnode@mosa-project.org")]
         public void NotI4(int a)
         {
             CodeSource = "static class Test { static bool NotI4(int expect, int a) { return expect == (~a); } }";
             Assert.IsTrue((bool)Run<I4_I4>("", "Test", "NotI4", (int)~a, (int)a));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expect"></param>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        delegate bool U4_U4(uint expect, uint a);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        [Row(0)]
+        [Row(1)]
+        [Row(2)]
+        [Row(5)]
+        [Row(10)]
+        [Row(11)]
+        [Row(100)]
+        [Row(uint.MinValue)]
+        [Row(uint.MaxValue)]
+        [Test, Author("rootnode", "rootnode@mosa-project.org")]
+        public void NotU4(uint a)
+        {
+            CodeSource = "static class Test { static bool NotU4(uint expect, uint a) { return expect == (~a); } }";
+            Assert.IsTrue((bool)Run<U4_U4>("", "Test", "NotU4", ~(uint)a, a));
         }
 
         /// <summary>
@@ -184,6 +238,33 @@ namespace Test.Mosa.Runtime.CompilerFramework.IL
         {
             CodeSource = "static class Test { static bool NotI8(long expect, long a) { return expect == (~a); } }";
             Assert.IsTrue((bool)Run<I8_I8>("", "Test", "NotI8", (long)~a, (long)a));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expect"></param>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        delegate bool U8_U8(ulong expect, ulong a);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        [Row(0)]
+        [Row(1)]
+        [Row(2)]
+        [Row(5)]
+        [Row(10)]
+        [Row(11)]
+        [Row(100)]
+        [Row(ulong.MinValue)]
+        [Row(ulong.MaxValue)]
+        [Test, Author("rootnode", "rootnode@mosa-project.org")]
+        public void NotU8(ulong a)
+        {
+            CodeSource = "static class Test { static bool NotU8(ulong expect, ulong a) { return expect == (~a); } }";
+            Assert.IsTrue((bool)Run<U8_U8>("", "Test", "NotU8", ~(ulong)a, a));
         }
     }
 }
