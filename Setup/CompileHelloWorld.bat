88bb1e5e4ba199bbd5fbe2ec4b3a9c4bc9efdb8e
Phil Garcia <phil@thinkedge.com>
2008-12-24 16:54:06 +0000

    - Moved hello.exe to Setup\Kernel subdirectory - Updated configuration files - Added simple Setup\CompileHelloWorld.bat script to compile to binary HelloWorld from solution
    git-svn-id: svn://localhost/mosa@475 45b04e34-05f1-4d87-a130-40a8491d5c8b

---
+++ Setup/CompileHelloWorld.bat
@@ -0,0 +1,18 @@
+
+rem #### Compile the solution first ####
+
+if not exist output mkdir output
+
+copy ..\Mosa\Bin\Mosa.HelloWorld.exe output
+
+cd output
+
+..\bin\mosacl.exe -a=x86 -f=PE --pe-file-alignment=4096 --map=hello.map -b=mb0.7 -o hello.exe Mosa.HelloWorld.exe
+
+cd ..
+
+del /q /f Kernel\hello.exe
+
+copy output\hello.exe Kernel\hello.exe
+
+pause
