59a96c124b3a343d79b50da72bb8267488da969d
Phil Garcia <phil@thinkedge.com>
2008-12-28 23:59:40 +0000

    - Fixed CompileHelloWorld.bat script
    git-svn-id: svn://localhost/mosa@479 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Setup/CompileHelloWorld.bat
+++ Setup/CompileHelloWorld.bat
@@ -1,16 +1,14 @@
 
-rem #### Compile the solution first ####
+Echo "#### Compile the Solution First!!! ####"
 
 if not exist output mkdir output
 
-cd output
-
-..\..\Mosa\bin\mosacl.exe -a=x86 -f=PE --pe-file-alignment=4096 --map=hello.map -b=mb0.7 -o ..\..\Mosa\Bin\Mosa.HelloWorld.exe Mosa.HelloWorld.exe
-
-cd ..
-
 del /q /f Kernel\hello.exe
 
-copy output\hello.exe Kernel\hello.exe
+cd output
+
+..\..\Mosa\bin\mosacl.exe -a=x86 -f=PE --pe-file-alignment=4096 --map=hello.map -b=mb0.7 -o ..\Kernel\hello.exe ..\..\Mosa\Bin\Mosa.HelloWorld.exe
+
+cd ..
 
 pause
