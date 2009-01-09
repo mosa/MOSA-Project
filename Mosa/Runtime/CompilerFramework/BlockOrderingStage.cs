3bde953fbfc9e746c6406f468f049de5c8658812
Phil Garcia <phil@thinkedge.com>
2009-01-09 17:06:06 +0000

    - More work on Block Ordering Stage
    git-svn-id: svn://localhost/mosa@509 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Mosa/Runtime/CompilerFramework/BlockOrderingStage.cs
+++ Mosa/Runtime/CompilerFramework/BlockOrderingStage.cs
@@ -25,7 +25,7 @@ namespace Mosa.Runtime.CompilerFramework
 		/// 
 		/// </summary>
 		protected IArchitecture arch;
-	
+
 		#endregion // Data members
 
 		#region Properties
@@ -79,25 +79,25 @@ namespace Mosa.Runtime.CompilerFramework
 			IBasicBlockProvider blockProvider = (IBasicBlockProvider)compiler.GetPreviousStage(typeof(IBasicBlockProvider));
 			List<BasicBlock> blocks = blockProvider.Blocks;
 
-			// Flag per basic block
-			BitArray visited = new BitArray(blocks.Count);
-			BitArray active = new BitArray(blocks.Count);
-
 			// Start counter for unique loop index (starts at 1)
-			int uniqueLoopIndexCount = 1; 
+			int uniqueLoopIndexCount = 1;
 
 			// Retreive the first block
 			BasicBlock firstBlock = blockProvider.FromLabel(-1);
-			
+
 			// Initial loop index for the first block is set to -1
 			firstBlock.LoopIndex = -1;
 
-			// Create list for loop headers
-			List<BasicBlock> loopEndBlocks = new List<BasicBlock>();
+			// Create list for loop 
+			List<ConnectedBlocks> loops = new List<ConnectedBlocks>();
 
 			// Create queue for first iteration 
 			Queue<ConnectedBlocks> queue = new Queue<ConnectedBlocks>();
 			queue.Enqueue(new ConnectedBlocks(null, firstBlock));
+
+			// Flag per basic block
+			BitArray visited = new BitArray(blocks.Count, false);
+			BitArray active = new BitArray(blocks.Count, false);
 
 			while (queue.Count != 0) {
 				ConnectedBlocks at = queue.Dequeue();
@@ -108,7 +108,7 @@ namespace Mosa.Runtime.CompilerFramework
 					// and the loop-end is in at.from
 
 					// Add loop-end to list
-					loopEndBlocks.Add(at.from);
+					loops.Add(at);
 
 					// Assign unique loop index (if not already set)
 					if (at.to.LoopIndex <= 0)
@@ -118,14 +118,56 @@ namespace Mosa.Runtime.CompilerFramework
 					continue;
 				}
 				else {
+					// Mark as active
 					active.Set(at.to.Index, false);
 				}
 
+				// Mark as visited
 				visited.Set(at.to.Index, false);
 
 				// Add successors to queue
 				foreach (BasicBlock successor in at.to.NextBlocks)
 					queue.Enqueue(new ConnectedBlocks(at.to, successor));
+			}
+
+			// Create stack of blocks for next step of iterations
+			Stack<BasicBlock> stack = new Stack<BasicBlock>();
+
+			// Second set of iterations
+			foreach (ConnectedBlocks loop in loops) {
+				BasicBlock loopHeader = loop.to;
+				BasicBlock loopEnd = loop.from;
+
+				// Creat list of blocks in loop
+				List<BasicBlock> loopBlocks = new List<BasicBlock>();
+
+				// Add loop-end to stack
+				stack.Push(loopEnd);
+
+				// Clear visit flag
+				visited = new BitArray(blocks.Count, false);
+
+				while (stack.Count != 0) {
+					BasicBlock at = stack.Pop();
+
+					// already visited, continue loop
+					if (visited.Get(at.Index)) 						
+						continue;
+
+					// Mark as visisted
+					visited.Set(at.Index, false);
+
+					// Add loop to list of blocks in loop
+					loopBlocks.Add(at);
+
+					// Add predecessors to queue
+					foreach (BasicBlock predecessor in at.PreviousBlocks)
+						if (predecessor != loopHeader)
+							stack.Push(predecessor);
+				}
+
+				loopBlocks.Add(loopHeader);
+
 			}
 
 		}
