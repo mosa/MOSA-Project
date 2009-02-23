90b1ca946b6569de5bf03ad939a32eb40cabbb6d
Phil Garcia <phil@thinkedge.com>
2009-02-23 19:53:56 +0000

    git-svn-id: svn://localhost/mosa@567 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Mosa/Platforms/x86/X86.cs
+++ Mosa/Platforms/x86/X86.cs
@@ -8,21 +8,14 @@
  */
 
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 
 using Mosa.Runtime.CompilerFramework;
-using Mosa.Runtime.Linker;
 using Mosa.Runtime.Metadata;
-using Mosa.Runtime.Metadata.Signatures;
-using Mosa.Runtime.Vm;
-using IR = Mosa.Runtime.CompilerFramework.IR;
 
 namespace Mosa.Platforms.x86
 {
 	/// <summary>
-	/// An x86 machine code emitter.
+	/// Returns X86 opcodes.
 	/// </summary>
 	public static class X86
 	{
@@ -36,18 +29,18 @@ namespace Mosa.Platforms.x86
 			{
 				public static OpCode R_C = new OpCode(new byte[] { 0x81 }, 0);
 				public static OpCode R_R = new OpCode(new byte[] { 0x03 });
-				public static OpCode R_M_I8 = new OpCode(new byte[] { 0x02 }, null);
-				public static OpCode R_M = new OpCode(new byte[] { 0x03 }, null);
-				public static OpCode M_R_I8 = new OpCode(new byte[] { 0x00 }, null);
-				public static OpCode M_R = new OpCode(new byte[] { 0x01 }, null);
+				public static OpCode R_M_I8 = new OpCode(new byte[] { 0x02 });
+				public static OpCode R_M = new OpCode(new byte[] { 0x03 });
+				public static OpCode M_R_I8 = new OpCode(new byte[] { 0x00 });
+				public static OpCode M_R = new OpCode(new byte[] { 0x01 });
 			}
 			public static class And
 			{
 				public static OpCode R_C = new OpCode(new byte[] { 0x81 }, 4);
 				public static OpCode M_C = new OpCode(new byte[] { 0x81 }, 4);
-				public static OpCode R_M = new OpCode(new byte[] { 0x23 }, null);
-				public static OpCode R_R = new OpCode(new byte[] { 0x23 }, null);
-				public static OpCode M_R = new OpCode(new byte[] { 0x21 }, null);
+				public static OpCode R_M = new OpCode(new byte[] { 0x23 });
+				public static OpCode R_R = new OpCode(new byte[] { 0x23 });
+				public static OpCode M_R = new OpCode(new byte[] { 0x21 });
 			}
 			public static class Div
 			{
@@ -84,6 +77,35 @@ namespace Mosa.Platforms.x86
 				public static OpCode R_M = new OpCode(new byte[] { 0x0B });
 				public static OpCode R_R = new OpCode(new byte[] { 0x0B });
 				public static OpCode M_R = new OpCode(new byte[] { 0x09 });
+			}
+			public static class Shl
+			{
+				public static OpCode R_R = new OpCode(new byte[] { 0xD3 }, 4);
+				public static OpCode M_R = new OpCode(new byte[] { 0xD3 }, 4);
+				public static OpCode R_C = new OpCode(new byte[] { 0xC1 }, 4);
+				public static OpCode M_C = new OpCode(new byte[] { 0xC1 }, 4);			
+			}
+			public static class Shr
+			{
+				public static OpCode R_R = new OpCode(new byte[] { 0xD3 }, 5);
+				public static OpCode M_R = new OpCode(new byte[] { 0xD3 }, 5);
+				public static OpCode R_C = new OpCode(new byte[] { 0xC1 }, 5);
+				public static OpCode M_C = new OpCode(new byte[] { 0xC1 }, 5);
+			}
+			public static class Sub
+			{
+				public static OpCode O_C = new OpCode(new byte[] { 0x81 }, 5);
+				public static OpCode R_O = new OpCode(new byte[] { 0x2B });
+				public static OpCode M_R = new OpCode(new byte[] { 0x29 });
+				public static OpCode M_R_I8 = new OpCode(new byte[] { 0x28 });
+
+			}
+			public static class Xor
+			{
+				public static OpCode R_C = new OpCode(new byte[] { 0x81 }, 6);
+				public static OpCode R_M = new OpCode(new byte[] { 0x33 });
+				public static OpCode R_R = new OpCode(new byte[] { 0x33 });
+				public static OpCode M_R = new OpCode(new byte[] { 0x31 });
 			}
 		}
 #pragma warning restore 1591
@@ -270,7 +292,7 @@ namespace Mosa.Platforms.x86
 
 		#endregion
 
-		#region SHL
+		#region Shl
 		/// <summary>
 		/// SHLs the specified dest.
 		/// </summary>
@@ -279,66 +301,24 @@ namespace Mosa.Platforms.x86
 		/// <returns></returns>
 		public static OpCode Shl(Operand dest, Operand src)
 		{
-			if ((dest is RegisterOperand) && (src is RegisterOperand)) return Shl(dest as RegisterOperand, src as RegisterOperand);
-			if ((dest is MemoryOperand) && (src is RegisterOperand)) return Shl(dest as MemoryOperand, src as RegisterOperand);
-			if ((dest is RegisterOperand) && (src is ConstantOperand)) return Shl(dest as RegisterOperand, src as ConstantOperand);
-			if ((dest is MemoryOperand) && (src is ConstantOperand)) return Shl(dest as MemoryOperand, src as ConstantOperand);
+			if ((dest is RegisterOperand) && (src is RegisterOperand))
+				return X86Intruction.Shl.R_R;
+
+			if ((dest is MemoryOperand) && (src is RegisterOperand))
+				return X86Intruction.Shl.M_R;
+
+			if ((dest is RegisterOperand) && (src is ConstantOperand))
+				return X86Intruction.Shl.R_C;
+
+			if ((dest is MemoryOperand) && (src is ConstantOperand))
+				return X86Intruction.Shl.M_C;
 
 			throw new ArgumentException(@"No opcode for operand type.");
 		}
 
-		/// <summary>
-		/// SHLs the specified dest.
-		/// </summary>
-		/// <param name="dest">The dest.</param>
-		/// <param name="src">The SRC.</param>
-		/// <returns></returns>
-		public static OpCode Shl(RegisterOperand dest, RegisterOperand src)
-		{
-			return new OpCode(new byte[] { 0xD3 }, 4);
-		}
-
-		/// <summary>
-		/// SHLs the specified dest.
-		/// </summary>
-		/// <param name="dest">The dest.</param>
-		/// <param name="src">The SRC.</param>
-		/// <returns></returns>
-		public static OpCode Shl(MemoryOperand dest, RegisterOperand src)
-		{
-			//if ((dest.Type.Type == CilElementType.I1) || (dest.Type.Type == CilElementType.U1))
-			//    return new OpCode(new byte[] { 0xD2 }, 4);
-
-			return new OpCode(new byte[] { 0xD3 }, 4);
-		}
-
-		/// <summary>
-		/// SHLs the specified dest.
-		/// </summary>
-		/// <param name="dest">The dest.</param>
-		/// <param name="src">The SRC.</param>
-		/// <returns></returns>
-		public static OpCode Shl(RegisterOperand dest, ConstantOperand src)
-		{
-			return new OpCode(new byte[] { 0xC1 }, 4);
-		}
-
-		/// <summary>
-		/// SHLs the specified dest.
-		/// </summary>
-		/// <param name="dest">The dest.</param>
-		/// <param name="src">The SRC.</param>
-		/// <returns></returns>
-		public static OpCode Shl(MemoryOperand dest, ConstantOperand src)
-		{
-			//if ((dest.Type.Type == CilElementType.I1) || (dest.Type.Type == CilElementType.U1))
-			//    return new OpCode(new byte[] { 0xC0 }, 4);
-
-			return new OpCode(new byte[] { 0xC1 }, 4);
-		}
 		#endregion
 
-		#region SHR
+		#region Shr
 		/// <summary>
 		/// SHRs the specified dest.
 		/// </summary>
@@ -347,63 +327,21 @@ namespace Mosa.Platforms.x86
 		/// <returns></returns>
 		public static OpCode Shr(Operand dest, Operand src)
 		{
-			if ((dest is RegisterOperand) && (src is RegisterOperand)) return Shr(dest as RegisterOperand, src as RegisterOperand);
-			if ((dest is MemoryOperand) && (src is RegisterOperand)) return Shr(dest as MemoryOperand, src as RegisterOperand);
-			if ((dest is RegisterOperand) && (src is ConstantOperand)) return Shr(dest as RegisterOperand, src as ConstantOperand);
-			if ((dest is MemoryOperand) && (src is ConstantOperand)) return Shr(dest as MemoryOperand, src as ConstantOperand);
+			if ((dest is RegisterOperand) && (src is RegisterOperand))
+				return X86Intruction.Shr.R_R;
+
+			if ((dest is MemoryOperand) && (src is RegisterOperand))
+				return X86Intruction.Shr.M_R;
+
+			if ((dest is RegisterOperand) && (src is ConstantOperand))
+				return X86Intruction.Shr.R_C;
+
+			if ((dest is MemoryOperand) && (src is ConstantOperand))
+				return X86Intruction.Shr.M_C;
 
 			throw new ArgumentException(@"No opcode for operand type.");
 		}
 
-		/// <summary>
-		/// SHRs the specified dest.
-		/// </summary>
-		/// <param name="dest">The dest.</param>
-		/// <param name="src">The SRC.</param>
-		/// <returns></returns>
-		public static OpCode Shr(RegisterOperand dest, RegisterOperand src)
-		{
-			return new OpCode(new byte[] { 0xD3 }, 5);
-		}
-
-		/// <summary>
-		/// SHRs the specified dest.
-		/// </summary>
-		/// <param name="dest">The dest.</param>
-		/// <param name="src">The SRC.</param>
-		/// <returns></returns>
-		public static OpCode Shr(MemoryOperand dest, RegisterOperand src)
-		{
-			//if ((dest.Type.Type == CilElementType.I1) || (dest.Type.Type == CilElementType.U1))
-			//    return new OpCode(new byte[] { 0xD2 }, 4);
-
-			return new OpCode(new byte[] { 0xD3 }, 5);
-		}
-
-		/// <summary>
-		/// SHRs the specified dest.
-		/// </summary>
-		/// <param name="dest">The dest.</param>
-		/// <param name="src">The SRC.</param>
-		/// <returns></returns>
-		public static OpCode Shr(RegisterOperand dest, ConstantOperand src)
-		{
-			return new OpCode(new byte[] { 0xC1 }, 5);
-		}
-
-		/// <summary>
-		/// SHRs the specified dest.
-		/// </summary>
-		/// <param name="dest">The dest.</param>
-		/// <param name="src">The SRC.</param>
-		/// <returns></returns>
-		public static OpCode Shr(MemoryOperand dest, ConstantOperand src)
-		{
-			//if ((dest.Type.Type == CilElementType.I1) || (dest.Type.Type == CilElementType.U1))
-			//    return new OpCode(new byte[] { 0xC0 }, 4);
-
-			return new OpCode(new byte[] { 0xC1 }, 5);
-		}
 		#endregion
 
 		#region Sub
@@ -415,47 +353,19 @@ namespace Mosa.Platforms.x86
 		/// <returns></returns>
 		public static OpCode Sub(Operand dest, Operand src)
 		{
-			if ((dest is Operand) && (src is ConstantOperand)) return Sub(dest as Operand, src as ConstantOperand);
-			if ((dest is RegisterOperand) && (src is Operand)) return Sub(dest as RegisterOperand, src as Operand);
-			if ((dest is MemoryOperand) && (src is RegisterOperand)) return Sub(dest as MemoryOperand, src as RegisterOperand);
+			if ((dest is Operand) && (src is ConstantOperand))
+				return X86Intruction.Sub.O_C;
+
+			if ((dest is RegisterOperand) && (src is Operand))
+				return X86Intruction.Sub.R_O;
+
+			if ((dest is MemoryOperand) && (src is RegisterOperand))
+				if ((dest.Type.Type == CilElementType.I1) || (dest.Type.Type == CilElementType.U1))
+					return X86Intruction.Sub.M_R_I8;
+				else
+					return X86Intruction.Sub.M_R;
 
 			throw new ArgumentException(@"No opcode for operand type.");
-		}
-
-		/// <summary>
-		/// Subs the specified dest.
-		/// </summary>
-		/// <param name="dest">The dest.</param>
-		/// <param name="src">The SRC.</param>
-		/// <returns></returns>
-		public static OpCode Sub(Operand dest, ConstantOperand src)
-		{
-			return new OpCode(new byte[] { 0x81 }, 5);
-		}
-
-		/// <summary>
-		/// Subs the specified dest.
-		/// </summary>
-		/// <param name="dest">The dest.</param>
-		/// <param name="src">The SRC.</param>
-		/// <returns></returns>
-		public static OpCode Sub(RegisterOperand dest, Operand src)
-		{
-			return new OpCode(new byte[] { 0x2B }, null);
-		}
-
-		/// <summary>
-		/// Subs the specified dest.
-		/// </summary>
-		/// <param name="dest">The dest.</param>
-		/// <param name="src">The SRC.</param>
-		/// <returns></returns>
-		public static OpCode Sub(MemoryOperand dest, RegisterOperand src)
-		{
-			if ((dest.Type.Type == CilElementType.I1) || (dest.Type.Type == CilElementType.U1))
-				return new OpCode(new byte[] { 0x28 }, null);
-
-			return new OpCode(new byte[] { 0x29 }, null);
 		}
 		#endregion
 
@@ -468,57 +378,21 @@ namespace Mosa.Platforms.x86
 		/// <returns></returns>
 		public static OpCode Xor(Operand dest, Operand src)
 		{
-			if ((dest is RegisterOperand) && (src is ConstantOperand)) return Xor(dest as RegisterOperand, src as ConstantOperand);
-			if ((dest is RegisterOperand) && (src is MemoryOperand)) return Xor(dest as RegisterOperand, src as MemoryOperand);
-			if ((dest is RegisterOperand) && (src is RegisterOperand)) return Xor(dest as RegisterOperand, src as RegisterOperand);
-			if ((dest is MemoryOperand) && (src is RegisterOperand)) return Xor(dest as MemoryOperand, src as RegisterOperand);
+			if ((dest is RegisterOperand) && (src is ConstantOperand))
+				return X86Intruction.Xor.R_C;
+
+			if ((dest is RegisterOperand) && (src is MemoryOperand))
+				return X86Intruction.Xor.R_M;
+
+			if ((dest is RegisterOperand) && (src is RegisterOperand))
+				return X86Intruction.Xor.R_R;
+
+			if ((dest is MemoryOperand) && (src is RegisterOperand))
+				return X86Intruction.Xor.M_R;
 
 			throw new ArgumentException(@"No opcode for operand type.");
 		}
 
-		/// <summary>
-		/// Ors the specified dest.
-		/// </summary>
-		/// <param name="dest">The dest.</param>
-		/// <param name="src">The SRC.</param>
-		/// <returns></returns>
-		public static OpCode Xor(RegisterOperand dest, ConstantOperand src)
-		{
-			return new OpCode(new byte[] { 0x81 }, 6);
-		}
-
-		/// <summary>
-		/// Xors the specified dest.
-		/// </summary>
-		/// <param name="dest">The dest.</param>
-		/// <param name="src">The SRC.</param>
-		/// <returns></returns>
-		public static OpCode Xor(RegisterOperand dest, MemoryOperand src)
-		{
-			return new OpCode(new byte[] { 0x33 }, null);
-		}
-
-		/// <summary>
-		/// Xors the specified dest.
-		/// </summary>
-		/// <param name="dest">The dest.</param>
-		/// <param name="src">The SRC.</param>
-		/// <returns></returns>
-		public static OpCode Xor(RegisterOperand dest, RegisterOperand src)
-		{
-			return new OpCode(new byte[] { 0x33 }, null);
-		}
-
-		/// <summary>
-		/// Xors the specified dest.
-		/// </summary>
-		/// <param name="dest">The dest.</param>
-		/// <param name="src">The SRC.</param>
-		/// <returns></returns>
-		public static OpCode Xor(MemoryOperand dest, RegisterOperand src)
-		{
-			return new OpCode(new byte[] { 0x31 }, null);
-		}
 		#endregion
 	}
 }
