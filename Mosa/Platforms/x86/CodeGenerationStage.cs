bb9bb1e8469cfaf12962c25eddf17abf919ee98d
Simon Wollwage <rootnode@mosa-project.org>
2009-03-16 21:08:54 +0000

    - Cmp fix for mono
    git-svn-id: svn://localhost/mosa@613 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Mosa/Platforms/x86/CodeGenerationStage.cs
+++ Mosa/Platforms/x86/CodeGenerationStage.cs
@@ -184,7 +184,14 @@ namespace Mosa.Platforms.x86
 
         void IX86InstructionVisitor<int>.Cmp(CmpInstruction instruction, int arg)
         {
-            _emitter.Cmp(instruction.Operand0, instruction.Operand1);
+            Operand op0 = instruction.Operand0;
+
+            if (instruction.Operand0 is MemoryOperand && instruction.Operand1 is MemoryOperand)
+            {
+                op0 = new RegisterOperand(op0.Type, GeneralPurposeRegister.EDX);
+                _emitter.Mov(op0, instruction.Operand0);
+            }
+            _emitter.Cmp(op0, instruction.Operand1);
         }
 
         void IX86InstructionVisitor<int>.Setcc(SetccInstruction instruction, int arg)
