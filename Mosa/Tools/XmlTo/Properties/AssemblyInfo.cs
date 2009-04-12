2e341f5dbc71995ef0e104ec6bcc50a378ea4563
Phil Garcia <phil@thinkedge.com>
2009-04-12 07:42:38 +0000

    git-svn-id: svn://localhost/mosa@657 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Mosa/Tools/XmlTo/Properties/AssemblyInfo.cs
+++ Mosa/Tools/XmlTo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XmlTo")]
+[assembly: AssemblyTitle("Mosa.Tools.XmlTo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XmlTo")]
+[assembly: AssemblyProduct("Mosa.Tools.XmlTo")]
 [assembly: AssemblyCopyright("Copyright ©  2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
