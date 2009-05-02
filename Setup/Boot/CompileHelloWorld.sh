8591291e2bf1f3fd26e5772f6e0df5bc0490f88d
Simon Wollwage <rootnode@mosa-project.org>
2009-05-02 17:29:22 +0000

    Mono Scripts
    git-svn-id: svn://localhost/mosa@697 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Setup/Boot/CompileHelloWorld.sh
+++ Setup/Boot/CompileHelloWorld.sh
@@ -1,13 +1,3 @@
-
-echo "#### Compile the Solution First!!! ####"
-
 mkdir build
-
-rm build/hello.exe
-
-cd build
-
-../../../Mosa/Bin/mosacl.exe -a=x86 -f=PE --pe-file-alignment=4096 --map=hello.map -b=mb0.7 -o ../build/hello.exe ../../../Mosa/Bin/Mosa.HelloWorld.exe
-
-cd ..
-
+rm ./build/hello.exe
+mono ../../Mosa/Bin/mosacl.exe -a=x86 -f=PE -pe-file-alignment=4096 -b=mb0.7 -o ./build/hello.exe ../../Mosa/Bin/Mosa.HelloWorld.exe
