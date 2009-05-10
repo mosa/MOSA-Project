b253e6338f4dde51d0ffe28b0791754dbde13f60
Phil Garcia <phil@thinkedge.com>
2009-05-10 21:36:06 +0000

    Removed corlib stub. Added VES project.
    git-svn-id: svn://localhost/mosa@714 45b04e34-05f1-4d87-a130-40a8491d5c8b

---
+++ Mosa/VES/Runtime.cs
@@ -0,0 +1,48 @@
+﻿/*
+* (c) 2008 MOSA - The Managed Operating System Alliance
+*
+* Licensed under the terms of the New BSD License.
+*
+* Authors:
+*  Phil Garcia (tgiphil) <phil@thinkedge.com>
+*/
+
+namespace Mosa.VES
+{
+	using System;
+
+	/// <summary>
+	/// Color
+	/// </summary>
+	public static class Runtime
+	{
+		/// <summary>
+		/// Allocates memory.
+		/// </summary>
+		/// <param name="size">The size.</param>
+		/// <returns></returns>
+		internal static uint AllocateMemory(uint size)
+		{
+			// TODO: Allocated Memory
+			// MemoryManager.Allocate(size);
+			return 0;
+		}
+
+		internal static Object AllocateObject (/* ??? */)
+		{
+			// TODO
+			// uint location = AllocateMemory(size);
+			return null;
+		}
+
+		/// <summary>
+		/// Frees the object.
+		/// </summary>
+		/// <param name="o">The object.</param>
+		public static void FreeObject (object o)
+		{
+			// TODO: De-allocated Memory
+			// MemoryManager.Free (GetObjectPointer (o));
+		}
+	}
+}
