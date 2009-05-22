5f1d05d28050714f36bc6c424b05ef2cc359324f
Phil Garcia <phil@thinkedge.com>
2009-05-22 14:56:45 +0000

    - Refactored MakeIsoImage
    git-svn-id: svn://localhost/mosa@721 45b04e34-05f1-4d87-a130-40a8491d5c8b

---
+++ Mosa/Tools/MakeIsoImage/Generator.cs
@@ -0,0 +1,169 @@
+﻿/*
+ * Copyright (c) 2009 MOSA - The Managed Operating System Alliance
+ *
+ * Licensed under the terms of the New BSD License.
+ *
+ * Author(s):
+ *  Royce Mitchell III (royce3) <royce3 [at] gmail [dot] com>
+ */
+
+#define ROCKRIDGE
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Mosa.Tools.MakeIsoImage
+{
+
+	/// <summary>
+	/// This class is responsible for encoding types of data to the filestream and also keeping track
+	/// of our current file position.
+	/// </summary>
+	internal class Generator
+	{
+		private bool pedantic;
+		public int Index;
+		private FileStream fs;
+		public System.Text.ASCIIEncoding Ascii;
+
+		public Generator(bool pedantic)
+		{
+			this.pedantic = pedantic;
+			this.Index = 0;
+			this.fs = null;
+			this.Ascii = new System.Text.ASCIIEncoding();
+		}
+		public void ResetWithFileStream(FileStream fs)
+		{
+			this.Index = 0;
+			this.fs = fs;
+		}
+
+		/// <summary>
+		/// This function converts a normal Name into an iso9660-compatible file Name.
+		/// TODO FIXME: check the pedantic setting...
+		/// </summary>
+		/// <param Name="name">the normal file Name</param>
+		/// <param Name="folder">if this is a Name for a folder</param>
+		/// <returns>the iso9660-compatible version of the file Name</returns>
+		public byte[] IsoName(string name, bool folder)
+		{
+			if (folder) {
+				if (name == ".")
+					return new byte[] { 0 };
+				if (name == "..")
+					return new byte[] { 1 };
+#if ROCKRIDGE
+				if (this.pedantic)
+					name = name.Replace('.', '_');
+#endif
+			}
+#if ROCKRIDGE
+			name = name.ToUpper();
+#endif
+			string ext = "";
+			int dot = name.LastIndexOf('.');
+			if (dot >= 0) {
+				ext = name.Substring(dot);
+				name = name.Substring(0, dot);
+			}
+#if ROCKRIDGE
+			name = name.Substring(0, System.Math.Min(name.Length, 8)).Replace('.', '_').Replace(' ', '_');
+			ext = ext.Substring(0, System.Math.Min(ext.Length, 4));
+#else
+                // make sure Name isn't too long, 1st pass ( trying to preserve extension )
+                int total = name.Length + ext.Length;
+                const int max_length = (256 - 34);
+                if (total > max_length)
+                {
+                    int new_length = name.Length - (total - max_length);
+                    if ( new_length >= 0 )
+                        name = name.Substring ( 0, new_length );
+                }
+#endif
+			name += ext;
+
+#if !ROCKRIDGE
+                // make sure Name isn't too long, 2nd pass ( if 1st pass failed, then extension is too long - sacrifice it now )
+                if (name.Length > max_length)
+                    name = name.Substring(0, max_length);
+#endif
+
+			return this.Ascii.GetBytes(name);
+		}
+
+		public void FinishBlock()
+		{
+			int extra = (-this.Index) % 2048;
+			if (extra < 0)
+				extra += 2048;
+			DupByte(0, extra);
+		}
+
+		public void DupByte(byte b, int count)
+		{
+			this.Index += count;
+			if (this.fs == null) return;
+			while (count-- > 0)
+				this.fs.WriteByte(b);
+		}
+
+		public void Bytes(byte[] b)
+		{
+			Bytes(b, 0, b.Length);
+		}
+
+		public void Bytes(byte[] b, int offset, int bytes)
+		{
+			this.Index += bytes;
+			if (this.fs == null) return;
+			this.fs.Write(b, offset, bytes);
+		}
+
+		public void String(string s)
+		{
+			Bytes(this.Ascii.GetBytes(s));
+		}
+
+		/// <summary>
+		/// writes a file out to cd. Generate the Boot Info Table if necessary
+		/// </summary>
+		/// <param Name="f">the file to write</param>
+		/// <param Name="primaryVolumeDescriptor">the PVD block, passed in case we need to generate the Boot Info Table</param>
+		public void WriteFile(IsoFile f, int primaryVolumeDescriptor)
+		{
+			if (f.fileInfo.Length > 0xffffffff)
+				throw new NotImplementedException(">4G files not implemented");
+
+			int bytes = (int)f.fileInfo.Length;
+			if (this.fs == null) {
+				this.Index += bytes;
+				return;
+			}
+
+			// TODO FIXME - create smaller reusable buffer and read fixed-size chunks at a time...
+			byte[] b = new byte[bytes];
+			if (bytes != f.fileInfo.OpenRead().Read(b, 0, bytes)) {
+				throw new Exception("number of bytes read from file != reported length of file: " + f.fileInfo.Name);
+			}
+
+			if (f.BootInfoTable) {
+				// TODO FIXME - this is TERRIBLE. This should be implemented at a higher level, and
+				// doing it here requires passing the primaryVolumeDescriptor to every call of WriteFile()
+				// The reason it is here is because this is the only place where the file actually gets
+				// pulled into memory and I didn't want to modify the boot image on-disk like mkisofs does.
+				Bytes(b, 0, 8);
+				Bytes(ConvertTo.Int2LSB(primaryVolumeDescriptor));
+				Bytes(ConvertTo.Int2LSB(f.DataBlock));
+				Bytes(ConvertTo.Int2LSB((int)f.fileInfo.Length));
+				Bytes(ConvertTo.Int2LSB(0)); // TODO FIXME - checksum
+				DupByte(0, 40); // reserved
+				Bytes(b, 64, bytes - 64);
+			}
+			else
+				Bytes(b);
+		}
+	}
+}
--- Mosa/Tools/MakeIsoImage/Program.cs
+++ Mosa/Tools/MakeIsoImage/Program.cs
@@ -11,17 +11,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace Mosa.Tools.MakeIsoImage
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //var f = new System.IO.FileInfo("C:\\cvs\\r3utils\\ParseIso\\SharpOS.iso");
-            //f.Attributes
+	internal class Program
+	{
+		/// <summary>
+		/// Mains the specified args.
+		/// </summary>
+		/// <param name="args">The args.</param>
+		/// <returns></returns>
+		private static int Main(string[] args)
+		{
+			Console.WriteLine("MakeIsoImage v0.9 [www.mosa-project.org]");
+			Console.WriteLine("Copyright 2009. New BSD License.");
+			Console.WriteLine("Written by Royce Mitchell III (royce3@gmail.com)");
+			Console.WriteLine();
 
-            // TODO FIXME - support remappings something like -map boot/boot.bin=c:/muos/build/debug/bin/iso9660_boot.bin
+			// TODO FIXME - support remappings something like -map boot/boot.bin=c:/muos/build/debug/bin/iso9660_boot.bin
 #if false
             var test = new Mosa.MakeIsoImage.Iso9660Generator(false);
             test.AddFile("Long File Name.txt",new System.IO.FileInfo("C:\\cvs\\mosa\\Mosa\\Tools\\MakeIsoImage\\bin\\Debug\\Long File Name.txt"));
@@ -29,69 +37,76 @@ namespace Mosa.Tools.MakeIsoImage
             return;
 #endif
 
-            int i;
-            var iso = new Mosa.MakeIsoImage.Iso9660Generator(false);
-            for (i = 0; i < args.Length; i++)
-            {
-                if ( args[i].Trim()[0] != '-' )
-                    break;
-                switch (args[i].Trim())
-                {
-                    case "-boot":
-                        string name = args[++i];
-                        iso.AddBootFile(name, new System.IO.FileInfo(name));
-                        break;
-                    case "-boot-load-size":
-                        short boot_load_size;
-                        if ( short.TryParse(args[++i], out boot_load_size) )
-                            iso.BootLoadSize ( boot_load_size );
-                        break;
-                    case "-boot-info-table":
-                        iso.SetBootInfoTable(true);
-                        break;
-                    case "-label":
-                        i++;
-                        string s = args[i];
-                        iso.SetVolumeLabel(s);
-                        break;
-                    default:
-                        break;
-                }
-            }
-            // at this point, args[i] should be our iso image name
-            if (i >= args.Length)
-            {
-                Console.Write("Missing iso file name");
-                return;
-            }
-            string iso_file_name = args[i++];
-            // now args[i] is root folder
-            if (i >= args.Length)
-            {
-                Console.Write("Missing root folder");
-                return;
-            }
-            while (i < args.Length)
-            {
-                AddDirectoryTree(iso,args[i++],"");
-            }
-            iso.Generate(iso_file_name);
-        }
-        static private void AddDirectoryTree(Mosa.MakeIsoImage.Iso9660Generator iso, string root, string virtual_prepend)
-        {
-            int i;
-            var dirinfo = new System.IO.DirectoryInfo(root.Replace('/','\\'));
-            System.IO.FileInfo[] files = dirinfo.GetFiles();
-            for (i = 0; i < files.Length; i++)
-            {
-                iso.AddFile(virtual_prepend + files[i].Name, files[i]);
-            }
-            System.IO.DirectoryInfo[] dirs = dirinfo.GetDirectories();
-            for (i = 0; i < dirs.Length; i++)
-            {
-                iso.MkDir(virtual_prepend + dirs[i].Name);
-                AddDirectoryTree(iso, root + '/' + dirs[i].Name, virtual_prepend + dirs[i].Name + '/');
-            }
-        }
-    }
+			try {
+				Iso9660Generator iso = new Iso9660Generator(false);
+				int i;
+
+				for (i = 0; i < args.Length; i++) {
+					if (args[i].Trim()[0] != '-')
+						break;
+					switch (args[i].Trim()) {
+						case "-boot":
+							i++;
+							iso.AddBootFile(args[i], new System.IO.FileInfo(args[i]));
+							break;
+						case "-boot-load-size":
+							short bootLoadSize;
+							if (short.TryParse(args[++i], out bootLoadSize))
+								iso.BootLoadSize(bootLoadSize);
+							break;
+						case "-boot-info-table":
+							iso.SetBootInfoTable(true);
+							break;
+						case "-label":
+							i++;
+							iso.SetVolumeLabel(args[i]);
+							break;
+						default:
+							break;
+					}
+				}
+
+				// at this point, args[i] should be our iso image name
+				if (i >= args.Length) {
+					Console.Error.Write("Missing iso file name");
+					return -1;
+				}
+
+				string isoFileName = args[i++];
+
+				// now args[i] is root folder
+				if (i >= args.Length) {
+					Console.Error.Write("Missing root folder");
+					return -1;
+				}
+
+				while (i < args.Length)
+					AddDirectoryTree(iso, args[i++], "");
+
+				iso.Generate(isoFileName);
+
+				Console.WriteLine("Completed!");
+			}
+
+			catch (Exception e) {
+				Console.Error.WriteLine("Error: " + e.ToString());
+				return -1;
+			}
+
+			return 0;
+		}
+
+		static private void AddDirectoryTree(Iso9660Generator iso, string root, string virtualPrepend)
+		{
+			DirectoryInfo dirinfo = new DirectoryInfo(root.Replace('/', '\\'));
+
+			foreach (FileInfo file in dirinfo.GetFiles())
+				iso.AddFile(virtualPrepend + file.Name, file);
+
+			foreach (DirectoryInfo dir in dirinfo.GetDirectories()) {
+				iso.MkDir(virtualPrepend + dir.Name);
+				AddDirectoryTree(iso, root + '/' + dir.Name, virtualPrepend + dir.Name + '/');
+			}
+		}
+	}
 }
