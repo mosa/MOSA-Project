2196b4da36d3725fef2bbe4af2c524a171f12fdc
Phil Garcia <phil@thinkedge.com>
2009-06-07 05:15:53 +0000

    - Added counter display in HelloWorld app
    git-svn-id: svn://localhost/mosa@738 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Mosa/HelloWorld/Boot.cs
+++ Mosa/HelloWorld/Boot.cs
@@ -1,4 +1,10 @@
-﻿
+﻿/*
+ * (c) 2008 MOSA - The Managed Operating System Alliance
+ *
+ * Licensed under the terms of the New BSD License.
+ *
+ */
+
 namespace Mosa.HelloWorld
 {
 	/// <summary>
@@ -12,13 +18,13 @@ namespace Mosa.HelloWorld
 		public static void Main()
 		{
 			unsafe {
-                
+
 				// Clear the screen
-				for (byte* i = (byte*)0xB8000; i < (byte*)0xB8FA0; i = i + 2) 
-					WriteChar(' ', 0x0A, i);				
+				for (byte* i = (byte*)0xB8000; i < (byte*)0xB8FA0; i = i + 2)
+					WriteChar(' ', 0x0A, i);
 
 				byte* index = (byte*)(0xB8000);
-                WriteChar('M', 0x0A, index); index = index + 2;
+				WriteChar('M', 0x0A, index); index = index + 2;
 				WriteChar('O', 0x0A, index); index = index + 2;
 				WriteChar('S', 0x0A, index); index = index + 2;
 				WriteChar('A', 0x0A, index); index = index + 2;
@@ -44,7 +50,7 @@ namespace Mosa.HelloWorld
 				WriteChar('k', 0x0C, index); index = index + 2;
 				WriteChar('e', 0x0C, index); index = index + 2;
 				WriteChar('\'', 0x0A, index); index = index + 2;
-				WriteChar(' ', 0x0A, index); 
+				WriteChar(' ', 0x0A, index);
 
 				index = (byte*)(0xB80A0);
 				WriteChar('-', 0x07, index); index = index + 2;
@@ -74,8 +80,8 @@ namespace Mosa.HelloWorld
 				WriteChar('-', 0x07, index); index = index + 2;
 				WriteChar('-', 0x07, index); index = index + 2;
 				WriteChar('-', 0x07, index); index = index + 2;
-				WriteChar('-', 0x07, index); 
-			
+				WriteChar('-', 0x07, index);
+
 				index = (byte*)(0xB8140);
 				WriteChar('C', 0x0E, index); index = index + 2;
 				WriteChar('o', 0x0E, index); index = index + 2;
@@ -96,7 +102,9 @@ namespace Mosa.HelloWorld
 				WriteChar('0', 0x0A, index); index = index + 2;
 				WriteChar('0', 0x0A, index); index = index + 2;
 				WriteChar('9', 0x0A, index); index = index + 2;
-				WriteChar('.', 0x0A, index); 
+				WriteChar('.', 0x0A, index);
+
+				uint cnt = 0;
 
 				index = (byte*)(0xB8036);
 				while (true) {
@@ -104,6 +112,7 @@ namespace Mosa.HelloWorld
 					WriteChar('\\', 0x0B, index);
 					WriteChar('|', 0x0C, index);
 					WriteChar('/', 0x09, index);
+					Write(cnt++, 0x09, (byte*)(0xB8140 + 0x140));
 				}
 			}
 		}
@@ -116,9 +125,31 @@ namespace Mosa.HelloWorld
 		/// <param name="address">The address.</param>
 		private unsafe static void WriteChar(char chr, byte color, byte* address)
 		{
-			*address = (byte) chr;
+			*address = (byte)chr;
 			*(address + 1) = color;
 		}
 
+		/// <summary>
+		/// Writes the specified value.
+		/// </summary>
+		/// <param name="val">The val.</param>
+		/// <param name="color">The color.</param>
+		/// <param name="address">The address.</param>
+		private unsafe static void Write(uint val, byte color, byte* address)
+		{
+			int count = 0;
+			uint temp = val;
+
+			do {
+				temp /= 10;
+				count++;
+			}
+			while (temp != 0);
+
+			for (int i = 0; i < count; i++) {
+				WriteChar((char)('0' + (val % 10)), color, address + (2 * (count - 1 - i)));
+				val /= 10;
+			}
+		}
 	}
 }
