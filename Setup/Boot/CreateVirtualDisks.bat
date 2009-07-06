774021dc6d35caf55f13f3cf5a2dec4a296e21ed
Phil Garcia <phil@thinkedge.com>
2009-07-06 14:46:42 +0000

    git-svn-id: svn://localhost/mosa@799 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Setup/Boot/CreateVirtualDisks.bat
+++ Setup/Boot/CreateVirtualDisks.bat
@@ -2,6 +2,6 @@
 ..\..\Mosa\Bin\Mosa.Tools.CreateBootImage.exe Vmware\mosaboot-vhd.config build\bootimage.vhd
 ..\..\Mosa\Bin\Mosa.Tools.CreateBootImage.exe VirtualBox\mosaboot-vdi.config build\bootimage.vdi
 ..\..\Mosa\Bin\Mosa.Tools.CreateBootImage.exe Qemu\mosaboot-img.config build\bootimage.img
-..\..\Mosa\Bin\Mosa.Tools.CreateBootImage.exe Floppy\mosaboot-floppy.config build\floppy144.img
+rem ..\..\Mosa\Bin\Mosa.Tools.CreateBootImage.exe Floppy\mosaboot-floppy.config build\floppy144.img
 
 CALL MakeISOImage.bat
