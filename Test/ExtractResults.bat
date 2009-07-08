7a0b39e1a089678e1ced20a325ed620361f936f8
Phil Garcia <phil@thinkedge.com>
2009-07-08 13:41:16 +0000

    - Added stylesheet for failed tests only
    git-svn-id: svn://localhost/mosa@807 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Test/ExtractResults.bat
+++ Test/ExtractResults.bat
@@ -1,2 +1,3 @@
-..\Mosa\bin\Mosa.Tools.XmlTo.exe Reports\Tests.xml Gallio-Text.xsl Reports\Tests.txt
+..\Mosa\bin\Mosa.Tools.XmlTo.exe Reports\Tests.xml Stylesheets\Gallio-All.xsl Reports\Tests.txt
+..\Mosa\bin\Mosa.Tools.XmlTo.exe Reports\Tests.xml Stylesheets\Gallio-Failed.xsl Reports\Failed.txt
 
