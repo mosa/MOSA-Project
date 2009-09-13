2572a5c577aaf67b090c2f08e7bc7b40eed6f2ed
Phil Garcia <phil@thinkedge.com>
2009-09-13 20:17:04 +0000

    - Experimental Branch Merge #12
    git-svn-id: svn://localhost/mosa@914 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Mosa/Runtime/CompilerFramework/CIL/BinaryComparisonInstruction.cs
+++ Mosa/Runtime/CompilerFramework/CIL/BinaryComparisonInstruction.cs
@@ -12,12 +12,15 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
 
+using Mosa.Runtime.Metadata;
+using Mosa.Runtime.Metadata.Signatures;
+
 namespace Mosa.Runtime.CompilerFramework.CIL
 {
 	/// <summary>
 	/// 
 	/// </summary>
-	public class BinaryComparisonInstruction : CILInstruction
+	public class BinaryComparisonInstruction : BinaryInstruction
 	{
 		#region Construction
 
@@ -31,6 +34,67 @@ namespace Mosa.Runtime.CompilerFramework.CIL
 		}
 
 		#endregion // Construction
+		
+		#region ICILInstruction Overrides
+
+		/// <summary>
+		/// Decodes the specified instruction.
+		/// </summary>
+		/// <param name="instruction">The instruction.</param>
+		/// <param name="decoder">The instruction decoder, which holds the code stream.</param>
+		public override void Decode(ref InstructionData instruction, IInstructionDecoder decoder)
+		{
+			// Decode base classes first
+			base.Decode(ref instruction, decoder);
+
+			// Set the result
+			instruction.Result = decoder.Compiler.CreateTemporary(new SigType(CilElementType.I4));
+		}
+
+		/// <summary>
+		/// Returns a formatted representation of the opcode.
+		/// </summary>
+		/// <returns>The code as a string value.</returns>
+		public override string ToString(ref InstructionData instruction)
+		{
+			string result, op;
+			bool un = false;
+			switch (_opcode) {
+				case OpCode.Ceq:
+					op = @"==";
+					break;
+
+				case OpCode.Cgt:
+					op = @">";
+					break;
+
+				case OpCode.Cgt_un:
+					op = @">";
+					un = true;
+					break;
+
+				case OpCode.Clt:
+					op = @"<";
+					break;
+
+				case OpCode.Clt_un:
+					op = @"<";
+					un = true;
+					break;
+
+				default:
+					throw new InvalidOperationException(@"Invalid opcode.");
+			}
+
+			if (true == un)
+				result = String.Format(@"{4} ; {0} = unchecked({1} {2} {3})", instruction.Result, instruction.Operand1, op, instruction.Operand2, base.ToString());
+			else
+				result = String.Format(@"{4} ; {0} = ({1} {2} {3})", instruction.Result, instruction.Operand1, op, instruction.Operand2, base.ToString());
+
+			return result;
+		}
+
+		#endregion // ICILInstruction Overrides
 
 	}
 }
