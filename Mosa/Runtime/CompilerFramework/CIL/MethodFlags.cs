1cc37f141a4ce8964cf5eacf23fb8f0479c18cde
Phil Garcia <phil@thinkedge.com>
2009-09-25 17:31:33 +0000

    - Experimental Branch Merge #91
    git-svn-id: svn://localhost/mosa@993 45b04e34-05f1-4d87-a130-40a8491d5c8b

---
+++ Mosa/Runtime/CompilerFramework/CIL/MethodFlags.cs
@@ -0,0 +1,55 @@
+/*
+ * (c) 2008 MOSA - The Managed Operating System Alliance
+ *
+ * Licensed under the terms of the New BSD License.
+ *
+ * Authors:
+ *  Michael Ruck (<mailto:sharpos@michaelruck.de>)
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mosa.Runtime.CompilerFramework.CIL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Flags]
+    public enum MethodFlags : ushort
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        TinyFormat = 0x02,
+        /// <summary>
+        /// 
+        /// </summary>
+        FatFormat = 0x03,
+        /// <summary>
+        /// 
+        /// </summary>
+        MoreSections = 0x08,
+        /// <summary>
+        /// 
+        /// </summary>
+        InitLocals = 0x10,
+        /// <summary>
+        /// 
+        /// </summary>
+        TinyCodeSizeMask = 0xFC,
+        /// <summary>
+        /// 
+        /// </summary>
+        HeaderSizeMask = 0xF000,
+        /// <summary>
+        /// 
+        /// </summary>
+        ValidHeader = 0x3000,
+        /// <summary>
+        /// 
+        /// </summary>
+        HeaderMask = 0x0003
+    }
+}
