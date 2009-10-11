d22c6e147bd9e7771a985528a0574222851c56c3
Phil Garcia <phil@thinkedge.com>
2009-10-11 03:48:50 +0000

    - Experimental Branch Merge #197
    git-svn-id: svn://localhost/mosa@1099 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Mosa/Tools/Compiler/AotMethodCompiler.cs
+++ Mosa/Tools/Compiler/AotMethodCompiler.cs
@@ -52,7 +52,7 @@ namespace Mosa.Tools.Compiler
                 new CilToIrTransformationStage(),
                 //InstructionLogger.Instance,
                 //InstructionStatisticsStage.Instance,
-                new DominanceCalculationStage(),
+                //new DominanceCalculationStage(),
                 //InstructionLogger.Instance,
                 //new EnterSSA(),
                 //InstructionLogger.Instance,
