aa4779b227f88e39c1161f1ad057e5db74c32a6b
Phil Garcia <phil@thinkedge.com>
2009-10-23 02:29:18 +0000

    - Experimental Branch Merge #298
    git-svn-id: svn://localhost/mosa@1200 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Mosa/Platforms/x86/IRToX86TransformationStage.cs
+++ Mosa/Platforms/x86/IRToX86TransformationStage.cs
@@ -436,12 +436,14 @@ namespace Mosa.Platforms.x86
 		/// <param name="ctx">The context.</param>
 		void IR.IIRVisitor.UDivInstruction(Context ctx)
 		{
-			ThreeTwoAddressConversion(ctx, ctx.Instruction);
+			ThreeTwoAddressConversion(ctx, CPUx86.Instruction.DivInstruction);
+
+			//Operand op1 = ctx.Operand1;
+			//Operand op2 = ctx.Operand2;
 
 			//RegisterOperand edx = new RegisterOperand(new SigType(CilElementType.U4), GeneralPurposeRegister.EDX);
-			//_codeEmitter.Xor(edx, edx);
-			//_codeEmitter.Div(instruction.Operand0, instruction.Operand1);
-
+			//ctx.InsertInstructionAfter(CPUx86.Instruction.XorInstruction, edx, edx);
+			//ctx.InsertInstructionAfter(CPUx86.Instruction.DivInstruction, op1, op2);
 		}
 
 		/// <summary>
@@ -725,9 +727,6 @@ namespace Mosa.Platforms.x86
 		/// <param name="instruction">The instruction.</param>
 		private static void ThreeTwoAddressConversion(Context ctx, IInstruction instruction)
 		{
-			if (ctx.OperandCount != 2)
-				return;
-
 			Operand opRes = ctx.Result;
 			Operand op1 = ctx.Operand1;
 			Operand op2 = ctx.Operand2;
