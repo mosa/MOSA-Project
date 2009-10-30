3bdea4e3439d236eac711a9f46026f768ca96552
Phil Garcia <phil@thinkedge.com>
2009-10-30 09:08:28 +0000

    - Experimental Branch Merge #355
    git-svn-id: svn://localhost/mosa@1257 45b04e34-05f1-4d87-a130-40a8491d5c8b

---
+++ Mosa/QuickTest/Test.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * (c) 2008 MOSA - The Managed Operating System Alliance
+ *
+ * Licensed under the terms of the New BSD License.
+ *
+ */
+
+namespace Mosa.QuickTest
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class Test
+	{
+		/// <summary>
+		/// Main
+		/// </summary>
+		public static void Main()
+		{
+			int x = 3;
+			int y = 4 + x;
+			int z = x * y;
+			int a = z - 5;
+			int b = a;
+			if (a > z)
+				b = a * 100;
+			else
+				b = a * 1000;
+		}
+
+	}
+}
