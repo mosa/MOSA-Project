c5e7a899f170c0e9b9fe73d9e220ad97781c8268
Phil Garcia <phil@thinkedge.com>
2009-11-09 05:36:26 +0000

    - Experimental Branch Merge #393
    git-svn-id: svn://localhost/mosa@1295 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Mosa/QuickTest/Test.cs
+++ Mosa/QuickTest/Test.cs
@@ -17,22 +17,16 @@ namespace Mosa.QuickTest
 		/// </summary>
 		public static void Main()
 		{
-			//int x = 3;
-			//int y = 4 + x;
-			//int z = x * y;
-			//int a = z - 5;
-			//int b = a;
-			//if (a > z)
-			//    b = a * 100;
-			//else
-			//    b = a * 1000;
-			//int q = a / 10;
-
-			//byte a = 11;
-			//int b = a;// +22;
-			//byte c = (byte)b;
-
-			//long x = 16;
+			int x = 3;
+			int y = 4 + x;
+			int z = x * y;
+			int a = z - 5;
+			int b = a;
+			if (a > z)
+				b = a * 100;
+			else
+				b = a * 1000;
+			int q = a / 10;
 		}
 
 		/// <summary>
