f8f0474d8f883acf0175be59a17cf2f67b1ccbd1
Phil Garcia <phil@thinkedge.com>
2009-11-09 07:28:52 +0000

    - Experimental Branch Merge #394
    git-svn-id: svn://localhost/mosa@1296 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Tests/ExtractResults.bat
+++ Tests/ExtractResults.bat
@@ -1,3 +1,3 @@
 ..\Mosa\bin\Mosa.Tools.XmlTo.exe Reports\Tests.xml Stylesheets\Gallio-All.xsl Reports\Tests.txt
 ..\Mosa\bin\Mosa.Tools.XmlTo.exe Reports\Tests.xml Stylesheets\Gallio-Failed.xsl Reports\Failed.txt
-
+..\Mosa\bin\Mosa.Tools.XmlTo.exe Reports\Tests.xml Stylesheets\Gallio-Passed.xsl Reports\Passed.txt
