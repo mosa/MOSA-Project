49b3aa486b518caff387636e9433d3754327e2ad
Phil Garcia <phil@thinkedge.com>
2009-11-16 01:59:30 +0000

    - Experimental Branch Merge #425
    git-svn-id: svn://localhost/mosa@1327 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Mosa/QuickTest/Test.cs
+++ Mosa/QuickTest/Test.cs
@@ -7,46 +7,33 @@
 
 namespace Mosa.QuickTest
 {
-	/// <summary>
-	/// 
-	/// </summary>
-	public static class App
-	{
-		/// <summary>
-		/// Main
-		/// </summary>
-		public static void Main()
-		{
-			int x = 3;
-			int y = 4 + x;
-			int z = x * y;
-			int a = z - 5;
-			int b = a;
-			if (a > z)
-				b = a * 100;
-			else
-				b = a * 1000;
-			int q = a / 10;
-		}
-
-		/// <summary>
-		/// Tests the specified expect.
-		/// </summary>
-		/// <param name="expect">The expect.</param>
-		/// <param name="a">A.</param>
-		/// <param name="b">The b.</param>
-		/// <returns></returns>
-        static long AddPtr(long a, long b)
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class App
+    {
+        /// <summary>
+        /// Main
+        /// </summary>
+        public static void Main()
         {
-            if (a > 2)
-                b = a * 100;
-            else
-                b = a * 1000;
 
-            if (b > a)
-                return (a / b + a);
-
-            return (a + b);
         }
-	}
+        /// <summary>
+        /// Muls the constant C right.
+        /// </summary>
+        /// <param name="expect">The expect.</param>
+        /// <param name="a">A.</param>
+        /// <param name="b">The b.</param>
+        /// <returns></returns>
+        static bool MulConstantCRight(char expect, char a)
+        {
+            return expect == a * (char)3;
+        }
+
+        //public static bool Ceq(long a, long b)
+        //{
+        //    return (a == b);
+        //}
+    }
 }
