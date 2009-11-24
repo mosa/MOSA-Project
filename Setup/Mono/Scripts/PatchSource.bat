5239e022ce48a15552bee0744b4ce8e67ff844e2
Phil Garcia <phil@thinkedge.com>
2009-11-24 02:48:51 +0000

    - Experimental Branch Merge #494
    git-svn-id: svn://localhost/mosa@1395 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Setup/Mono/Scripts/PatchSource.bat
+++
@@ -1,3 +0,0 @@
-copy Patches\Parser.cs "build\mono-%1\mcs\class\System.XML\System.Xml.XPath\"
-copy Patches\PatternParser.cs "build\mono-%1\mcs\class\System.XML\System.Xml.XPath\"
-copy Patches\PatternTokenizer.cs "build\mono-%1\mcs\class\System.XML\System.Xml.XPath\"
--- Setup/Mono/Scripts/UpdateProject.bat
+++
@@ -1,3 +0,0 @@
-copy "Projects\%2" "build\mono-%1\mcs\class\%2"
-
-..\..\Mosa\bin\Mosa.Tools.Mono.UpdateProject.exe "build\mono-%1\mcs\class\%2" > NUL
