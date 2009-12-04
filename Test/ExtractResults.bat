81afb7da84162da7c8a38e5a17a1cab3feb5d25c
Phil Garcia <phil@thinkedge.com>
2009-12-04 08:34:00 +0000

    - Removed old Test directory
    git-svn-id: svn://localhost/mosa@1422 45b04e34-05f1-4d87-a130-40a8491d5c8b

--- Test/ExtractResults.bat
+++
@@ -1,3 +0,0 @@
-..\Mosa\bin\Mosa.Tools.XmlTo.exe Reports\Tests.xml Stylesheets\Gallio-All.xsl Reports\Tests.txt
-..\Mosa\bin\Mosa.Tools.XmlTo.exe Reports\Tests.xml Stylesheets\Gallio-Failed.xsl Reports\Failed.txt
-
