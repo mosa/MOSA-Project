e62de72bfc9a18ffc5b757a99f6b75ab9a9254e0
Phil Garcia <phil@thinkedge.com>
2009-12-13 01:31:38 +0000

    - Added Native methods stubs for FlushTLB, SetCR0 and SetCR3 to emulator
    git-svn-id: svn://localhost/mosa@1430 45b04e34-05f1-4d87-a130-40a8491d5c8b

---
+++ Mosa/EmulatedKernel/Native.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * (c) 2008 MOSA - The Managed Operating System Alliance
+ *
+ * Licensed under the terms of the New BSD License.
+ *
+ * Authors:
+ *  Phil Garcia (tgiphil) <phil@thinkedge.com>
+ */
+
+namespace Mosa.Platforms.x86
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class Native
+	{
+
+		/// <summary>
+		/// Flushes the Translation Lookaside Buffer (TLB).
+		/// </summary>
+		/// <param name="address">The address.</param>
+		public static void FlushTLB(uint address)
+		{
+		}
+
+		/// <summary>
+		/// Sets the cr0.
+		/// </summary>
+		/// <param name="state">The state.</param>
+		public static void SetCR0(byte state) 
+		{
+		}
+
+		/// <summary>
+		/// Sets the cr3.
+		/// </summary>
+		/// <param name="state">The state.</param>
+		public static void SetCR3(byte state)
+		{
+		}
+
+	}
+}
