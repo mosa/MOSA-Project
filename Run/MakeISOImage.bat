438b790f0fac0f20a7592f0e82ecb5bd14ae6c36
tgiphil <phil@thinkedge.com>
2009-12-31 16:59:21 -0800

    - Minor correction to MakeISOImage

--- Run/MakeISOImage.bat
+++ Run/MakeISOImage.bat
@@ -3,7 +3,7 @@ mkdir build\iso\boot
 mkdir build\iso\boot\grub
 
 copy ISO\menu.lst build\iso\boot\grub
-copy ..\Tools\grub\\stage2_eltorito build\iso\boot\grub
+copy ..\Tools\grub\stage2_eltorito build\iso\boot\grub
 copy build\hello.exe build\iso\boot
 
 ..\bin\Mosa.MakeIsoImage.exe -label mosa -boot ..\Tools\grub\stage2_eltorito -boot-info-table -boot-load-size 4 build\bootimage.iso build\iso
