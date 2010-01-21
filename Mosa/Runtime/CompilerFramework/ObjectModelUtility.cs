692456e8f5bd96a4ee44f0217389a8d3de985eec
Simon Wollwage <kintaro@think-in-co.de>
2010-01-21 02:28:14 +0100

    Fixed struct size issue

--- Mosa/Runtime/CompilerFramework/ObjectModelUtility.cs
+++ Mosa/Runtime/CompilerFramework/ObjectModelUtility.cs
@@ -17,13 +17,20 @@ namespace Mosa.Runtime.CompilerFramework
         public static int ComputeTypeSize(TokenTypes token, IMetadataProvider metadataProvider, IArchitecture architecture)
         {
             Metadata.Tables.TypeDefRow typeDefinition;
-            Metadata.Tables.TypeDefRow followingTypeDefinition;
+            Metadata.Tables.TypeDefRow followingTypeDefinition = new Mosa.Runtime.Metadata.Tables.TypeDefRow();
             metadataProvider.Read(token, out typeDefinition);
-            metadataProvider.Read(token + 1, out followingTypeDefinition);
+            try
+            {
+                metadataProvider.Read(token + 1, out followingTypeDefinition);
+            }
+            catch (System.Exception)
+            {
+            }
 
             int result = 0;
             TokenTypes fieldList = typeDefinition.FieldList;
-            while (fieldList != followingTypeDefinition.FieldList)
+            TokenTypes last = metadataProvider.GetMaxTokenValue(TokenTypes.Field);
+            while (fieldList != followingTypeDefinition.FieldList && fieldList != last)
                 result += FieldSize(fieldList++, metadataProvider, architecture);
 
             return result;
