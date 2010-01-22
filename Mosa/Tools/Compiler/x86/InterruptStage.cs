9ded60aa405bb3a562b5b2bb602ce728a8f35b84
Simon Wollwage <kintaro@think-in-co.de>
2010-01-22 15:01:38 +0100

    Fixed Interruptstage to emit correct setup of ID Table

--- Mosa/Tools/Compiler/x86/InterruptStage.cs
+++ Mosa/Tools/Compiler/x86/InterruptStage.cs
@@ -60,8 +60,8 @@ namespace Mosa.Tools.Compiler.x86
 		{
 			_linker = compiler.Pipeline.FindFirst<IAssemblyLinker>();
 
-			CreateIVTMethod(compiler);
 			CreateISRMethods(compiler);
+			CreateIVTMethod(compiler);
 		}
 
 		#endregion // IAssemblyCompilerStage Members
@@ -79,23 +79,24 @@ namespace Mosa.Tools.Compiler.x86
 
 			SigType I1 = new SigType(CilElementType.I1);
 			SigType I4 = new SigType(CilElementType.I4);
+			RegisterOperand eax = new RegisterOperand(I4, GeneralPurposeRegister.EAX);
 
 			for (int i = 0; i <= 256; i++) {
 				InstructionSet set = new InstructionSet(100);
 				Context ctx = new Context(set, -1);
 
-				ctx.AppendInstruction(CPUx86.Instruction.CliInstruction);
+				ctx.SetInstruction(CPUx86.Instruction.CliInstruction);
 				if ((i != 8) && (i < 10 || i > 14)) // For IRQ 8, 10, 11, 12, 13, 14 the cpu automatically pushed the error code
-					ctx.AppendInstruction(CPUx86.Instruction.PushInstruction, new ConstantOperand(I1, 0x0));
+					ctx.AppendInstruction(CPUx86.Instruction.PushInstruction, null, new ConstantOperand(I4, 0x0));
 				ctx.AppendInstruction(CPUx86.Instruction.PushadInstruction);
-				ctx.AppendInstruction(CPUx86.Instruction.PushInstruction, new ConstantOperand(I4, i));
+				ctx.AppendInstruction(CPUx86.Instruction.PushInstruction, null, new ConstantOperand(I4, i));
 				// TODO: Set method parameters 
 				ctx.AppendInstruction(CPUx86.Instruction.CallInstruction, InterruptMethod);
-				ctx.AppendInstruction(CPUx86.Instruction.PopInstruction);
+				ctx.AppendInstruction(CPUx86.Instruction.PopInstruction, eax);
 				ctx.AppendInstruction(CPUx86.Instruction.PopadInstruction);
-				ctx.AppendInstruction(CPUx86.Instruction.PopInstruction);
+				ctx.AppendInstruction(CPUx86.Instruction.PopInstruction, eax);
 				ctx.AppendInstruction(CPUx86.Instruction.StiInstruction);
-				ctx.AppendInstruction(CPUx86.Instruction.IRetdInstruction);
+				//ctx.AppendInstruction(CPUx86.Instruction.IRetdInstruction);
 
 				CompilerGeneratedMethod method = LinkTimeCodeGenerator.Compile(compiler, @"InterruptISR" + i.ToString(), set);
 			}
@@ -110,7 +111,7 @@ namespace Mosa.Tools.Compiler.x86
 			InstructionSet set = new InstructionSet(4048);
 			Context ctx = new Context(set, -1);
 
-			ctx.AppendInstruction(IR.Instruction.PrologueInstruction);
+			ctx.SetInstruction(IR.Instruction.PrologueInstruction);
 			ctx.Other = 0; // stacksize
 
 			// Create the IVT Table
@@ -119,15 +120,16 @@ namespace Mosa.Tools.Compiler.x86
 			RegisterOperand eax = new RegisterOperand(I4, GeneralPurposeRegister.EAX);
 			RegisterOperand ebx = new RegisterOperand(I4, GeneralPurposeRegister.EBX);
 
-			ctx.AppendInstruction(CPUx86.Instruction.MovInstruction, ecx, new ConstantOperand(I4, 0x201000));
+			ctx.AppendInstruction(CPUx86.Instruction.MovInstruction, ecx, new ConstantOperand(I4, (int)0x201000));
 
-			for (int i = 0; i <= 256; i++) {
-				ctx.AppendInstruction(CPUx86.Instruction.MovInstruction, eax, new LabelOperand(I4, @"Mosa.Tools.Compiler.LinkerGenerated.<$>InterruptISR" + i.ToString() + "()"));
-				//ctx.Label = i;
-				ctx.AppendInstruction(CPUx86.Instruction.MovInstruction, new MemoryOperand(I4, ecx.Register, new IntPtr(i * 4)), eax);
+			for (int i = 0; i <= 256; i++) 
+			{
+				LinkerSymbol symbol = _linker.GetSymbol(@"Mosa.Tools.Compiler.LinkerGenerated.<$>InterruptISR" + i.ToString() + "()");
+				Operand address = new ConstantOperand(I4, (int)_linker.GetSection(SectionKind.Text).VirtualAddress + symbol.SectionAddress);
+				ctx.AppendInstruction(CPUx86.Instruction.MovInstruction, new MemoryOperand(I4, ecx.Register, new IntPtr(i * 4)), address);
 			}
 
-			ctx.SetInstruction(IR.Instruction.EpilogueInstruction);
+			ctx.AppendInstruction(IR.Instruction.EpilogueInstruction);
 			ctx.Other = 0;
 
 			CompilerGeneratedMethod method = LinkTimeCodeGenerator.Compile(compiler, @"InterruptInit", set);
