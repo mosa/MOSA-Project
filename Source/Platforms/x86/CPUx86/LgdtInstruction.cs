9b436843ec36f27608ccf4b63bf65dec7cdb914c
Simon Wollwage <kintaro@think-in-co.de>
2010-02-11 01:27:29 +0100

    Fixed loading of IDT and GDT register

--- Source/Platforms/x86/CPUx86/LgdtInstruction.cs
+++ Source/Platforms/x86/CPUx86/LgdtInstruction.cs
@@ -64,7 +64,10 @@ namespace Mosa.Platforms.x86.CPUx86
 		/// <param name="context">The context.</param>
 		public void ReplaceIntrinsicCall(Context context)
 		{
-			context.SetInstruction(CPUx86.Instruction.LgdtInstruction, null, context.Operand1);
+			//context.SetInstruction(CPUx86.Instruction.LgdtInstruction, null, context.Operand1);
+			MemoryOperand operand = new MemoryOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.Ptr), GeneralPurposeRegister.EAX, new System.IntPtr(0));
+			context.SetInstruction(CPUx86.Instruction.MovInstruction, new RegisterOperand(new Mosa.Runtime.Metadata.Signatures.SigType(Mosa.Runtime.Metadata.CilElementType.Ptr), GeneralPurposeRegister.EAX), context.Operand1);
+			context.AppendInstruction(CPUx86.Instruction.LgdtInstruction, null, operand);
 		}
 
 		#endregion // Methods
