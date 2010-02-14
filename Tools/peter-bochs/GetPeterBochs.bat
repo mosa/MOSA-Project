3437fd4897145710bf6439f7044ce48331c8b7ce
tgiphil <phil@thinkedge.com>
2010-02-14 11:36:51 -0800

    - Added support for peter-bochs-debugger

---
+++ Tools/peter-bochs/GetPeterBochs.bat
@@ -0,0 +1,7 @@
+if exist peter-bochs-debugger.jar goto exit
+
+..\wget\wget http://peter-bochs.googlecode.com/files/peter-bochs-debugger20100207.jar
+
+rename peter-bochs-debugger20100207.jar peter-bochs-debugger.jar
+
+:exit
