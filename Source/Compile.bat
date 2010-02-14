562f6e2ecaccc47033580a94b461d1d9974a27e4
tgiphil <phil@thinkedge.com>
2010-02-14 13:48:56 +0800

    - Fixed Compile script for non-Vista systems

--- Source/Compile.bat
+++ Source/Compile.bat
@@ -1,4 +1,4 @@
 set MSBUILDPATH=%SYSTEMROOT%\Microsoft.NET\Framework\v3.5
 set MSBUILD=%MSBUILDPATH%\msbuild.exe
 
-%MSBUILD% /nologo /m /p:BuildInParallel=true /p:Configuration=Release mosa.sln
+%MSBUILD% /nologo /m /p:BuildInParallel=true /p:Configuration=Release /p:Platform="Any CPU" mosa.sln
