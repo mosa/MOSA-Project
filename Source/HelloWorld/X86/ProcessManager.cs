3ae98ea51981bc860f3b5d43cc8a08058a9f5a7b
tgiphil <phil@thinkedge.com>
2010-02-20 13:20:05 -0800

    - Worked on process memory allocation

--- Source/HelloWorld/X86/ProcessManager.cs
+++ Source/HelloWorld/X86/ProcessManager.cs
@@ -46,20 +46,75 @@ namespace Mosa.Kernel.X86
 
 			// TODO: Unlock
 
-			return process->ProcessId;
+			return (uint)process;
 		}
 
 		/// <summary>
 		/// Terminates the process.
 		/// </summary>
 		/// <param name="pid">The pid.</param>
-		public static void TerminateProcess(uint pid)
+		public static void TerminateProcess(uint process)
 		{
 			// TODO
 
 			// 1. Set status to terminating
 			// 2. Stop all threads
 			// 3. Release all memory
+		}
+
+		/// <summary>
+		/// Allocates the memory.
+		/// </summary>
+		/// <param name="process">The process.</param>
+		/// <param name="size">The size.</param>
+		/// <returns></returns>
+		public static uint AllocateMemory(uint process, uint size)
+		{
+			// Normalize 
+			uint normsize = (uint)((size + PageFrameAllocator.PageSize - 1) & ~(PageFrameAllocator.PageSize - 1));
+			uint pages = normsize / PageFrameAllocator.PageSize;
+			uint address = VirtualPageAllocator.Reserve(pages);
+
+			UpdateMemoryBitMap(process, address, pages, false);
+
+			return address;
+		}
+
+		/// <summary>
+		/// Updates the memory bit map.
+		/// </summary>
+		/// <param name="process">The process.</param>
+		/// <param name="address">The address.</param>
+		/// <param name="pages">The pages.</param>
+		/// <param name="free">if set to <c>true</c> [free].</param>
+		private unsafe static void UpdateMemoryBitMap(uint process, uint address, uint pages, bool free)
+		{
+			uint bitmap = ((Process*)process)->MemoryMap;
+			uint start = address / PageFrameAllocator.PageSize;
+
+			for (uint index = 0; index < pages; index++)
+				SetPageStatus(bitmap, start + index, free);
+		}
+
+		/// <summary>
+		/// Sets the page status in the bitmap.
+		/// </summary>
+		/// <param name="page">The page.</param>
+		/// <param name="free">if set to <c>true</c> [free].</param>
+		private static void SetPageStatus(uint bitmap, uint page, bool free)
+		{
+			uint at = (uint)(bitmap + (page / 32));
+			byte bit = (byte)(page % 32);
+			uint mask = (byte)(1 << bit);
+
+			uint value = Memory.Get32(at);
+
+			if (free)
+				value = (byte)(value & ~mask);
+			else
+				value = (byte)(value | mask);
+
+			Memory.Set32(at, value);
 		}
 
 		/// <summary>
