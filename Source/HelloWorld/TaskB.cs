07e69a8faa5ceba6721d32e4640d96753c8886ae
tgiphil <phil@thinkedge.com>
2010-02-27 10:22:31 -0800

    - Worked on threading

---
+++ Source/HelloWorld/TaskB.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * (c) 2008 MOSA - The Managed Operating System Alliance
+ *
+ * Licensed under the terms of the New BSD License.
+ *
+ */
+
+using Mosa.Platforms.x86;
+using Mosa.Kernel.X86;
+
+namespace Mosa.HelloWorld
+{
+	
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class TaskB
+	{
+		public static uint _counter = 0;
+
+		/// <summary>
+		/// Mains this instance.
+		/// </summary>
+		public static void Run()
+		{
+			while (true) {
+				_counter++;
+				Native.Cli();
+				Screen.SetCursor(17, 0);
+				Screen.Write(_counter, 8, 10);
+				Native.Sti();
+			}
+		}
+
+
+	}
+}
---
+++ Source/HelloWorld/X86/IdleTask.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * (c) 2008 MOSA - The Managed Operating System Alliance
+ *
+ * Licensed under the terms of the New BSD License.
+ *
+ */
+
+using Mosa.Platforms.x86;
+using Mosa.Kernel.X86;
+
+namespace Mosa.Platforms.x86.Intrinsic
+{
+	
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class IdleTask
+	{
+		public static uint _counter = 0;
+
+		/// <summary>
+		/// Mains this instance.
+		/// </summary>
+		public static void Run()
+		{
+			while (true) {
+				_counter++;
+				Native.Hlt();	// wait for interrupt
+			}
+		}
+
+	}
+}
