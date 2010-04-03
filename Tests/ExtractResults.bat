3ee962a5e3fb4d14946ddeb3c23d050d8b05f13d
tgiphil <phil@thinkedge.com>
2010-04-04 00:45:00 +0800

    - Updated test scripts (trivial change)

--- Tests/ExtractResults.bat
+++ Tests/ExtractResults.bat
@@ -1,3 +1,3 @@
-..\bin\Mosa.Tools.XmlTo.exe Reports\Tests.xml Stylesheets\Gallio-All.xsl Reports\Tests.txt
-..\bin\Mosa.Tools.XmlTo.exe Reports\Tests.xml Stylesheets\Gallio-Failed.xsl Reports\Failed.txt
-..\bin\Mosa.Tools.XmlTo.exe Reports\Tests.xml Stylesheets\Gallio-Passed.xsl Reports\Passed.txt
+..\bin\Mosa.Tools.XmlTo.exe Reports\Tests.xml Stylesheets\Gallio-All.xsl reports\Tests.txt
+..\bin\Mosa.Tools.XmlTo.exe Reports\Tests.xml Stylesheets\Gallio-Failed.xsl reports\Failed.txt
+..\bin\Mosa.Tools.XmlTo.exe Reports\Tests.xml Stylesheets\Gallio-Passed.xsl reports\Passed.txt
