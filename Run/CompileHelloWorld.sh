d17e1787177a03992318e5d8cc080d212c0ecd7c
Simon Wollwage <kintaro@think-in-co.de>
2010-05-07 01:14:31 +0200

    - Fixed Compile script for Unix systems

--- Run/CompileHelloWorld.sh
+++ Run/CompileHelloWorld.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir build
 rm ./build/hello.exe
-mono ../bin/mosacl.exe -a=x86 -f=PE -pe-file-alignment=4096 -b=mb0.7 -o ./build/hello.exe ../bin/Mosa.HelloWorld.exe -sa
+mono ../bin/mosacl.exe -a=x86 -f=PE -pe-file-alignment=4096 -b=mb0.7 -sa -o ./build/hello.exe ../bin/Mosa.HelloWorld.exe ../bin/Mosa.Kernel.dll
