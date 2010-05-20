bec65c5f475effc06ca57a5877f3ef362c81df04
tgiphil <phil@thinkedge.com>
2010-05-20 08:50:27 -0700

    - Merged with grover's updates

--- Tests/Runtime/CompilerFramework/BaseCode/TestCaseMethodCompiler.cs
+++ Tests/Runtime/CompilerFramework/BaseCode/TestCaseMethodCompiler.cs
@@ -22,30 +22,30 @@ using Mosa.Tools.Compiler;
 
 namespace Test.Mosa.Runtime.CompilerFramework.BaseCode
 {
-    class TestCaseMethodCompiler : MethodCompilerBase
-    {
-        private readonly TestCaseAssemblyCompiler assemblyCompiler;
+	class TestCaseMethodCompiler : MethodCompilerBase
+	{
+		private readonly TestCaseAssemblyCompiler assemblyCompiler;
 
-        public TestCaseMethodCompiler(TestCaseAssemblyCompiler assemblyCompiler, IAssemblyLinker linker, IArchitecture architecture, ICompilationSchedulerStage compilationScheduler, RuntimeType type, RuntimeMethod method) :
-            base(linker, architecture, compilationScheduler, type, method)
-        {
-            this.assemblyCompiler = assemblyCompiler;
+		public TestCaseMethodCompiler(TestCaseAssemblyCompiler assemblyCompiler, IAssemblyLinker linker, IArchitecture architecture, ICompilationSchedulerStage compilationScheduler, RuntimeType type, RuntimeMethod method) :
+			base(linker, architecture, compilationScheduler, type, method)
+		{
+			this.assemblyCompiler = assemblyCompiler;
 
-            // Populate the pipeline
-            this.Pipeline.AddRange(new IMethodCompilerStage[] {
+			// Populate the pipeline
+			this.Pipeline.AddRange(new IMethodCompilerStage[] {
                 new DecodingStage(),
-                //new InstructionLogger(),
+                //InstructionLogger.Instance,
                 new BasicBlockBuilderStage(),
-                //new InstructionLogger(),
+                //InstructionLogger.Instance,
 				new OperandDeterminationStage(),
-                new InstructionLogger(),
+                //InstructionLogger.Instance,
                 new StaticAllocationResolutionStage(),
-                //new InstructionLogger(),
+                //InstructionLogger.Instance,
                 //new ConstantFoldingStage(),
                 new CILTransformationStage(),
-                //new InstructionLogger(),
+                //InstructionLogger.Instance,
                 new CILLeakGuardStage() { MustThrowCompilationException = true },
-                //new InstructionLogger(),
+                //InstructionLogger.Instance,
 				//InstructionStatisticsStage.Instance,
                 //new DominanceCalculationStage(),
                 //new EnterSSA(),
@@ -54,40 +54,40 @@ namespace Test.Mosa.Runtime.CompilerFramework.BaseCode
                 //new LeaveSSA(),
 				new StackLayoutStage(),
 				new PlatformStubStage(),
-                //new InstructionLogger(),
+                //InstructionLogger.Instance,
 				//new BlockReductionStage(),
 				new LoopAwareBlockOrderStage(),
 				//new SimpleTraceBlockOrderStage(),
 				//new ReverseBlockOrderStage(),  // reverse all the basic blocks and see if it breaks anything
 				//new BasicBlockOrderStage()	
 				new CodeGenerationStage(),
-                //new InstructionLogger(),
+                //InstructionLogger.Instance,
             });
-        }
+		}
 
-        public override Stream RequestCodeStream()
-        {
-            LinkerStream stream = base.RequestCodeStream() as LinkerStream;
-            VirtualMemoryStream vms = (VirtualMemoryStream)stream.BaseStream;
-            if (this.Method.Address == IntPtr.Zero)
-                this.Method.Address = new IntPtr(vms.Base.ToInt64() + vms.Position);
-            return stream;
-        }
+		public override Stream RequestCodeStream()
+		{
+			LinkerStream stream = base.RequestCodeStream() as LinkerStream;
+			VirtualMemoryStream vms = (VirtualMemoryStream)stream.BaseStream;
+			if (this.Method.Address == IntPtr.Zero)
+				this.Method.Address = new IntPtr(vms.Base.ToInt64() + vms.Position);
+			return stream;
+		}
 
-        /// <summary>
-        /// Called after the method compiler has finished compiling the method.
-        /// </summary>
-        protected override void EndCompile()
-        {
-            // If we're compiling a type initializer, run it immediately.
-            MethodAttributes attrs = MethodAttributes.SpecialName | MethodAttributes.RTSpecialName | MethodAttributes.Static;
-            if ((this.Method.Attributes & attrs) == attrs && this.Method.Name == ".cctor")
-            {
-                CCtor cctor = (CCtor)Marshal.GetDelegateForFunctionPointer(this.Method.Address, typeof(CCtor));
-                this.assemblyCompiler.QueueCCtorForInvocationAfterCompilation(cctor);
-            }
+		/// <summary>
+		/// Called after the method compiler has finished compiling the method.
+		/// </summary>
+		protected override void EndCompile()
+		{
+			// If we're compiling a type initializer, run it immediately.
+			MethodAttributes attrs = MethodAttributes.SpecialName | MethodAttributes.RTSpecialName | MethodAttributes.Static;
+			if ((this.Method.Attributes & attrs) == attrs && this.Method.Name == ".cctor")
+			{
+				CCtor cctor = (CCtor)Marshal.GetDelegateForFunctionPointer(this.Method.Address, typeof(CCtor));
+				this.assemblyCompiler.QueueCCtorForInvocationAfterCompilation(cctor);
+			}
 
-            base.EndCompile();
-        }
-    }
+			base.EndCompile();
+		}
+	}
 }
