e269fd510ea24a77ffb8352485b29ceca4382b35
tgiphil <phil@thinkedge.com>
2010-06-09 00:27:35 -0700

    - Updated RunTest.bat script to work with Gallio installation on either the x86 or x64

--- Tests/RunTests.bat
+++ Tests/RunTests.bat
@@ -1,4 +1,8 @@
-"%ProgramFiles%\Gallio\bin\Gallio.Echo.exe" /rnf:Tests /rt:Xml-Inline /report-directory:reports ..\bin\Test.Mosa.Runtime.CompilerFramework.dll
+IF EXIST "%ProgramFiles%\Gallio\bin\Gallio.Echo.exe" SET GALLIO="%ProgramFiles%\Gallio\bin\Gallio.Echo.exe"
+IF EXIST "%ProgramFiles(x86)%\Gallio\bin\Gallio.Echo.exe" SET GALLIO="%ProgramFiles(x86)%\Gallio\bin\Gallio.Echo.exe"
+
+
+%GALLIO% /rnf:Tests /rt:Xml-Inline /report-directory:reports ..\bin\Test.Mosa.Runtime.CompilerFramework.dll
 
 CALL ExtractResults.BAT
 
