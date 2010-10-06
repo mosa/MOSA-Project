1f7daa7a26e109d9389e2c3effa25c99b67483d0
Simon Wollwage <kintaro@think-in-co.de>
2010-10-06 20:51:37 +0200

    - Added Mosa.Runtime.Runtime.Throw to VmCalls in TestAssemblyLinker

--- Tests/Runtime/CompilerFramework/BaseCode/TestAssemblyLinker.cs
+++ Tests/Runtime/CompilerFramework/BaseCode/TestAssemblyLinker.cs
@@ -43,6 +43,8 @@ namespace Test.Mosa.Runtime.CompilerFramework.BaseCode
 
 		private readonly AllocateObjectDelegate allocateObjectHandler;
 
+        private readonly ThrowDelegate throwHandler;
+
 		#endregion // Data members
 
 		#region Construction
@@ -59,6 +61,7 @@ namespace Test.Mosa.Runtime.CompilerFramework.BaseCode
 
 			this.allocateArrayHandler = new AllocateArrayDelegate(global::Mosa.Runtime.Runtime.AllocateArray);
 			this.allocateObjectHandler = new AllocateObjectDelegate(global::Mosa.Runtime.Runtime.AllocateObject);
+            this.throwHandler = new ThrowDelegate(global::Mosa.Runtime.Runtime.Throw);
 		}
 
 		#endregion // Construction
@@ -191,6 +194,9 @@ namespace Test.Mosa.Runtime.CompilerFramework.BaseCode
 		[UnmanagedFunctionPointer(System.Runtime.InteropServices.CallingConvention.Cdecl)]
 		private unsafe delegate void* AllocateArrayDelegate(void* methodTable, uint elementSize, uint elements);
 
+        [UnmanagedFunctionPointer(System.Runtime.InteropServices.CallingConvention.Cdecl)]
+        private unsafe delegate void ThrowDelegate(object exception);
+
 		protected override unsafe void AddVmCalls(IDictionary<string, LinkerSymbol> virtualMachineCalls)
 		{
 			Trace.WriteLine(@"TestAssemblyLinker adding VM calls:");
@@ -214,6 +220,16 @@ namespace Test.Mosa.Runtime.CompilerFramework.BaseCode
 			symbol = new LinkerSymbol(allocateObjectMethod, SectionKind.Text, virtualAddress);
 			symbol.VirtualAddress = new IntPtr(symbol.SectionAddress);
 			virtualMachineCalls.Add(allocateObjectMethod, symbol);
+
+            IntPtr throwDelegate = Marshal.GetFunctionPointerForDelegate(this.throwHandler);
+
+            const string throwMethod = @"Mosa.Runtime.Runtime.Throw(Object exception)";
+            virtualAddress = throwDelegate.ToInt64();
+            Trace.WriteLine(String.Format("\t{0} at 0x{1:x08}", throwMethod, virtualAddress));
+
+            symbol = new LinkerSymbol(throwMethod, SectionKind.Text, virtualAddress);
+            symbol.VirtualAddress = new IntPtr(symbol.SectionAddress);
+            virtualMachineCalls.Add(throwMethod, symbol);
 		}
 
 		/// <summary>
