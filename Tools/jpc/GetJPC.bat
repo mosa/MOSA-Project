7cb4a6c950a0aa91ac3dbdfa82a71e8123d94184
tgiphil <phil@thinkedge.com>
2010-10-08 19:32:35 -0700

    - Updated JPC location

--- Tools/jpc/GetJPC.bat
+++ Tools/jpc/GetJPC.bat
@@ -1,2 +1,2 @@
-if not exist JPCDebugger.jar ..\wget\wget http://www-jpc.physics.ox.ac.uk/jars/JPCDebugger.jar
-if not exist JPCApplication.jar ..\wget\wget http://www-jpc.physics.ox.ac.uk/jars/JPCApplication.jar
+if not exist JPCDebugger.jar ..\wget\wget http://sourceforge.net/projects/jpc/files/releases/2.4.1/JPCApplication.jar/download
+if not exist JPCApplication.jar ..\wget\wget https://sourceforge.net/projects/javapc/files/releases/2.4/JPCDebugger.jar/download
