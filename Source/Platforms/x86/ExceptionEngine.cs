a69b41334e787f7b17a777d85aed4b76803a01db
Simon Wollwage <kintaro@think-in-co.de>
2010-10-19 18:24:35 +0200

    - Added comments to Exception Engine

--- Source/Platforms/x86/ExceptionEngine.cs
+++ Source/Platforms/x86/ExceptionEngine.cs
@@ -14,42 +14,42 @@ using System.Text;
 
 namespace Mosa.Platforms.x86
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    public static class ExceptionEngine
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="eax"></param>
-        /// <param name="ebx"></param>
-        /// <param name="ecx"></param>
-        /// <param name="edx"></param>
-        /// <param name="esi"></param>
-        /// <param name="edi"></param>
-        /// <param name="ebp"></param>
-        /// <param name="exception"></param>
-        /// <param name="eip"></param>
-        /// <param name="esp"></param>
-        public static void ThrowException(uint eax, uint ebx, uint ecx, uint edx, uint esi, uint edi, uint ebp, Exception exception, uint eip, uint esp)
-        {
-            // Read callee's EIP from method header
-            eip = Native.GetEip();
-            // Create context
-            RegisterContext registerContext = new RegisterContext(eax, ebx, ecx, edx, esi, edi, ebp, eip, esp + 40);
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class ExceptionEngine
+	{
+		/// <summary>
+		/// Saves the context and handles the excpetion.
+		/// </summary>
+		/// <param name="eax">Current status of eax</param>
+		/// <param name="ebx">Current status of ebx</param>
+		/// <param name="ecx">Current status of ecx</param>
+		/// <param name="edx">Current status of edx</param>
+		/// <param name="esi">Current status of esi</param>
+		/// <param name="edi">Current status of edi</param>
+		/// <param name="ebp">Current status of ebp</param>
+		/// <param name="exception">The exception object</param>
+		/// <param name="eip">Current status of eip</param>
+		/// <param name="esp">Current status of esp</param>
+		public static void ThrowException(uint eax, uint ebx, uint ecx, uint edx, uint esi, uint edi, uint ebp, Exception exception, uint eip, uint esp)
+		{
+			// Read callee's EIP from method header
+			eip = Native.GetEip();
+			// Create context
+			RegisterContext registerContext = new RegisterContext(eax, ebx, ecx, edx, esi, edi, ebp, eip, esp + 40);
 
-            // Return after exception has been handled
-            RestoreContext(registerContext);
-        }
+			// Return after exception has been handled
+			RestoreContext(registerContext);
+		}
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="context"></param>
-        public static void RestoreContext(RegisterContext context)
-        {
-            Native.RestoreContext();
-        }
-    }
+		/// <summary>
+		/// Restores the context by loading the values from the given context.
+		/// </summary>
+		/// <param name="context">The register context to restore the state from</param>
+		public static void RestoreContext(RegisterContext context)
+		{
+			Native.RestoreContext();
+		}
+	}
 }
