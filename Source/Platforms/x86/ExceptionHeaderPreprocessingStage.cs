56bb7335e1b3a6da770f38499a4e8437e9ec3789
Simon Wollwage <kintaro@think-in-co.de>
2010-10-21 20:36:22 +0200

    - Worked on exception clause preprocessing

--- Source/Platforms/x86/ExceptionHeaderPreprocessingStage.cs
+++ Source/Platforms/x86/ExceptionHeaderPreprocessingStage.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
+using Mosa.Runtime.CompilerFramework.CIL;
 using Mosa.Runtime.CompilerFramework.Operands;
 using Mosa.Runtime.Metadata;
 using Mosa.Runtime.CompilerFramework;
@@ -27,10 +28,15 @@ namespace Mosa.Platforms.x86
 	public sealed class ExceptionHeaderPreprocessingStage : BaseMethodCompilerStage, IMethodCompilerStage, IPlatformStage, IPipelineStage
 	{
 		/// <summary>
+		/// 
+		/// </summary>
+		private Dictionary<int, EhClause> labelMapping = new Dictionary<int, EhClause>();
+		/// <summary>
 		/// Runs the specified method compiler.
 		/// </summary>
 		public void Run()
 		{
+			ProcessExceptionClauseLabels();
 			// Iterate all blocks and preprocess them
 			foreach (BasicBlock block in BasicBlocks)
 				ProcessBlock(block);
@@ -39,10 +45,42 @@ namespace Mosa.Platforms.x86
 		/// <summary>
 		/// 
 		/// </summary>
+		private void ProcessExceptionClauseLabels()
+		{
+			this.labelMapping.Clear();
+
+			foreach (EhClause clause in this.MethodCompiler.Method.ExceptionClauseHeader.Clauses)
+			{
+				AddClauseLabels(clause);
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="clause"></param>
+		private void AddClauseLabels(EhClause clause)
+		{
+			this.labelMapping.Add(clause.TryOffset, clause);
+			this.labelMapping.Add(clause.TryEnd, clause);
+			this.labelMapping.Add(clause.HandlerOffset, clause);
+			this.labelMapping.Add(clause.HandlerEnd, clause);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
 		/// <param name="block"></param>
 		private void ProcessBlock(BasicBlock block)
 		{
-			Context context = CreateContext(block);
+			for (Context context = CreateContext(block); !context.EndOfInstruction; context.GotoNext())
+			{
+				if (this.labelMapping.ContainsKey(context.Label))
+				{
+					// TODO:
+					// Add patch to method
+				}
+			}
 		}
 	}
 }
