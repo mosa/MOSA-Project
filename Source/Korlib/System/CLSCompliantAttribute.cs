f36848973b96f76d49c5ccdd50c6b51cdc516010
tgiphil <phil@thinkedge.com>
2010-11-13 11:19:37 -0800

    - Added CLSCompliantAttribute to Korlib

---
+++ Source/Korlib/System/CLSCompliantAttribute.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * (c) 2008 MOSA - The Managed Operating System Alliance
+ *
+ * Licensed under the terms of the New BSD License.
+ *
+ * Authors:
+ *  Phil Garcia (tgiphil) <phil@thinkedge.com>
+ */
+
+using System.Runtime.InteropServices;
+
+namespace System
+{
+	/// <summary>
+	/// Implementation of the "CLSCompliantAttribute" class.
+	/// </summary>
+	public sealed class CLSCompliantAttribute : Attribute
+	{
+		bool is_compliant;
+
+		public CLSCompliantAttribute(bool isCompliant)
+		{
+			this.is_compliant = isCompliant;
+		}
+
+		public bool IsCompliant
+		{
+			get
+			{
+				return is_compliant;
+			}
+		}
+	}
+}
