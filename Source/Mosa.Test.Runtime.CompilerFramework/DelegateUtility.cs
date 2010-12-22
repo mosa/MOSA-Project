60bcd7ba7acd5a32d3338e65ed1a9e76ecafb850
tgiphil <phil@thinkedge.com>
2010-12-22 12:06:38 -0800

    - Expanding test suite

--- Source/Mosa.Test.Runtime.CompilerFramework/DelegateUtility.cs
+++ Source/Mosa.Test.Runtime.CompilerFramework/DelegateUtility.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+* (c) 2008 MOSA - The Managed Operating System Alliance
+*
+* Licensed under the terms of the New BSD License.
+*
+* Authors:
+*  Phil Garcia (tgiphil) <phil@thinkedge.com>
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ Source/Mosa.Test.Suite/Simple.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Mosa.Test.Suite
+{
+	public static class Simple
+	{
+		public static bool ReturnTrue() { return true; }
+
+		public static bool ReturnFalse() { return false; }
+	}
+}
