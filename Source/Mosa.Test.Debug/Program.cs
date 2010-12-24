13707188a64a2088fdf378a360dbf58b50dbdad6
tgiphil <phil@thinkedge.com>
2010-12-24 10:37:44 -0800

    - Added a Mosa.Test.Debug project to assist (me) with testing the Test Framework.

---
+++ Source/Mosa.Test.Debug/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Mosa.Test.Runtime.CompilerFramework.IL;
+
+namespace Mosa.Test.Debug
+{
+	class Program
+	{
+		public static void Main()
+		{
+			Add add = new Add();
+
+			add.AddU4(0x32, 0x32);
+
+			return;
+		}
+	}
+}
