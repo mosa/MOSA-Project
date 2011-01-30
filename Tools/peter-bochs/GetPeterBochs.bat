f64c087b089273f466e0f4dd3b22b2dc1b5cfad1
tgiphil <phil@thinkedge.com>
2011-01-30 15:04:32 +0800

    - Updated tools

--- Tools/peter-bochs/GetPeterBochs.bat
+++ Tools/peter-bochs/GetPeterBochs.bat
@@ -1,9 +1,7 @@
-if exist peter-bochs-debugger20101110.jar goto exit
 
-del peter-bochs-debugger*.jar
+if not exist peter-bochs-debugger20110127.jar ..\wget\wget http://peter-bochs.googlecode.com/files/peter-bochs-debugger20110127.jar
+if not exist peter-bochs-debugger20110127.jar ..\wget\wget http://download.mosa-project.org/peter-bochs-debugger20110127.jar
 
-..\wget\wget http://peter-bochs.googlecode.com/files/peter-bochs-debugger20101110.jar
-
-copy peter-bochs-debugger20101110.jar peter-bochs-debugger.jar
+copy peter-bochs-debugger20110127.jar peter-bochs-debugger.jar
 
 :exit
