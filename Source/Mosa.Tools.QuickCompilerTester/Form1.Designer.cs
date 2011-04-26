59d349ca7c5151c0d6d5c9691a4cea980be173de
davidleon <davidl@126.com>
2011-04-27 03:16:40 +1000

    added compilation visualizer and quick viewer of a on-the-fly compiler generated compilation stages for a piece of C# code.

---
+++ Source/Mosa.Tools.QuickCompilerTester/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Mosa.Tools.QuickCompilerTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(521, 340);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Check Compile Stage Results";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 394);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Source/Mosa.Tools.TypeExplorer/StringLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mosa.Runtime.CompilerFramework;
+
+namespace Mosa.Tools.TypeExplorer
+{
+	class StringLogger : ILoggerWriter
+	{
+		List<string> sb;
+		int indentlength = 0;
+		public StringLogger()
+		{
+			sb = new List<string>();
+		}
+		public void WriteLine(string s)
+		{
+			sb.Add(new string(' ', indentlength) + s);
+		}
+		public void Indent()
+		{
+			indentlength += 4;
+		}
+		public void Unindent()
+		{
+			indentlength -= 4;
+		}
+		public string[] GetFullLog()
+		{
+			return sb.ToArray();
+		}
+	}
+}
