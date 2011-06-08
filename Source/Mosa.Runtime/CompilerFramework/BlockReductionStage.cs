cd7e5437b24e887fb61e702da34f5f50cb3c1350
tgiphil <phil@thinkedge.com>
2011-06-07 21:15:35 -0700

    - Fixed minor comment error

--- Source/Mosa.Runtime/CompilerFramework/BlockReductionStage.cs
+++ Source/Mosa.Runtime/CompilerFramework/BlockReductionStage.cs
@@ -1,11 +1,11 @@
-///*
-// * (c) 2008 MOSA - The Managed Operating System Alliance
-// *
-// * Licensed under the terms of the New BSD License.
-// *
-// * Authors:
-// *  Phil Garcia (tgiphil) <phil@thinkedge.com>
-// */
+/*
+ * (c) 2008 MOSA - The Managed Operating System Alliance
+ *
+ * Licensed under the terms of the New BSD License.
+ *
+ * Authors:
+ *  Phil Garcia (tgiphil) <phil@thinkedge.com>
+ */
 
 //using System;
 //using System.Collections;
