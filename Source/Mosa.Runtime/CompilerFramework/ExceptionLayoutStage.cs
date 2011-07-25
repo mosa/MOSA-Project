afe9be19c45fe7fcd18a2e902e88f5cc36134f95
tgiphil <phil@thinkedge.com>
2011-07-24 22:58:15 -0700

    - Implementing ExceptionLayoutStage (incomplete)

--- Source/Mosa.Runtime/CompilerFramework/ExceptionLayoutStage.cs
+++ Source/Mosa.Runtime/CompilerFramework/ExceptionLayoutStage.cs
@@ -8,8 +8,11 @@
  */
 
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
+
+using Mosa.Compiler.Linker;
 
 using CIL = Mosa.Runtime.CompilerFramework.CIL;
 
@@ -19,7 +22,10 @@ namespace Mosa.Runtime.CompilerFramework
 	{
 		#region Data members
 
-		private List<Pair> sortedClauses;
+		private List<ExceptionClauseNode> sortedClauses;
+
+		private Dictionary<BasicBlock, ExceptionClause> exceptionBlocks;
+		private Dictionary<int, ExceptionClause> exceptionLabelMap;
 
 		#endregion // Data members
 
@@ -40,34 +46,110 @@ namespace Mosa.Runtime.CompilerFramework
 		/// </summary>
 		public void Run()
 		{
+			// Step 1 - Sort the exception clauses into postorder-traversal
 			BuildSort();
+
+			// Step 2 - Assign blocks to innermost exception clause
+			AssignBlockstoClauses();
+
+			// Step 3 - Emit table of PC ranges and the clause handler
+			EmitExceptionTable();
 		}
 
 		#endregion // IMethodCompilerStage members
 
-		#region Sort Class and Methods
-		
-		private class Pair : IComparable<Pair>
+		private void AssignBlockstoClauses()
+		{
+			BuildExceptionLabelMap();
+
+			exceptionBlocks = new Dictionary<BasicBlock, ExceptionClause>();
+
+			Stack<int> exceptionLabelStack = new Stack<int>();
+
+			// Main Code
+			Trace(-1, exceptionLabelStack);
+
+			// Handler Code
+			foreach (ExceptionClause clause in methodCompiler.ExceptionClauseHeader.Clauses)
+			{
+				Trace(clause.HandlerOffset, exceptionLabelStack);
+				exceptionLabelStack.Clear();
+			}
+		}
+
+		private void BuildExceptionLabelMap()
+		{
+			exceptionLabelMap = new Dictionary<int, ExceptionClause>();
+
+			foreach (ExceptionClause clause in methodCompiler.ExceptionClauseHeader.Clauses)
+				exceptionLabelMap.Add(clause.TryOffset, clause);
+		}
+
+		private void Trace(int label, Stack<int> exceptionLabelStack)
+		{
+			ExceptionClause newTraceClause = null;
+			bool newTrace = exceptionLabelMap.TryGetValue(label, out newTraceClause);
+
+			// TODO
+
+			// Note: if newTrace is true, assign block to new clause
+			// if clause ends, pop from exception label stack
+			// how do we know a clause ends?
+		}
+
+		private void EmitExceptionTable()
+		{
+			// TODO:
+
+			List<string> entries = new List<string>();
+
+			foreach (ExceptionClauseNode pair in sortedClauses)
+			{
+				ExceptionClause clause = pair.Clause;
+
+				// foreach(BasicBlock block in clause)
+				// {
+
+				// 1. Start 
+				// 2. End
+				// 3. Handler
+
+				// }
+			}
+
+
+			int tableSize = entries.Count * nativePointerSize;
+
+			using (Stream stream = methodCompiler.Linker.Allocate(this.methodCompiler.Method.FullName + @"$etable", SectionKind.Text, tableSize, nativePointerAlignment))
+			{
+				stream.Position = tableSize;
+			}
+
+		}
+
+		#region Postorder-traversal Sort
+
+		private class ExceptionClauseNode : IComparable<ExceptionClauseNode>
 		{
 			public ExceptionClause Clause { get; private set; }
 
 			public int Start { get { return Clause.TryOffset; } }
 			public int Length { get { return Clause.TryLength; } }
 
-			public Pair Parent;
-			public List<Pair> Children;
+			public ExceptionClauseNode Parent;
+			public List<ExceptionClauseNode> Children;
 
-			public Pair(ExceptionClause clause)
+			public ExceptionClauseNode(ExceptionClause clause)
 			{
 				Clause = clause;
 			}
 
-			public bool IsInside(Pair pair)
+			public bool IsInside(ExceptionClauseNode pair)
 			{
 				return this.Start > pair.Start && (this.Start + this.Length < pair.Start + pair.Length);
 			}
 
-			public int CompareTo(Pair other)
+			public int CompareTo(ExceptionClauseNode other)
 			{
 				return this.Start.CompareTo(other.Start);
 			}
@@ -76,15 +158,15 @@ namespace Mosa.Runtime.CompilerFramework
 
 		private void BuildSort()
 		{
-			sortedClauses = new List<Pair>(methodCompiler.ExceptionClauseHeader.Clauses.Count);
+			sortedClauses = new List<ExceptionClauseNode>(methodCompiler.ExceptionClauseHeader.Clauses.Count);
 
 			foreach (ExceptionClause clause in methodCompiler.ExceptionClauseHeader.Clauses)
-				sortedClauses.Add(new Pair(clause));
+				sortedClauses.Add(new ExceptionClauseNode(clause));
 		}
 
-		private List<Pair> Sort(List<Pair> listToSort)
+		private List<ExceptionClauseNode> Sort(List<ExceptionClauseNode> listToSort)
 		{
-			var result = new List<Pair>();
+			var result = new List<ExceptionClauseNode>();
 			listToSort.Sort();
 			foreach (var pair in listToSort)
 			{
@@ -98,23 +180,23 @@ namespace Mosa.Runtime.CompilerFramework
 			return result;
 		}
 
-		private void InsertChildren(List<Pair> result, Pair root)
+		private void InsertChildren(List<ExceptionClauseNode> result, ExceptionClauseNode root)
 		{
 			result.InsertRange(result.IndexOf(root), root.Children);
 			foreach (var pair in root.Children)
 				InsertChildren(result, pair);
 		}
 
-		private List<Pair> GetChildren(Pair pair, List<Pair> list)
+		private List<ExceptionClauseNode> GetChildren(ExceptionClauseNode pair, List<ExceptionClauseNode> list)
 		{
-			var children = new List<Pair>();
+			var children = new List<ExceptionClauseNode>();
 			foreach (var other in list)
 			{
 				if (other.IsInside(pair))
 					children.Add(other);
 			}
 
-			var result = new List<Pair>(children);
+			var result = new List<ExceptionClauseNode>(children);
 			foreach (var x in children)
 			{
 				foreach (var y in children)
