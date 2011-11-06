df8d84309513cd1d389058712df58fb73169577d
tgiphil <phil@thinkedge.com>
2011-11-06 11:56:42 -0800

    - Cleaning up project

---
+++ Source/Mosa.Test.Korlib/KernelMemory.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * (c) 2008 MOSA - The Managed Operating System Alliance
+ *
+ * Licensed under the terms of the New BSD License.
+ *
+ * Authors:
+ *  Phil Garcia (tgiphil) <phil@thinkedge.com>
+ */
+
+
+namespace Mosa.Kernel
+{
+	public class KernelMemory
+	{
+		const uint memoryPtr = 0x21700000;	// Location for pointer to allocated memory!
+
+		static unsafe public uint AllocateMemory(uint size)
+		{
+			uint alloc = ((uint*)memoryPtr)[0];
+			((uint*)memoryPtr)[0] = alloc + size;
+ 
+			return alloc;
+		}
+
+	}
+}
