c53c99453b00d6e4e16e4f7760ac371b170b4a6f
tgiphil <phil@thinkedge.com>
2011-11-06 21:58:25 -0800

    - Moved Mosa.Tools.XmlTo

---
+++ Source/Mosa.Tools.XmlTo/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+* (c) 2008 MOSA - The Managed Operating System Alliance
+*
+* Licensed under the terms of the New BSD License.
+*
+* Authors:
+*  Phil Garcia (tgiphil) <phil@thinkedge.com>
+*/
+
+using System;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+
+namespace Mosa.Tools.XmlTo
+{
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			Console.WriteLine();
+			Console.WriteLine("XMLTo v0.1 [www.mosa-project.org]");
+			Console.WriteLine("Copyright 2010. New BSD License.");
+			Console.WriteLine("Written by Philipp Garcia (phil@thinkedge.com)");
+			Console.WriteLine();
+
+			if (args.Length < 3) {
+				Console.WriteLine("Usage: XMLTo <xml file> <xsl file> <output file>");
+				Console.Error.WriteLine("ERROR: Missing arguments");
+				return -1;
+			}
+
+			try {
+				XPathDocument myXPathDoc = new XPathDocument(args[0]);
+				XslCompiledTransform myXslTrans = new XslCompiledTransform();
+				myXslTrans.Load(args[1]);
+				XmlTextWriter myWriter = new XmlTextWriter(args[2], null);
+				myXslTrans.Transform(myXPathDoc, null, myWriter);
+
+				return 0;
+			}
+			catch (Exception e) {
+				Console.Error.WriteLine("Exception: {0}", e.ToString());
+				return -1;
+			}
+
+		}
+	}
+}
