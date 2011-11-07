f67e264cc3469260edbde31408f5581321d7f71d
tgiphil <phil@thinkedge.com>
2011-11-06 22:06:38 -0800

    - Added two new projects stubs: Mosa.Tools.Verifier and Mosa.Compiler.Verifier

---
+++ Source/Mosa.Tools.Verifier/Program.cs
@@ -0,0 +1,27 @@
+/*
+ * (c) 2011 MOSA - The Managed Operating System Alliance
+ *
+ * Licensed under the terms of the New BSD License.
+ *
+ * Authors:
+ *  Phil Garcia (tgiphil) <phil@thinkedge.com>
+ */
+
+namespace Mosa.Tools.Verifier
+{
+	/// <summary>
+	/// Class containing the entry point of the program.
+	/// </summary>
+	internal static class Program
+	{
+		/// <summary>
+		/// Main entry point for the compiler.
+		/// </summary>
+		/// <param name="args">The command line arguments.</param>
+		internal static void Main(string[] args)
+		{
+			// TODO
+		}
+	}
+}
+
