725d010090d8dd70e118d8fc9bc0ae891f31bffb
tgiphil <phil@thinkedge.com>
2012-02-17 02:05:08 -0800

    - Updated build scripts: FAT16 (instead of FAT12) and large virtual disks (can hold COSMOS images now)

--- Run/Compile.bat
+++ Run/Compile.bat
@@ -7,6 +7,6 @@ del /q /f build\hello.exe
 
 cd build
 
-..\..\bin\mosacl.exe -a=x86 -f=PE --pe-file-alignment=4096 --map=hello.map -b=mb0.7 -sa -o ..\build\hello.exe ..\..\bin\%1
+..\..\bin\mosacl.exe -a=x86 -f=PE --pe-file-alignment=4096 --map=hello.map -b=mb0.7 -sa -o ..\build\main.exe ..\..\bin\%1
 
 cd ..
