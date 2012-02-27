2999d56564635ee7b8c7155842d5e010e98b2c08
tgiphil <phil@thinkedge.com>
2012-02-26 21:39:36 -0800

    - Added platform projects for AVR32, ARM, x64 for future use - Updated Peter Bochs - Removed unused stages from project

--- Tools/peter-bochs/GetPeterBochs.bat
+++ Tools/peter-bochs/GetPeterBochs.bat
@@ -1,7 +1,7 @@
 
-if not exist peter-bochs-debugger20110614.jar ..\wget\wget http://peter-bochs.googlecode.com/files/peter-bochs-debugger20110614.jar
-if not exist peter-bochs-debugger20110614.jar ..\wget\wget http://download.mosa-project.org/peter-bochs-debugger20110614.jar
+if not exist peter-bochs-debugger20111224.jar ..\wget\wget http://peter-bochs.googlecode.com/files/peter-bochs-debugger20111224.jar
+if not exist peter-bochs-debugger20111224.jar ..\wget\wget http://download.mosa-project.org/peter-bochs-debugger20111224.jar
 
-copy peter-bochs-debugger20110614.jar peter-bochs-debugger.jar
+copy peter-bochs-debugger20111224.jar peter-bochs-debugger.jar
 
 :exit
