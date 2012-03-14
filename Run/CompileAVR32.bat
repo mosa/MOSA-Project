b8ec7138f2ef816522ebda7c9a401bf32d56a99c
Kai Patrick Reisert <kpreisert@googlemail.com>
2012-03-15 02:45:23 +0800

    Fixed last commit

--- Run/CompileAVR32.bat
+++ Run/CompileAVR32.bat
@@ -7,6 +7,6 @@ del /q /f build\main.exe
 
 cd build
 
-..\..\bin\mosacl.exe -a=avr32 -f=ELF32 --elf-file-alignment=4096 --map=hello.map -b=null -sa -o ..\build\main.exe ..\..\bin\%1
+..\..\bin\mosacl.exe -a=avr32 -f=ELF32 --elf-file-alignment=4096 --map=hello.map -sa -o ..\build\main.exe ..\..\bin\%1
 
 cd ..
