36e9e6b3c7e9dc8e60cbae42bd0f3e3465da55ad
Phil Garcia <phil@thinkedge.com>
2013-08-08 07:27:33 -0700

    - Updated test cases

--- Source/Mosa.Test.Cases/CIL/IsInstFixture.cs
+++ Source/Mosa.Test.Cases/CIL/IsInstFixture.cs
@@ -23,105 +23,135 @@ namespace Mosa.Test.Cases.CIL
 		}
 
 		[Test]
-		public void IsInstTest1()
+		public void IsInstAAToAA()
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest1(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest1"));
+			Assert.AreEqual(IsInstTests.IsInstAAToAA(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstAAToAA"));
 		}
 
 		[Test]
-		public void IsInstTest2()
+		public void IsInstBBToAA()
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest2(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest2"));
+			Assert.AreEqual(IsInstTests.IsInstBBToAA(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstBBToAA"));
 		}
 
 		[Test]
-		public void IsInstTest3()
+		public void IsInstCCToAA()
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest3(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest3"));
+			Assert.AreEqual(IsInstTests.IsInstCCToAA(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstCCToAA"));
 		}
 
 		[Test]
-		public void IsInstTest4()
+		public void IsInstCCToBB()
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest4(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest4"));
+			Assert.AreEqual(IsInstTests.IsInstCCToBB(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstCCToBB"));
 		}
 
 		[Test]
-		public void IsInstTest5()
+		public void IsInstDDToAA()
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest5(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest5"));
+			Assert.AreEqual(IsInstTests.IsInstDDToAA(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstDDToAA"));
 		}
 
 		[Test]
-		public void IsInstTest6()
+		public void IsInstDDToBB()
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest6(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest6"));
+			Assert.AreEqual(IsInstTests.IsInstDDToBB(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstDDToBB"));
 		}
 
 		[Test]
-		public void IsInstTest7()
+		public void IsInstDDToCC()
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest7(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest7"));
+			Assert.AreEqual(IsInstTests.IsInstDDToCC(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstDDToCC"));
 		}
 
 		[Test]
-		public void IsInstTest8()
+		public void IsInstAAtoIAA()
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest8(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest8"));
+			Assert.AreEqual(IsInstTests.IsInstAAtoIAA(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstAAtoIAA"));
 		}
 
 		[Test]
-		public void IsInstTest9()
+		public void IsInstBBToIAA()
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest9(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest9"));
+			Assert.AreEqual(IsInstTests.IsInstBBToIAA(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstBBToIAA"));
 		}
 
 		[Test]
-		public void IsInstTest10()
+		public void IsInstCCToIAA()
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest10(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest10"));
+			Assert.AreEqual(IsInstTests.IsInstCCToIAA(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstCCToIAA"));
 		}
 
 		[Test]
-		public void IsInstTest11()
+		public void IsInstCCToIBB()
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest11(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest11"));
+			Assert.AreEqual(IsInstTests.IsInstCCToIBB(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstCCToIBB"));
 		}
 
 		[Test]
-		public void IsInstTest12()
+		public void IsInstI4ToI4([I4] int i)
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest12(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest12"));
+			Assert.AreEqual(IsInstTests.IsInstI4ToI4(i), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstI4ToI4", i));
 		}
 
 		[Test]
-		public void IsInstTest13()
+		public void IsInstU4ToI4()
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest13(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest13"));
+			Assert.AreEqual(IsInstTests.IsInstU4ToI4(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstU4ToI4"));
 		}
 
 		[Test]
-		public void IsInstTest14()
+		public void IsInstI8ToI8()
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest14(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest14"));
+			Assert.AreEqual(IsInstTests.IsInstI8ToI8(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstI8ToI8"));
 		}
 
 		[Test]
-		public void IsInstTest15()
+		public void IsInstU8ToU8()
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest15(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest15"));
+			Assert.AreEqual(IsInstTests.IsInstU8ToU8(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstU8ToU8"));
 		}
 
 		[Test]
-		public void IsInstTest16()
+		public void IsInstI4ToU4()
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest16(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest16"));
+			Assert.AreEqual(IsInstTests.IsInstI4ToU4(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstI4ToU4"));
 		}
 
 		[Test]
-		public void IsInstTest17()
+		public void IsInstI1ToI1()
 		{
-			Assert.AreEqual(IsInstTests.IsInstTest17(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstTest17"));
+			Assert.AreEqual(IsInstTests.IsInstI1ToI1(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstI1ToI1"));
+		}
+
+		[Test]
+		public void IsInstI2ToI2()
+		{
+			Assert.AreEqual(IsInstTests.IsInstI2ToI2(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstI2ToI2"));
+		}
+
+		[Test]
+		public void IsInstU1ToU1()
+		{
+			Assert.AreEqual(IsInstTests.IsInstU1ToU1(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstU1ToU1"));
+		}
+
+		[Test]
+		public void IsInstU2ToU2()
+		{
+			Assert.AreEqual(IsInstTests.IsInstU2ToU2(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstU2ToU2"));
+		}
+
+		[Test]
+		public void IsInstCToC()
+		{
+			Assert.AreEqual(IsInstTests.IsInstCToC(), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstCToC"));
+		}
+
+		[Test]
+		public void IsInstBToB([B] bool b)
+		{
+			Assert.AreEqual(IsInstTests.IsInstBToB(b), Run<bool>("Mosa.Test.Collection", "IsInstTests", "IsInstBToB", b));
 		}
 	}
 }
