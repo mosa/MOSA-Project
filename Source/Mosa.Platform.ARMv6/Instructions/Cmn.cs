b45fcde74f221b05f99bd0b98cc5e6bd0df1bc25
Phil Garcia <phil@thinkedge.com>
2013-10-20 01:38:45 -0700

    - Added ARM v6 instructions

---
+++ Source/Mosa.Platform.ARMv6/Instructions/Cmn.cs
@@ -0,0 +1,57 @@
+/*
+ * (c) 2013 MOSA - The Managed Operating System Alliance
+ *
+ * Licensed under the terms of the New BSD License.
+ *
+ * Authors:
+ *  Phil Garcia (tgiphil) <phil@thinkedge.com>
+ *  Marcelo Caetano (marcelocaetano) <marcelo.caetano@ymail.com>
+ */
+
+using Mosa.Compiler.Framework;
+
+namespace Mosa.Platform.ARMv6.Instructions
+{
+	/// <summary>
+	/// Cmn instruction: Compare Negative
+	/// Sets flags. Like ADD but with no destination register.
+	/// </summary>
+	public class Cmn : ARMv6Instruction
+	{
+		#region Construction
+
+		/// <summary>
+		/// Initializes a new instance of <see cref="Cmn"/>.
+		/// </summary>
+		public Cmn() :
+			base(1, 3)
+		{
+		}
+
+		#endregion Construction
+
+		#region Methods
+
+		/// <summary>
+		/// Emits the specified platform instruction.
+		/// </summary>
+		/// <param name="context">The context.</param>
+		/// <param name="emitter">The emitter.</param>
+		protected override void Emit(Context context, MachineCodeEmitter emitter)
+		{
+			// TODO
+		}
+
+		/// <summary>
+		/// Allows visitor based dispatch for this instruction object.
+		/// </summary>
+		/// <param name="visitor">The visitor object.</param>
+		/// <param name="context">The context.</param>
+		public override void Visit(IARMv6Visitor visitor, Context context)
+		{
+			visitor.Cmn(context);
+		}
+
+		#endregion Methods
+	}
+}
