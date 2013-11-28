889fcf53b7343c846bf7116ab440cac6de10122c
Stefan Andres Charsley <charsleysa@gmail.com>
2013-11-28 21:43:17 +1300

    FIX: problems from merging so many updates

--- Source/Mosa.Kernel.x86/Kernel.cs
+++ Source/Mosa.Kernel.x86/Kernel.cs
@@ -1,74 +1,37 @@
 ﻿/*
-
  * (c) 2008 MOSA - The Managed Operating System Alliance
-
  *
-
  * Licensed under the terms of the New BSD License.
-
  *
-
  * Authors:
-
  *  Phil Garcia (tgiphil) <phil@thinkedge.com>
-
  */
-
-
 
 using Mosa.Kernel.x86.Smbios;
 
-
-
 namespace Mosa.Kernel.x86
-
 {
-
 	/// <summary>
-
 	///
-
 	/// </summary>
-
 	public static class Kernel
-
 	{
-
 		public static void Setup()
-
 		{
-
 			IDT.SetInterruptHandler(null);
-
 			SSE.Setup();
-
 			Multiboot.Setup();
-
 			ProgrammableInterruptController.Setup();
-
 			GDT.Setup();
-
 			IDT.Setup();
-
 			PageFrameAllocator.Setup();
-
 			PageTable.Setup();
-
 			VirtualPageAllocator.Setup();
-
 			//ProcessManager.Setup();
-
 			//TaskManager.Setup();
-
 			SmbiosManager.Setup();
-
 			ConsoleManager.Setup();
-
-
 			//Serial.Setup();
-
 		}
-
 	}
-
 }
