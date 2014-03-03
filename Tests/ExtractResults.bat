6872e7d86424e319ea7f8bf51eeb0fc5162e64a7
Phil Garcia <phil@thinkedge.com>
2014-03-02 20:44:21 -0800

    - Removed old Test files

--- Tests/ExtractResults.bat
+++
@@ -1,3 +0,0 @@
-..\bin\Mosa.Tool.XmlTo.exe Reports\Tests.xml Stylesheets\Gallio-All.xsl reports\Tests.txt
-..\bin\Mosa.Tool.XmlTo.exe Reports\Tests.xml Stylesheets\Gallio-Failed.xsl reports\Failed.txt
-..\bin\Mosa.Tool.XmlTo.exe Reports\Tests.xml Stylesheets\Gallio-Passed.xsl reports\Passed.txt
