4bbe7751197e3b96325599d4db8daca3c106257c
Phil Garcia <phil@thinkedge.com>
2014-05-29 00:05:13 -0700

    - Updated xunit test script (required for .net 4.0)

--- Tests/RunUnitTests.bat
+++ Tests/RunUnitTests.bat
@@ -3,7 +3,7 @@ CD ..\Source
 CALL Compile-Debug.bat
 CD ..\Tests
 
-SET XUNIT="..\Tools\xunit\xunit.console.x86.exe"
+SET XUNIT="..\Tools\xunit\xunit.console.clr4.x86.exe"
 
 %XUNIT% Mosa.UnitTests.xunit
 
