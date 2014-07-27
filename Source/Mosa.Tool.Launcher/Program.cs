7d6fb73a04427c38a1d9beefa631183865d38a96
Phil Garcia <phil@thinkedge.com>
2014-07-26 17:34:21 -0700

    - Started Mosa.Tool.Launcher (GUI application for the compiler)

---
+++ Source/Mosa.Tool.Launcher/Program.cs
@@ -0,0 +1,36 @@
+/*
+ * (c) 2014 MOSA - The Managed Operating System Alliance
+ *
+ * Licensed under the terms of the New BSD License.
+ *
+ * Authors:
+ *  Phil Garcia (tgiphil) <phil@thinkedge.com>
+ */
+
+using System;
+using System.Windows.Forms;
+
+namespace Mosa.Tool.Launcher
+{
+	internal static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			var main = new MainForm();
+
+			if (args.Length != 0)
+			{
+				
+			}
+
+			Application.Run(main);
+		}
+	}
+}
