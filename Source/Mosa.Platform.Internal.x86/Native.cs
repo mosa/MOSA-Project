0a94e6f083fef3563c00b2a6fd3f1dcc80650b5b
Phil Garcia <phil@thinkedge.com>
2014-08-10 20:52:07 -0700

    - Removed GetEip method

--- Source/Mosa.Platform.Internal.x86/Native.cs
+++ Source/Mosa.Platform.Internal.x86/Native.cs
@@ -222,9 +222,6 @@ namespace Mosa.Platform.Internal.x86
 		[DllImportAttribute(@"Mosa.Platform.x86.Intrinsic.Set, Mosa.Platform.x86")]
 		public extern static void Set64(uint address, ulong value);
 
-		[DllImportAttribute(@"Mosa.Platform.x86.Intrinsic.GetEIP, Mosa.Platform.x86")]
-		public extern static uint GetEIP();
-
 		[DllImportAttribute(@"Mosa.Platform.x86.Intrinsic.GetEBP, Mosa.Platform.x86")]
 		public extern static uint GetEBP();
 
