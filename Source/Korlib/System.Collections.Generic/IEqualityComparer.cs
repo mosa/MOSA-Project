ae50b138bd5eb7575d61036b32de13769b08e0c7
Stefan Andres Charsley <charsleysa@gmail.com>
2014-12-18 21:14:02 +1300

    Metadata - Second Iteration (Incomplete)
    - worked on #123

--- Source/Korlib/System.Collections.Generic/IEqualityComparer.cs
+++ Source/Korlib/System.Collections.Generic/IEqualityComparer.cs
@@ -1,10 +1,11 @@
 ﻿/*
- * (c) 2008 MOSA - The Managed Operating System Alliance
+ * (c) 2014 MOSA - The Managed Operating System Alliance
  *
  * Licensed under the terms of the New BSD License.
  *
  * Authors:
  *  Phil Garcia (tgiphil) <phil@thinkedge.com>
+ *  Stefan Andres Charsley (charsleysa) <charsleysa@gmail.com>
  */
 
 namespace System.Collections.Generic
@@ -12,7 +13,21 @@ namespace System.Collections.Generic
 	/// <summary>
 	///
 	/// </summary>
-	internal class IEqualityComparer
+	public interface IEqualityComparer<in T>
 	{
+		/// <summary>
+		/// Determines whether the specified objects are equal.
+		/// </summary>
+		/// <param name="x">The first object of type T to compare.</param>
+		/// <param name="y">The second object of type T to compare.</param>
+		/// <returns>True if the specified objects are equal; otherwise, false.</returns>
+		bool Equals(T x, T y);
+
+		/// <summary>
+		/// Returns a hash code for the specified object.
+		/// </summary>
+		/// <param name="obj">The <see cref="Object"/> for which a hash code is to be returned.</param>
+		/// <returns>A hash code for the specified object.</returns>
+		int GetHashCode(T obj);
 	}
 }
