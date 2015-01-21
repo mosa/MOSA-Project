6bb492d963e2d1e527fba23c29701f9894ed4ba7
Sebatian Loncar <sebastian.loncar@gmail.com>
2015-01-21 15:20:40 +0100

    Added unix paths. Fixed crash on startup, when qemu is not found.

--- Source/Mosa.Utility.Launcher/AppLocations.cs
+++ Source/Mosa.Utility.Launcher/AppLocations.cs
@@ -37,55 +37,67 @@ namespace Mosa.Utility.Launcher
 			{
 				// find QEMU executable
 				QEMU = TryFind(
-					"qemu-system-i386.exe",
+                    new string[] {"qemu-system-i386.exe", "qemu-system-i386"},
 					new string[] {
 					CombineParameterAndDirectory("MOSA",@"Tools\QEMU"),
 					CombineParameterAndDirectory("MOSA",@"QEMU"),
 					@"..\Tools\QEMU",
 					@"Tools\QEMU",
 					CombineParameterAndDirectory("ProgramFiles",@"qemu"),
-					CombineParameterAndDirectory("ProgramFiles(x86)",@"qemu")
+					CombineParameterAndDirectory("ProgramFiles(x86)",@"qemu"),
+                    @"/bin"
 				}
 				);
 			}
 
-			if (string.IsNullOrEmpty(QEMUImg))
-			{
-				QEMUImg = TryFind(
-				   "qemu-img.exe",
-				   new string[] {
-					CombineParameterAndDirectory("MOSA",@"Tools\QEMU"),
-					CombineParameterAndDirectory("MOSA",@"QEMU"),
-					@"..\Tools\QEMU",
-					@"Tools\QEMU",
-					CombineParameterAndDirectory("ProgramFiles",@"qemu"),
-					CombineParameterAndDirectory("ProgramFiles(x86)",@"qemu")
-				}
-					);
-			}
+            if (!string.IsNullOrEmpty(QEMU))
+            {
 
-			if (string.IsNullOrEmpty(QEMUBIOSDirectory))
-			{
-				QEMUBIOSDirectory = Path.GetDirectoryName(
-				TryFind(
-					"bios.bin",
-					new string[] {
-						Path.GetDirectoryName(QEMU),
-						Path.Combine(Path.GetDirectoryName(QEMU),"bios")
-					}
-				)
-			);
-			}
+                if (string.IsNullOrEmpty(QEMUImg))
+                {
+                    QEMUImg = TryFind(
+                        new string[] { "qemu-img.exe", "qemu-img" },
+                        new string[]
+                        {
+                            CombineParameterAndDirectory("MOSA", @"Tools\QEMU"),
+                            CombineParameterAndDirectory("MOSA", @"QEMU"),
+                            @"..\Tools\QEMU",
+                            @"Tools\QEMU",
+                            CombineParameterAndDirectory("ProgramFiles", @"qemu"),
+                            CombineParameterAndDirectory("ProgramFiles(x86)", @"qemu"),
+                            @"/bin"
+                        }
+                    );
+                }
+
+                if (string.IsNullOrEmpty(QEMUBIOSDirectory))
+                {
+                    QEMUBIOSDirectory = Path.GetDirectoryName(
+                        TryFind(
+                            new string[] { "bios.bin" },
+                            new string[]
+                            {
+                                Path.GetDirectoryName(QEMU),
+                                Path.Combine(Path.GetDirectoryName(QEMU), "bios"),
+                                "/usr/share/qemu"
+                            }
+                        )
+                    );
+                }
+
+            }
+
 			if (string.IsNullOrEmpty(NDISASM))
 			{
 				// find NDISMASM
 				NDISASM = TryFind(
-					   "ndisasm.exe",
+                    new string[] {"ndisasm.exe", "ndisasm"},
 					   new string[] {
 					CombineParameterAndDirectory("MOSA",@"Tools\ndisasm"),
 					CombineParameterAndDirectory("MOSA",@"ndisasm"),
 					@"..\Tools\ndisasm",
-					@"Tools\ndisasm"
+					@"Tools\ndisasm",
+                    @"/bin"
 				}
 				);
 			}
@@ -93,7 +105,7 @@ namespace Mosa.Utility.Launcher
 			{
 				// find BOCHS
 				BOCHS = TryFind(
-					"bochs.exe",
+                    new string[] {"bochs.exe", "bochs"},
 					new string[] {
 					CombineParameterAndDirectory("ProgramFiles",@"Bochs-2.6.5"),
 					CombineParameterAndDirectory("ProgramFiles(x86)",@"Bochs-2.6.5"),
@@ -102,8 +114,9 @@ namespace Mosa.Utility.Launcher
 					CombineParameterAndDirectory("MOSA",@"Tools\Bochs"),
 					CombineParameterAndDirectory("MOSA",@"Bochs"),
 					@"..\Tools\Bochs",
-					@"Tools\Bochs"
-				}
+					@"Tools\Bochs",
+                    @"/bin"
+                    }
 				);
 			}
 
@@ -111,7 +124,7 @@ namespace Mosa.Utility.Launcher
 			{
 				// find vmware player
 				VMwarePlayer = TryFind(
-					"vmplayer.exe",
+                    new string[] {"vmplayer.exe", "vmplayer"},
 					new string[] {
 					CombineParameterAndDirectory("ProgramFiles",@"VMware\VMware Player"),
 					CombineParameterAndDirectory("ProgramFiles(x86)",@"VMware\VMware Player")
@@ -123,7 +136,7 @@ namespace Mosa.Utility.Launcher
 			{
 				// find mkisofs
 				mkisofs = TryFind(
-					"mkisofs.exe",
+                    new string[] {"mkisofs.exe", "mkisofs"},
 					new string[] {
 					CombineParameterAndDirectory("ProgramFiles",@"VMware\VMware Player"),
 					CombineParameterAndDirectory("ProgramFiles(x86)",@"VMware\VMware Player"),
@@ -148,15 +161,14 @@ namespace Mosa.Utility.Launcher
 			return Path.Combine(variable, subdirectory);
 		}
 
-		private string TryFind(string file, IList<string> directories)
+        private string TryFind(IList<string> files, IList<string> directories)
 		{
 			string location;
 
 			foreach (var directory in directories)
-			{
-				if (TryFind(file, directory, out location))
-					return location;
-			}
+                foreach (var file in files)
+				    if (TryFind(file, directory, out location))
+					    return location;
 
 			return string.Empty;
 		}
