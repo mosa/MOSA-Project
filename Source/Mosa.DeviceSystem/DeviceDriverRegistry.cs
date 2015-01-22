fde52e94a916b82dece3376a0a39b6f8a9b6c839
Phil Garcia <phil@thinkedge.com>
2015-01-21 22:50:57 -0800

    - Removed Switch usage in DeviceDriverRegitry.cs

--- Source/Mosa.DeviceSystem/DeviceDriverRegistry.cs
+++ Source/Mosa.DeviceSystem/DeviceDriverRegistry.cs
@@ -194,44 +194,26 @@ namespace Mosa.DeviceSystem
 			{
 				var attribute = new PCIDeviceDriverAttribute();
 				foreach (var arg in attributeData.NamedArguments)
-					switch (arg.MemberName)
-					{
-						case "Platforms":
-							attribute.Platforms = (PlatformArchitecture)arg.TypedValue.Value;
-							break;
-
-						case "DeviceID":
-							attribute.DeviceID = (ushort)arg.TypedValue.Value;
-							break;
-
-						case "VendorID":
-							attribute.VendorID = (ushort)arg.TypedValue.Value;
-							break;
-
-						case "SubVendorID":
-							attribute.SubVendorID = (ushort)arg.TypedValue.Value;
-							break;
-
-						case "SubDeviceID":
-							attribute.SubDeviceID = (ushort)arg.TypedValue.Value;
-							break;
-
-						case "RevisionID":
-							attribute.RevisionID = (byte)arg.TypedValue.Value;
-							break;
-
-						case "ProgIF":
-							attribute.ProgIF = (byte)arg.TypedValue.Value;
-							break;
-
-						case "ClassCode":
-							attribute.ClassCode = (ushort)arg.TypedValue.Value;
-							break;
-
-						case "SubClassCode":
-							attribute.SubClassCode = (byte)arg.TypedValue.Value;
-							break;
-					}
+				{
+					if (arg.MemberName == "Platforms")
+						attribute.Platforms = (PlatformArchitecture)arg.TypedValue.Value;
+					else if (arg.MemberName == "DeviceID")
+						attribute.DeviceID = (ushort)arg.TypedValue.Value;
+					else if (arg.MemberName == "VendorID")
+						attribute.VendorID = (ushort)arg.TypedValue.Value;
+					else if (arg.MemberName == "SubVendorID")
+						attribute.SubVendorID = (ushort)arg.TypedValue.Value;
+					else if (arg.MemberName == "SubDeviceID")
+						attribute.SubDeviceID = (ushort)arg.TypedValue.Value;
+					else if (arg.MemberName == "RevisionID")
+						attribute.RevisionID = (byte)arg.TypedValue.Value;
+					else if (arg.MemberName == "ProgIF")
+						attribute.ProgIF = (byte)arg.TypedValue.Value;
+					else if (arg.MemberName == "ClassCode")
+						attribute.ClassCode = (ushort)arg.TypedValue.Value;
+					else if (arg.MemberName == "SubClassCode")
+						attribute.SubClassCode = (byte)arg.TypedValue.Value;
+				}
 				return attribute;
 			}
 		}
@@ -240,24 +222,16 @@ namespace Mosa.DeviceSystem
 		{
 			var attribute = new DeviceDriverPhysicalMemoryAttribute();
 			foreach (var arg in attributeData.NamedArguments)
-				switch (arg.MemberName)
-				{
-					case "MemorySize":
-						attribute.MemorySize = (uint)arg.TypedValue.Value;
-						break;
-
-					case "MemoryAlignment":
-						attribute.MemoryAlignment = (uint)arg.TypedValue.Value;
-						break;
-
-					case "RestrictUnder16M":
-						attribute.RestrictUnder16M = (bool)arg.TypedValue.Value;
-						break;
-
-					case "RestrictUnder4G":
-						attribute.RestrictUnder4G = (bool)arg.TypedValue.Value;
-						break;
-				}
+			{
+				if (arg.MemberName == "MemorySize")
+					attribute.MemorySize = (uint)arg.TypedValue.Value;
+				else if (arg.MemberName == "MemoryAlignment")
+					attribute.MemoryAlignment = (uint)arg.TypedValue.Value;
+				else if (arg.MemberName == "RestrictUnder16M")
+					attribute.RestrictUnder16M = (bool)arg.TypedValue.Value;
+				else if (arg.MemberName == "RestrictUnder4G")
+					attribute.RestrictUnder4G = (bool)arg.TypedValue.Value;
+			}
 			return attribute;
 		}
 	}
