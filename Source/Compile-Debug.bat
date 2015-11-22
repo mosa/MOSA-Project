b438b180d5d2e871644965db46f2011c027e0b74
Phil Garcia <phil@thinkedge.com>
2015-11-21 20:58:12 -0800

    - Updated build scripts

--- Source/Compile-Debug.bat
+++ Source/Compile-Debug.bat
@@ -1,5 +1,5 @@
-if exist %SYSTEMROOT%\Microsoft.NET\Framework\v3.5 set MSBUILDPATH=%SYSTEMROOT%\Microsoft.NET\Framework\v3.5
 if exist %SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319 set MSBUILDPATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319
+if exist "%ProgramFiles(x86)%\MSBuild\14.0\Bin" set MSBUILDPATH="%ProgramFiles(x86)%\MSBuild\14.0\Bin"
 
 set MSBUILD=%MSBUILDPATH%\msbuild.exe
 
