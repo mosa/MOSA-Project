fceef56455dd28217158c14009dd89ebd99d1e91
Phil Garcia <phil@thinkedge.com>
2015-12-05 00:25:11 -0800

    - Re-introduced Exception Handling Tests - Added test compiler factory

--- Source/Mosa.Test.Collection.x86.xUnit/ExceptionHandlingFixture.cs
+++ Source/Mosa.Test.Collection.x86.xUnit/ExceptionHandlingFixture.cs
@@ -4,7 +4,7 @@ using Xunit;
 
 namespace Mosa.Test.Collection.x86.xUnit
 {
-	public class _ExceptionHandlingFixture : X86TestFixture
+	public class ExceptionHandlingFixture : X86TestFixture
 	{
 		[Fact]
 		public void TryFinally1()
