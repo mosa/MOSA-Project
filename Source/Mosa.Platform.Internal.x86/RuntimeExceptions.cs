14eacc9ed623e1087c1dd2f10f1a7a478dd765d0
Stefan Andres Charsley <charsleysa@gmail.com>
2016-01-05 21:59:32 +1300

    Runtime checks

---
+++ Source/Mosa.Platform.Internal.x86/RuntimeExceptions.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+using Mosa.Internal;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Mosa.Platform.Internal.x86
+{
+	public static class RuntimeExceptions
+	{
+		public static void ThrowIndexOutOfRangeException()
+		{
+			throw new IndexOutOfRangeException();
+		}
+	}
+}
