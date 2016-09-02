d0dfe090f4e18d03ef538a4785ba6bff1a36488d
Phil Garcia <phil@thinkedge.com>
2016-09-02 00:24:06 -0700

    - Bug fixes

--- Source/Mosa.Platform.x86/Stages/TweakTransformationStage.cs
+++ Source/Mosa.Platform.x86/Stages/TweakTransformationStage.cs
@@ -14,12 +14,7 @@ namespace Mosa.Platform.x86.Stages
 		{
 			visitationDictionary[X86.Call] = Call;
 			visitationDictionary[X86.Cmp] = Cmp;
-			visitationDictionary[X86.Cvtsd2ss] = Cvtsd2ss;
-			visitationDictionary[X86.Cvtss2sd] = Cvtss2sd;
-			visitationDictionary[X86.Cvttsd2si] = Cvttsd2si;
-			visitationDictionary[X86.Cvttss2si] = Cvttss2si;
 			visitationDictionary[X86.Mov] = Mov;
-			visitationDictionary[X86.Movsx] = Movsx;
 			visitationDictionary[X86.Movzx] = Movzx;
 			visitationDictionary[X86.Sar] = Sar;
 			visitationDictionary[X86.Shl] = Shl;
@@ -44,10 +39,10 @@ namespace Mosa.Platform.x86.Stages
 			if (!destinationOperand.IsCPURegister)
 			{
 				Context before = context.InsertBefore();
-				Operand eax = AllocateVirtualRegister(destinationOperand.Type);
+				Operand v1 = AllocateVirtualRegister(destinationOperand.Type);
 
-				before.SetInstruction(X86.Mov, eax, destinationOperand);
-				context.Operand1 = eax;
+				before.SetInstruction(X86.Mov, v1, destinationOperand);
+				context.Operand1 = v1;
 			}
 		}
 
@@ -57,6 +52,8 @@ namespace Mosa.Platform.x86.Stages
 		/// <param name="context">The context.</param>
 		public void Cmp(Context context)
 		{
+			// fixme: may not be necessary
+
 			Operand left = context.Operand1;
 			Operand right = context.Operand2;
 
@@ -88,70 +85,6 @@ namespace Mosa.Platform.x86.Stages
 		}
 
 		/// <summary>
-		/// Visitation function for <see cref="IX86Visitor.Cvtsd2ss"/> instructions.
-		/// </summary>
-		/// <param name="context">The context.</param>
-		public void Cvtsd2ss(Context context)
-		{
-			Operand result = context.Result;
-
-			if (!result.IsCPURegister)
-			{
-				Operand register = AllocateVirtualRegister(result.Type);
-				context.Result = register;
-				context.AppendInstruction(X86.Movss, InstructionSize.Size32, result, register);
-			}
-		}
-
-		/// <summary>
-		/// Visitation function for <see cref="IX86Visitor.Cvtss2sd" /> instructions.
-		/// </summary>
-		/// <param name="context">The context.</param>
-		public void Cvtss2sd(Context context)
-		{
-			Operand result = context.Result;
-
-			if (!result.IsCPURegister)
-			{
-				Operand register = AllocateVirtualRegister(result.Type);
-				context.Result = register;
-				context.AppendInstruction(X86.Movsd, InstructionSize.Size64, result, register);
-			}
-		}
-
-		/// <summary>
-		/// Visitation function for <see cref="IX86Visitor.Cvttsd2si"/> instructions.
-		/// </summary>
-		/// <param name="context">The context.</param>
-		public void Cvttsd2si(Context context)
-		{
-			Operand result = context.Result;
-
-			if (!result.IsCPURegister)
-			{
-				Operand register = AllocateVirtualRegister(result.Type);
-				context.Result = register;
-				context.AppendInstruction(X86.Mov, result, register);
-			}
-		}
-
-		/// <summary>
-		/// Visitation function for <see cref="IX86Visitor.Cvttss2si"/> instructions.
-		/// </summary>
-		/// <param name="context">The context.</param>
-		public void Cvttss2si(Context context)
-		{
-			Operand result = context.Result;
-			Operand register = AllocateVirtualRegister(result.Type);
-
-			if (!result.IsCPURegister)
-			{
-				context.Result = register;
-				context.AppendInstruction(X86.Mov, result, register);
-			}
-		}
-
-		/// <summary>
 		/// Visitation function for <see cref="IX86Visitor.Mov"/> instructions.
 		/// </summary>
 		/// <param name="context">The context.</param>
@@ -159,31 +92,11 @@ namespace Mosa.Platform.x86.Stages
 		{
 			Debug.Assert(!context.Result.IsResolvedConstant);
 
-			// Convert moves to float moves, if necessary
-			if (context.Result.IsR4)
-			{
-				context.SetInstruction(X86.Movss, InstructionSize.Size32, context.Result, context.Operand1);
-			}
-			else if (context.Result.IsR8)
-			{
-				context.SetInstruction(X86.Movsd, InstructionSize.Size64, context.Result, context.Operand1);
-			}
-			else if (context.Operand1.IsConstant && (context.Result.Type.IsUI1 || context.Result.Type.IsUI2 || context.Result.IsBoolean || context.Result.IsChar))
+			// fixme: may not be necessary
+			if (context.Operand1.IsConstant && (context.Result.Type.IsUI1 || context.Result.Type.IsUI2 || context.Result.IsBoolean || context.Result.IsChar))
 			{
 				// Correct source size of constant based on destination size
 				context.Operand1 = Operand.CreateConstant(context.Result.Type, context.Operand1.ConstantUnsignedLongInteger);
-			}
-		}
-
-		/// <summary>
-		/// Visitation function for <see cref="IX86Visitor.Movsx"/> instructions.
-		/// </summary>
-		/// <param name="context">The context.</param>
-		public void Movsx(Context context)
-		{
-			if (context.Operand1.IsInt || context.Operand1.IsPointer || !context.Operand1.IsValueType)
-			{
-				context.ReplaceInstructionOnly(X86.Mov);
 			}
 		}
 
@@ -193,6 +106,7 @@ namespace Mosa.Platform.x86.Stages
 		/// <param name="context">The context.</param>
 		public void Movzx(Context context)
 		{
+			// fixme: may not be necessary
 			if (context.Operand1.IsInt || context.Operand1.IsPointer || !context.Operand1.IsValueType)
 			{
 				context.ReplaceInstructionOnly(X86.Mov);
@@ -240,6 +154,7 @@ namespace Mosa.Platform.x86.Stages
 			if (context.Operand2.IsByte)
 				return;
 
+			// fixme: may not be necessary
 			context.Operand2 = Operand.CreateConstant(TypeSystem.BuiltIn.U1, context.Operand2.ConstantUnsignedLongInteger);
 		}
 	}
