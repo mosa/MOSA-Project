4cc15046157dab3d539f9caa4f7e1b6dfdc3520e
Phil Garcia <phil@thinkedge.com>
2016-09-25 11:30:50 -0700

    - Updated xunit

--- Source/Mosa.Compiler.Common/Properties/AssemblyInfo.cs
+++ Source/Mosa.Compiler.Common/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mosa.Compiler.Common.Properties")]
+[assembly: AssemblyTitle("Mosa.Compiler.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MOSA Project")]
-[assembly: AssemblyProduct("Mosa.Compiler.Common.Properties")]
+[assembly: AssemblyProduct("Mosa.Compiler.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
