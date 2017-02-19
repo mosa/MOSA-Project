9bf2693de9f0400a3e8ecc0a376ca766dc7a81dd
Phil Garcia <phil@thinkedge.com>
2017-02-18 22:34:27 -0800

    - Started Wizard

--- Source/Mosa.VisualStudio.VSIX/Properties/AssemblyInfo.cs
+++ Source/Mosa.VisualStudio.VSIX/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mosa.VisualStudio.VSIXProject")]
+[assembly: AssemblyTitle("Mosa.VisualStudio.VSIX")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mosa.VisualStudio.VSIXProject")]
+[assembly: AssemblyProduct("Mosa.VisualStudio.VSIX")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
---
+++ Source/Mosa.VisualStudio.VSIX/VSPackage.cs
@@ -0,0 +1,23 @@
+// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+
+namespace Mosa.VisualStudio.VSIXProject
+{
+	[PackageRegistration(UseManagedResourcesOnly = true)]
+	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+	[Guid("d8c4c7d1-b911-4130-bafb-636f2b8b0af4")]
+	public class VSPackage : Package
+	{
+		protected override void Initialize()
+		{
+			base.Initialize();
+
+			// Place additional initialization code here
+		}
+	}
+}
