ceea31e1b39ff3e3f2f0a0cd972c7f3ff9593144
Phil Garcia <phil@thinkedge.com>
2017-02-19 17:12:20 -0800

    - Updated nuget libraries

--- Tests/RunAllUnitTests.bat
+++ Tests/RunAllUnitTests.bat
@@ -1,7 +1,7 @@
 
 cd ..\bin
 
-SET XUNIT="..\Source\packages\xunit.runner.console.2.2.0-beta5-build3474\tools\xunit.console.x86.exe"
+SET XUNIT="..\Source\packages\xunit.runner.console.2.2.0-rc4-build3536\tools\xunit.console.x86.exe"
 
 echo %DATE% %TIME%
 
