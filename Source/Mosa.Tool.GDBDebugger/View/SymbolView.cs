efbe2b25c7bb868570e36952c5fb1b8ab1032c2e
robot9706 <robot9706@gmail.com>
2017-07-02 15:21:22 +0200

    Merge

--- Source/Mosa.Tool.GDBDebugger/View/SymbolView.cs
+++ Source/Mosa.Tool.GDBDebugger/View/SymbolView.cs
@@ -9,144 +9,142 @@ using System.Windows.Forms;
 
 namespace Mosa.Tool.GDBDebugger.View
 {
-	public partial class SymbolView : DebugDockContent
-	{
-		private List<SymbolEntry> symbols;
+    public partial class SymbolView : DebugDockContent
+    {
+        private class SymbolEntry
+        {
+            public string Name { get { return Symbol.Name; } }
 
-		private class SymbolEntry
-		{
-			public string Name { get { return Symbol.Name; } }
+            [Browsable(false)]
+            public int Size { get { return Symbol.Size; } }
 
-			[Browsable(false)]
-			public int Size { get { return Symbol.Size; } }
+            public string Address { get { return BasePlatform.ToHex(Symbol.Address, Symbol.Size); } }
 
-			public string Address { get { return BasePlatform.ToHex(Symbol.Address, Symbol.Size); } }
+            [Browsable(false)]
+            public string Kind { get { return Symbol.Kind; } }
 
-			[Browsable(false)]
-			public string Kind { get { return Symbol.Kind; } }
+            [Browsable(false)]
+            public Symbol Symbol { get; }
 
-			[Browsable(false)]
-			public Symbol Symbol { get; }
+            public SymbolEntry(Symbol symbol)
+            {
+                Symbol = symbol;
+            }
+        }
 
-			public SymbolEntry(Symbol symbol)
-			{
-				Symbol = symbol;
-			}
-		}
+        public SymbolView(MainForm mainForm)
+            : base(mainForm)
+        {
+            InitializeComponent();
+        }
 
-		public SymbolView(MainForm mainForm)
-			: base(mainForm)
-		{
-			InitializeComponent();
-		}
+        private void SymbolView_Load(object sender, EventArgs e)
+        {
+            toolStripComboBox1.SelectedIndex = 0;
+            toolStripComboBox2.SelectedIndex = 0;
+        }
 
-		private void SymbolView_Load(object sender, EventArgs e)
-		{
-			toolStripComboBox1.SelectedIndex = 0;
-			toolStripComboBox2.SelectedIndex = 0;
-		}
+        public void CreateEntries()
+        {
+            var symbols = new List<SymbolEntry>();
 
-		public void CreateEntries()
-		{
-			var symbols = new List<SymbolEntry>();
+            string filter = toolStripTextBox1.Text.Trim();
+            string kind = toolStripComboBox1.SelectedIndex < 1 ? string.Empty : toolStripComboBox1.SelectedItem.ToString().Trim();
 
-			string filter = toolStripTextBox1.Text.Trim();
-			string kind = toolStripComboBox1.SelectedIndex < 1 ? string.Empty : toolStripComboBox1.SelectedItem.ToString().Trim();
+            uint start = 0;
+            uint end = UInt32.MaxValue;
 
-			uint start = 0;
-			uint end = UInt32.MaxValue;
+            switch (toolStripComboBox2.SelectedIndex)
+            {
+                case 1: start = 1; end = 1; break;
+                case 2: start = 2; end = 2; break;
+                case 3: start = 4; end = 4; break;
+                case 4: start = 8; end = 8; break;
+                default: break;
+            }
 
-			switch (toolStripComboBox2.SelectedIndex)
-			{
-				case 1: start = 1; end = 1; break;
-				case 2: start = 2; end = 2; break;
-				case 3: start = 4; end = 4; break;
-				case 4: start = 8; end = 8; break;
-				default: break;
-			}
+            foreach (var symbol in MainForm.DebugSource.Symbols)
+            {
+                if (!(filter.Length == 0 || symbol.Name.Contains(filter)))
+                    continue;
 
-			foreach (var symbol in MainForm.DebugSource.Symbols)
-			{
-				if (!(filter.Length == 0 || symbol.Name.Contains(filter)))
-					continue;
+                if (kind != string.Empty && symbol.Kind.ToString() != kind)
+                    continue;
 
-				if (kind != string.Empty && symbol.Kind.ToString() != kind)
-					continue;
+                if (symbol.Size > end || symbol.Size < start)
+                    continue;
 
-				if (symbol.Size > end || symbol.Size < start)
-					continue;
+                symbols.Add(new SymbolEntry(symbol));
+            }
 
-				symbols.Add(new SymbolEntry(symbol));
-			}
+            dataGridView1.DataSource = symbols;
+            dataGridView1.AutoResizeColumns();
+            dataGridView1.Columns[0].Width = 500;
+        }
 
-			dataGridView1.DataSource = symbols;
-			dataGridView1.AutoResizeColumns();
-			dataGridView1.Columns[0].Width = 500;
-		}
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            CreateEntries();
+        }
 
-		private void textBox1_TextChanged(object sender, EventArgs e)
-		{
-			CreateEntries();
-		}
+        private SymbolEntry clickedSymbolEntry;
 
-		private SymbolEntry clickedSymbolEntry;
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Right)
+                return;
 
-		private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
-		{
-			if (e.Button != MouseButtons.Right)
-				return;
+            if (e.RowIndex < 0 || e.ColumnIndex < 0)
+                return;
 
-			if (e.RowIndex < 0 || e.ColumnIndex < 0)
-				return;
+            var row = dataGridView1.Rows[e.RowIndex].DataBoundItem as SymbolEntry;
 
-			var row = dataGridView1.Rows[e.RowIndex].DataBoundItem as SymbolEntry;
+            clickedSymbolEntry = row;
 
-			clickedSymbolEntry = row;
+            var relativeMousePosition = dataGridView1.PointToClient(Cursor.Position);
 
-			var relativeMousePosition = dataGridView1.PointToClient(Cursor.Position);
+            MenuItem menu = new MenuItem(row.Name);
+            menu.Enabled = false;
+            ContextMenu m = new ContextMenu();
+            m.MenuItems.Add(menu);
+            m.MenuItems.Add(new MenuItem("Copy to &Clipboard", new EventHandler(MenuItem3_Click)));
+            m.MenuItems.Add(new MenuItem("Add to &Watch List", new EventHandler(MenuItem1_Click)));
+            m.MenuItems.Add(new MenuItem("Set &Breakpoint", new EventHandler(MenuItem2_Click)));
+            m.Show(dataGridView1, relativeMousePosition);
+        }
 
-			MenuItem menu = new MenuItem(row.Name);
-			menu.Enabled = false;
-			ContextMenu m = new ContextMenu();
-			m.MenuItems.Add(menu);
-			m.MenuItems.Add(new MenuItem("Copy to &Clipboard", new EventHandler(MenuItem3_Click)));
-			m.MenuItems.Add(new MenuItem("Add to &Watch List", new EventHandler(MenuItem1_Click)));
-			m.MenuItems.Add(new MenuItem("Set &Breakpoint", new EventHandler(MenuItem2_Click)));
-			m.Show(dataGridView1, relativeMousePosition);
-		}
+        private void MenuItem1_Click(Object sender, EventArgs e)
+        {
+            if (clickedSymbolEntry == null)
+                return;
 
-		private void MenuItem1_Click(Object sender, EventArgs e)
-		{
-			if (clickedSymbolEntry == null)
-				return;
+            MainForm.AddWatch(clickedSymbolEntry.Name, clickedSymbolEntry.Symbol.Address, clickedSymbolEntry.Size);
+        }
 
-			MainForm.AddWatch(clickedSymbolEntry.Name, clickedSymbolEntry.Symbol.Address, clickedSymbolEntry.Size);
-		}
+        private void MenuItem2_Click(Object sender, EventArgs e)
+        {
+            if (clickedSymbolEntry == null)
+                return;
 
-		private void MenuItem2_Click(Object sender, EventArgs e)
-		{
-			if (clickedSymbolEntry == null)
-				return;
+            MainForm.AddBreakPoint(clickedSymbolEntry.Symbol.Address, clickedSymbolEntry.Name);
+        }
 
-			MainForm.AddBreakPoint(clickedSymbolEntry.Symbol.Address, clickedSymbolEntry.Name);
-		}
+        private void MenuItem3_Click(Object sender, EventArgs e)
+        {
+            if (clickedSymbolEntry == null)
+                return;
 
-		private void MenuItem3_Click(Object sender, EventArgs e)
-		{
-			if (clickedSymbolEntry == null)
-				return;
+            Clipboard.SetText(clickedSymbolEntry.Name);
+        }
 
-			Clipboard.SetText(clickedSymbolEntry.Name);
-		}
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CreateEntries();
+        }
 
-		private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
-		{
-			CreateEntries();
-		}
-
-		private void toolStripComboBox2_SelectedIndexChanged(object sender, EventArgs e)
-		{
-			CreateEntries();
-		}
-	}
+        private void toolStripComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CreateEntries();
+        }
+    }
 }
