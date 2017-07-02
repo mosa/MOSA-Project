890b5e28aa55a253f6edab971961f869132b1fe1
Phil Garcia <phil@thinkedge.com>
2017-07-02 13:45:42 -0700

    - Minor changes

--- Source/Mosa.Tool.GDBDebugger/ConnectWindow.cs
+++ Source/Mosa.Tool.GDBDebugger/ConnectWindow.cs
@@ -1,37 +1,39 @@
-﻿using Mosa.Tool.GDBDebugger.GDB;
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+using Mosa.Tool.GDBDebugger.GDB;
 using System;
 using System.Windows.Forms;
 
 namespace Mosa.Tool.GDBDebugger
 {
-    public partial class ConnectWindow : Form
-    {
-        public Connector Debugger
-        {
-            get;
-            private set;
-        }
+	public partial class ConnectWindow : Form
+	{
+		public Connector Debugger
+		{
+			get;
+			private set;
+		}
 
-        public ConnectWindow()
-        {
-            InitializeComponent();
-        }
+		public ConnectWindow()
+		{
+			InitializeComponent();
+		}
 
-        private void btnCancel_Click(object sender, EventArgs e)
-        {
-            DialogResult = DialogResult.Cancel;
-        }
+		private void btnCancel_Click(object sender, EventArgs e)
+		{
+			DialogResult = DialogResult.Cancel;
+		}
 
-        private void btnConnect_Click(object sender, EventArgs e)
-        {
-            Debugger = new Connector(new X86Platform(), tbHost.Text, (int)numPort.Value);
+		private void btnConnect_Click(object sender, EventArgs e)
+		{
+			Debugger = new Connector(new X86Platform(), tbHost.Text, (int)numPort.Value);
 
-            DialogResult = DialogResult.OK;
-        }
+			DialogResult = DialogResult.OK;
+		}
 
-        private void CheckConnectButton(object sender, EventArgs e)
-        {
-            btnConnect.Enabled = (!string.IsNullOrEmpty(tbHost.Text));
-        }
-    }
+		private void CheckConnectButton(object sender, EventArgs e)
+		{
+			btnConnect.Enabled = (!string.IsNullOrEmpty(tbHost.Text));
+		}
+	}
 }
