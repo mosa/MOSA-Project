144715c91044a6e9b7b1aca149fa34ea4ba093a3
robot9706 <robot9706@gmail.com>
2017-07-24 01:18:33 +0200

    VBE world

---
+++ Source/Mosa.VBEWorld.x86/Boot.cs
@@ -0,0 +1,93 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+using Mosa.DeviceSystem;
+using Mosa.Kernel.x86;
+using Mosa.Runtime.x86;
+
+namespace Mosa.VBEWorld.x86
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class Boot
+	{
+		public static ConsoleSession Console;
+
+		public static VBEFrameBuffer32bpp _framebuffer;
+
+		/// <summary>
+		/// Main
+		/// </summary>
+		public static void Main()
+		{
+			Kernel.x86.Kernel.Setup();
+
+			Console = ConsoleManager.Controller.Boot;
+			Console.Clear();
+
+			IDT.SetInterruptHandler(ProcessInterrupt);
+
+			Serial.SetupPort(Serial.COM1); //For debugging
+
+			if(SetupVBE())
+			{
+				Log("VBE setup OK!");
+
+				DoGraphics();
+			}
+			else
+			{
+				Log("VBE setup ERROR!");
+			}
+
+			ForeverLoop();
+		}
+
+		private static void DoGraphics()
+		{
+			_framebuffer.FillRectangle(0x00555555, 0, 0, _framebuffer.Width, _framebuffer.Height);
+
+			MosaLogo.Draw(_framebuffer, 10);
+		}
+
+		private static bool SetupVBE()
+		{
+			if (!Multiboot.IsMultibootEnabled)
+			{
+				Log("Multiboot not enabled.");
+				return false;
+			}
+
+			if (!Multiboot.VBEPresent)
+			{
+				Log("VBE is not enabled.");
+				return false;
+			}
+
+			VBEMode vbeInfo = Multiboot.VBEModeInfoStructure;
+			Log("VBE mode: " + vbeInfo.ScreenWidth.ToString() + "x" + vbeInfo.ScreenHeight.ToString() + "x" + vbeInfo.BitsPerPixel);
+
+			_framebuffer = new VBEFrameBuffer32bpp(vbeInfo);
+
+			return true;
+		}
+
+		private static void Log(string line)
+		{
+			Serial.Write(Serial.COM1, line + "\r\n");
+			Console.WriteLine(line);
+		}
+
+		private static void ForeverLoop()
+		{
+			while (true)
+			{
+				Native.Hlt();
+			}
+		}
+		
+		public static void ProcessInterrupt(uint interrupt, uint errorCode)
+		{
+		}
+	}
+}
---
+++ Source/Mosa.VBEWorld.x86/VBEFrameBuffer32bpp.cs
@@ -0,0 +1,70 @@
+﻿using Mosa.DeviceSystem;
+using Mosa.Kernel.x86;
+using Mosa.Runtime;
+
+namespace Mosa.VBEWorld.x86
+{
+	public unsafe class VBEFrameBuffer32bpp : IFrameBuffer
+	{
+		public uint Width { get; private set; }
+		public uint Height { get; private set; }
+
+		private int _pixelsPerLine;
+
+		private uint* _memory;
+		private uint _memorySize;
+
+		public VBEFrameBuffer32bpp(VBEMode mode)
+		{
+			Width = mode.ScreenWidth;
+			Height = mode.ScreenHeight;
+
+			_pixelsPerLine = mode.Pitch / (mode.BitsPerPixel / 8);
+
+			_memory = (uint*)mode.MemoryPhysicalLocation;
+
+			//Map the LFB
+			uint lfbLocation = mode.MemoryPhysicalLocation;
+			_memorySize = (uint)(mode.ScreenWidth * mode.ScreenHeight * (mode.BitsPerPixel / 8));
+
+			uint pageSize = 0x1000; //???
+
+			uint pages = _memorySize / pageSize;
+			for (uint x = 0; x < pages; x++)
+			{
+				PageTable.MapVirtualAddressToPhysical((uint)(lfbLocation + pageSize * x), (uint)(lfbLocation + pageSize * x), true);
+			}
+		}
+
+		public uint GetPixel(uint x, uint y)
+		{
+			return _memory[x + _pixelsPerLine * y];
+		}
+
+		public void SetPixel(uint color, uint x, uint y)
+		{
+			_memory[x + _pixelsPerLine * y] = color;
+		}
+
+		//Non IFrameBuffer functions
+		public void Clear()
+		{
+			Internal.MemorySet((void*)_memory, 0x00, _memorySize);
+		}
+
+		public void FillRectangle(uint color, uint x, uint y, uint w, uint h)
+		{
+			uint startAddress = (uint)(x + _pixelsPerLine * y);
+
+			for (uint offsetY = 0; offsetY < h; offsetY++)
+			{
+				for (uint offsetX = 0; offsetX < w; offsetX++)
+				{
+					_memory[startAddress + offsetX] = color;
+				}
+
+				startAddress += (uint)_pixelsPerLine;
+			}
+		}
+	}
+}
