8aaf33d1d623f2254ebb60612f8e199383e30caf
Phil Garcia <phil@thinkedge.com>
2017-08-01 23:43:27 -0700

    - Minor changes

--- Source/Mosa.Compiler.Common/Alignment.cs
+++ Source/Mosa.Compiler.Common/Alignment.cs
@@ -9,39 +9,29 @@ namespace Mosa.Compiler.Common
 			return position + ((alignment - (position % alignment)) % alignment);
 		}
 
-		public static ulong AlignUp(ulong position, uint alignment)
-		{
-			return position + ((alignment - (position % alignment)) % alignment);
-		}
-
 		public static int AlignUp(int position, int alignment)
 		{
 			return position + ((alignment - (position % alignment)) % alignment);
 		}
 
-		public static long AlignUp(long position, uint alignment)
+		public static ulong AlignUp(ulong position, uint alignment)
 		{
 			return position + ((alignment - (position % alignment)) % alignment);
 		}
 
-		public static int AlignDown(int position, int alignment)
+		public static uint AlignDown(uint position, uint alignment)
 		{
-			if (alignment == 0)
-				return position;
-
-			position -= position % alignment;
-
-			return position;
+			return position - ((alignment - (position % alignment)) % alignment);
 		}
 
-		public static ulong AlignDown(ulong position, int alignment)
+		public static int AlignDown(int position, int alignment)
 		{
-			if (alignment == 0)
-				return position;
+			return position - ((alignment - (position % alignment)) % alignment);
+		}
 
-			position -= position % (ulong)alignment;
-
-			return position;
+		public static ulong AlignDown(ulong position, uint alignment)
+		{
+			return position - ((alignment - (position % alignment)) % alignment);
 		}
 	}
 }
