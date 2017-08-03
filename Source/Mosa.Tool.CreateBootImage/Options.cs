bd2ba8ce319e22455858ebb83de1f6fc375c31dd
robot9706 <robot9706@gmail.com>
2017-08-03 13:27:09 +0200

    Bootimage tool commandline progress

---
+++ Source/Mosa.Tool.CreateBootImage/Options.cs
@@ -0,0 +1,117 @@
+﻿using CommandLine;
+using Mosa.Utility.BootImage;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Mosa.Tool.CreateBootImage
+{
+	class Options
+	{
+		private BootImageOptions _options;
+
+		[Option('m', "mbr")]
+		public string MBRFile
+		{
+			set { _options.MBROption = true; _options.MBRCode = File.ReadAllBytes(value); }
+		}
+
+		[Option('b', "boot")]
+		public string BootCodeFile
+		{
+			set { _options.FatBootCode = File.ReadAllBytes(value); }
+		}
+
+		[Option("vhd")]
+		public bool VHDImageFormat
+		{
+			set { _options.ImageFormat = ImageFormat.VHD; }
+			get { return (_options.ImageFormat == ImageFormat.VHD); }
+		}
+
+		[Option("img")]
+		public bool IMGImageFormat
+		{
+			set { _options.ImageFormat = ImageFormat.IMG; }
+			get { return (_options.ImageFormat == ImageFormat.IMG); }
+		}
+
+		[Option("vdi")]
+		public bool VDIImageFormat
+		{
+			set { _options.ImageFormat = ImageFormat.VDI; }
+			get { return (_options.ImageFormat == ImageFormat.VDI); }
+		}
+
+		[Option("syslinux")]
+		public bool Syslinux
+		{
+			set { _options.PatchSyslinuxOption = true; }
+			get { return _options.PatchSyslinuxOption; }
+		}
+
+		[Option("guid")]
+		public string GUID
+		{
+			set { _options.MediaGuid = new Guid(value); }
+			get { return _options.MediaGuid.ToString(); }
+		}
+
+		[Option("snapguid")]
+		public string SnapGUID
+		{
+			set { _options.MediaLastSnapGuid = new Guid(value); }
+			get { return _options.MediaLastSnapGuid.ToString(); }
+		}
+
+		[Option("fat32")]
+		public bool Fat32FileSystem
+		{
+			set { _options.FileSystem = FileSystem.FAT32; }
+			get { return (_options.FileSystem == FileSystem.FAT32); }
+		}
+
+		[Option("fat16")]
+		public bool Fat16FileSystem
+		{
+			set { _options.FileSystem = FileSystem.FAT16; }
+			get { return (_options.FileSystem == FileSystem.FAT16); }
+		}
+
+		[Option("fat12")]
+		public bool Fat12FileSystem
+		{
+			set { _options.FileSystem = FileSystem.FAT12; }
+			get { return (_options.FileSystem == FileSystem.FAT12); }
+		}
+
+		[Option("blocks")]
+		public uint BlockCount
+		{
+			set { _options.BlockCount = Convert.ToUInt32(value); }
+			get { return _options.BlockCount; }
+		}
+
+		[Option("volume")]
+		public string VolumeLabel
+		{
+			set { _options.VolumeLabel = value; }
+			get { return _options.VolumeLabel; }
+		}
+
+		[OptionArray("file")]
+		public IEnumerable<string> AdditionalFiles
+		{ get; set; }
+
+		public BootImageOptions BootImageOptions
+		{
+			get { return _options; }
+		}
+
+		public Options()
+		{
+			_options = new BootImageOptions();
+		}
+	}
+}
--- Source/Mosa.Tool.CreateBootImage/Program.cs
+++ Source/Mosa.Tool.CreateBootImage/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
 
+using CommandLine;
 using Mosa.Compiler.Common;
 using Mosa.Utility.BootImage;
 using System;
@@ -14,45 +15,63 @@ namespace Mosa.Tool.CreateBootImage
 	{
 		public static BootImageOptions Parse(string filename)
 		{
-			var options = new BootImageOptions();
+			Options options = ParseOptions(File.ReadAllText(filename).Split(new char[] { '\r', '\n', ' ' }, StringSplitOptions.RemoveEmptyEntries));
+			if (options == null)
+				return null;
 
-			var reader = File.OpenText(filename);
+			return options.BootImageOptions;
 
-			while (true)
+			//var options = new BootImageOptions();
+
+			//var reader = File.OpenText(filename);
+
+			//while (true)
+			//{
+			//	string line = reader.ReadLine();
+			//	if (line == null) break;
+
+			//	if (string.IsNullOrEmpty(line))
+			//		continue;
+
+			//	string[] parts = line.Split(new char[] { '\t', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+			//	switch (parts[0].Trim())
+			//	{
+			//		case "-mbr": options.MBROption = true; options.MBRCode = (parts.Length > 1) ? File.ReadAllBytes(parts[1]) : null; break;
+			//		case "-boot": options.FatBootCode = (parts.Length > 1) ? File.ReadAllBytes(parts[1]) : null; break;
+			//		case "-vhd": options.ImageFormat = ImageFormat.VHD; break;
+			//		case "-img": options.ImageFormat = ImageFormat.IMG; break;
+			//		case "-vdi": options.ImageFormat = ImageFormat.VDI; break;
+			//		case "-syslinux": options.PatchSyslinuxOption = true; break;
+			//		case "-guid": if (parts.Length > 1) options.MediaGuid = new Guid(parts[1]); break;
+			//		case "-snapguid": if (parts.Length > 1) options.MediaLastSnapGuid = new Guid(parts[1]); break;
+			//		case "-fat12": options.FileSystem = FileSystem.FAT12; break;
+			//		case "-fat16": options.FileSystem = FileSystem.FAT16; break;
+			//		case "-fat32": options.FileSystem = FileSystem.FAT32; break;
+			//		case "-file":
+			//			if (parts.Length > 2) options.IncludeFiles.Add(new IncludeFile(parts[1], parts[2]));
+			//			else options.IncludeFiles.Add(new IncludeFile(parts[1])); break;
+			//		case "-blocks": options.BlockCount = Convert.ToUInt32(parts[1]); break;
+			//		case "-volume": options.VolumeLabel = parts[1]; break;
+			//		default: break;
+			//	}
+			//}
+
+			//reader.Close();
+
+			//return options;
+		}
+
+		private static Options ParseOptions(string[] args)
+		{
+			ParserResult<Options> result = new Parser(config => config.HelpWriter = Console.Out).ParseArguments<Options>(args);
+
+			if (result.Tag == ParserResultType.NotParsed)
 			{
-				string line = reader.ReadLine();
-				if (line == null) break;
-
-				if (string.IsNullOrEmpty(line))
-					continue;
-
-				string[] parts = line.Split(new char[] { '\t', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-				switch (parts[0].Trim())
-				{
-					case "-mbr": options.MBROption = true; options.MBRCode = (parts.Length > 1) ? File.ReadAllBytes(parts[1]) : null; break;
-					case "-boot": options.FatBootCode = (parts.Length > 1) ? File.ReadAllBytes(parts[1]) : null; break;
-					case "-vhd": options.ImageFormat = ImageFormat.VHD; break;
-					case "-img": options.ImageFormat = ImageFormat.IMG; break;
-					case "-vdi": options.ImageFormat = ImageFormat.VDI; break;
-					case "-syslinux": options.PatchSyslinuxOption = true; break;
-					case "-guid": if (parts.Length > 1) options.MediaGuid = new Guid(parts[1]); break;
-					case "-snapguid": if (parts.Length > 1) options.MediaLastSnapGuid = new Guid(parts[1]); break;
-					case "-fat12": options.FileSystem = FileSystem.FAT12; break;
-					case "-fat16": options.FileSystem = FileSystem.FAT16; break;
-					case "-fat32": options.FileSystem = FileSystem.FAT32; break;
-					case "-file":
-						if (parts.Length > 2) options.IncludeFiles.Add(new IncludeFile(parts[1], parts[2]));
-						else options.IncludeFiles.Add(new IncludeFile(parts[1])); break;
-					case "-blocks": options.BlockCount = Convert.ToUInt32(parts[1]); break;
-					case "-volume": options.VolumeLabel = parts[1]; break;
-					default: break;
-				}
+				return null;
 			}
 
-			reader.Close();
-
-			return options;
+			return ((Parsed<Options>)result).Value;
 		}
 
 		/// <summary>
