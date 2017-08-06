480d75514aa33ad7c136168c578dbb83e24285b2
Phil Garcia <phil@thinkedge.com>
2017-08-05 22:18:14 -0700

    - Implemented Null-Pointer Optimization

---
+++ Source/Mosa.UnitTest.Collection.xUnit/NullCheckOptimizationFixture.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+using Xunit;
+
+namespace Mosa.UnitTest.Collection.xUnit
+{
+	public class NullCheckOptimizationFixture : TestFixture
+	{
+		[Fact]
+		public void NullTest1()
+		{
+			Assert.Equal(Mosa.UnitTest.Collection.NullCheckOptimizationTests.NullTest1(), Run<bool>("Mosa.UnitTest.Collection.NullCheckOptimizationTests.NullTest1"));
+		}
+
+		[Fact]
+		public void NullTest2()
+		{
+			Assert.Equal(Mosa.UnitTest.Collection.NullCheckOptimizationTests.NullTest2(), Run<bool>("Mosa.UnitTest.Collection.NullCheckOptimizationTests.NullTest2"));
+		}
+	}
+}
