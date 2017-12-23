ce76497541cd75b43f32702e6fed811dc631b6d7
Phil Garcia <phil@thinkedge.com>
2017-12-23 12:28:46 -0800

    - Moving toward rule based/dynamic code generation for instructions, optimizations, etc.

--- Source/Mosa.Compiler.Framework/Expression/ExpressionNode.cs
+++ Source/Mosa.Compiler.Framework/Expression/ExpressionNode.cs
@@ -1,37 +1,40 @@
 ﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
 
 using System.Collections.Generic;
+using System.Text;
 
 namespace Mosa.Compiler.Framework.Expression
 {
 	public class ExpressionNode
 	{
-		public TokenType Token { get; protected set; }
+		public Token Token { get; protected set; }
+
+		public TokenType TokenType { get { return Token.TokenType; } }
 
 		public ExpressionNode Left { get; protected set; }
 		public ExpressionNode Right { get; protected set; }
 
 		public List<ExpressionNode> Parameters { get; protected set; }
 
-		public ExpressionNode(TokenType token)
+		public ExpressionNode(Token token)
 		{
 			Token = token;
 		}
 
-		public ExpressionNode(TokenType token, ExpressionNode left)
+		public ExpressionNode(Token token, ExpressionNode left)
 		{
 			Token = token;
 			Left = left;
 		}
 
-		public ExpressionNode(TokenType token, ExpressionNode left, ExpressionNode right)
+		public ExpressionNode(Token token, ExpressionNode left, ExpressionNode right)
 		{
 			Token = token;
 			Left = left;
 			Right = right;
 		}
 
-		public ExpressionNode(TokenType token, List<ExpressionNode> parameters)
+		public ExpressionNode(Token token, List<ExpressionNode> parameters)
 		{
 			Token = token;
 			Parameters = parameters;
@@ -39,7 +42,30 @@ namespace Mosa.Compiler.Framework.Expression
 
 		public override string ToString()
 		{
-			return Token.ToString();
+			var sb = new StringBuilder();
+
+			sb.Append(Token.ToString());
+
+			if (Left != null && Right != null)
+			{
+				sb.Append("(");
+
+				if (Left != null)
+				{
+					sb.Append(" ");
+					sb.Append(Left.ToString());
+				}
+
+				if (Right != null)
+				{
+					sb.Append(" ");
+					sb.Append(Right.ToString());
+				}
+
+				sb.Append(")");
+			}
+
+			return sb.ToString();
 		}
 	}
 }
