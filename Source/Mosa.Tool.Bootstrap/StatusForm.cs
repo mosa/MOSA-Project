f566539f0a4917b030f947048fd3cd977e8da0d0
Phil Garcia <phil@thinkedge.com>
2018-04-27 06:41:44 -0700

    Adding Installer tool (#454)
    * - Added Bootstrap Tool
    - Updated Inno installer
    
    * - Updated appveyor.yml
    
    * - Updated appveyor.yml
    
    * - Updated appveyor.yml
    
    * - Updated appveyor.yml
    
    * - Updated appveyor.yml
    
    * - Updated appveyor.yml
    
    * - Updated appveyor.yml
    
    * - Updated appveyor.yml
    
    * - Updated appveyor.yml
    
    * - Updated appveyor.yml
    
    * - Updated appveyor.yml
    
    * - Updated appveyor.yml
    
    * - Updated appveyor.yml
    
    * - Updated appveyor.yml
    
    * - Updated appveyor.yml
    
    * - Updated appveyor.yml
    
    * - Updated appveyor.yml

---
+++ Source/Mosa.Tool.Bootstrap/StatusForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mosa.Tool.Bootstrap
+{
+	public partial class StatusForm : Form
+	{
+		public StatusForm(string status)
+		{
+			InitializeComponent();
+			this.lbStatus.Text = status;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+	}
+}
