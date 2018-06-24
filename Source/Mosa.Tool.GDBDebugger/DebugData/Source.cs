63aa3ac9cac6b30bae8318dc0e0575fd2a969fbe
Phil Garcia <phil@thinkedge.com>
2018-06-24 10:13:39 -0700

    - Added SourceDataView to GDBDebugger

---
+++ Source/Mosa.Tool.GDBDebugger/DebugData/Source.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+using Mosa.Tool.GDBDebugger.DebugData;
+
+namespace Mosa.Tool.GDBDebugger.Debug
+{
+	public static class DebugData
+	{
+		public static SourceLocation Find(DebugSource debugSource, ulong address)
+		{
+			var method = debugSource.GetMethod(address);
+
+			if (method == null)
+				return null;
+
+			var sourceLabel = debugSource.GetSourceLabel(method.ID, (int)(address - method.Address));
+
+			if (sourceLabel == null)
+				return null;
+
+			var sourceInfo = debugSource.GetSourcePreviousClosest(method.ID, sourceLabel.Label);
+
+			if (sourceInfo == null)
+				return null;
+
+			var sourceFileInfo = debugSource.GetSourceFile(sourceInfo.SourceFileID);
+
+			var sourceLocation = new SourceLocation()
+			{
+				Address = method.Address + (ulong)sourceLabel.StartOffset,
+				Length = sourceLabel.Length,
+				Label = sourceLabel.Label,
+				SourceLabel = sourceInfo.Label,
+				MethodFullName = method.FullName,
+				StartLine = sourceInfo.StartLine,
+				StartColumn = sourceInfo.StartColumn,
+				EndLine = sourceInfo.EndLine,
+				EndColumn = sourceInfo.EndColumn,
+				SourceFilename = sourceFileInfo.Filename,
+				MethodID = method.ID,
+				SourceFileID = sourceInfo.SourceFileID,
+			};
+
+			return sourceLocation;
+		}
+	}
+}
+
+//SourceLabelInfo:
+//	MethodID
+//	Label<-            | need this
+//	Start              |
+//	Length             V
+//                     V
+//                     V
+//SourceInfo:          V
+//	MethodID           |
+//	Offset [/Label] <- | to get this
+//	[...]
+//	SourceFileID
