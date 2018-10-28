568c78b7a4772c822a802b630ffeb6edac3ef8bc
Arakis <sebastian.loncar@gmail.com>
2018-10-28 17:00:27 +0100

    Set Segment Registers

--- Source/Mosa.Platform.x86/Intrinsic/Lgdt.cs
+++ Source/Mosa.Platform.x86/Intrinsic/Lgdt.cs
@@ -11,24 +11,24 @@ namespace Mosa.Platform.x86.Intrinsic
 	{
 		void IIntrinsicPlatformMethod.ReplaceIntrinsicCall(Context context, MethodCompiler methodCompiler)
 		{
-			Helper.FoldOperand1ToConstant(context);
+			//Helper.FoldOperand1ToConstant(context);
 
-			var constantx10 = methodCompiler.CreateConstant(0x10);
+			//var constantx10 = methodCompiler.CreateConstant(0x10);
 
-			Operand eax = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I4, GeneralPurposeRegister.EAX);
-			Operand ds = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I2, SegmentRegister.DS);
-			Operand es = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I2, SegmentRegister.ES);
-			Operand fs = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I2, SegmentRegister.FS);
-			Operand gs = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I2, SegmentRegister.GS);
-			Operand ss = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I2, SegmentRegister.SS);
+			//Operand eax = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I4, GeneralPurposeRegister.EAX);
+			//Operand ds = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I2, SegmentRegister.DS);
+			//Operand es = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I2, SegmentRegister.ES);
+			//Operand fs = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I2, SegmentRegister.FS);
+			//Operand gs = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I2, SegmentRegister.GS);
+			//Operand ss = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I2, SegmentRegister.SS);
 
 			context.SetInstruction(X86.Lgdt, null, context.Operand1);
-			context.AppendInstruction(X86.MovConst32, eax, constantx10);
-			context.AppendInstruction(X86.MovStoreSeg32, ds, eax);
-			context.AppendInstruction(X86.MovStoreSeg32, es, eax);
-			context.AppendInstruction(X86.MovStoreSeg32, fs, eax);
-			context.AppendInstruction(X86.MovStoreSeg32, gs, eax);
-			context.AppendInstruction(X86.MovStoreSeg32, ss, eax);
+			//context.AppendInstruction(X86.MovConst32, eax, constantx10);
+			//context.AppendInstruction(X86.MovStoreSeg32, ds, eax);
+			//context.AppendInstruction(X86.MovStoreSeg32, es, eax);
+			//context.AppendInstruction(X86.MovStoreSeg32, fs, eax);
+			//context.AppendInstruction(X86.MovStoreSeg32, gs, eax);
+			//context.AppendInstruction(X86.MovStoreSeg32, ss, eax);
 			context.AppendInstruction(X86.JmpFar);
 		}
 	}
