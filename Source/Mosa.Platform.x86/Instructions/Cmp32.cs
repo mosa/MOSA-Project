0a874c9e23b45f1da4dfe4da571b83a104e8bf46
Phil Garcia <phil@thinkedge.com>
2018-11-07 23:34:43 -0800

    - Adding x64 support

--- Source/Mosa.Platform.x86/Instructions/Cmp32.cs
+++ Source/Mosa.Platform.x86/Instructions/Cmp32.cs
@@ -41,12 +41,15 @@ namespace Mosa.Platform.x86.Instructions
 
 		public override bool IsParityFlagUndefined { get { return true; } }
 
-		internal override void EmitLegacy(InstructionNode node, X86CodeEmitter emitter)
+		public override void Emit(InstructionNode node, BaseCodeEmitter emitter)
 		{
 			System.Diagnostics.Debug.Assert(node.ResultCount == 0);
 			System.Diagnostics.Debug.Assert(node.OperandCount == 2);
 
-			emitter.Emit(LegacyOpcode, node.Operand1, node.Operand2);
+			emitter.OpcodeEncoder.AppendByte(0x3B);
+			emitter.OpcodeEncoder.Append2Bits(0b11);
+			emitter.OpcodeEncoder.Append3Bits(node.Operand1.Register.RegisterCode);
+			emitter.OpcodeEncoder.Append3Bits(node.Operand2.Register.RegisterCode);
 		}
 	}
 }
