e6af84b8b6bfd5106a53e6b042ed2f4b1d6282dd
Phil Garcia <phil@thinkedge.com>
2018-11-24 21:12:48 -0800

    - Adding x64 support

--- Source/Mosa.Platform.x86/Instructions/StaticEmitters.cs
+++ Source/Mosa.Platform.x86/Instructions/StaticEmitters.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
 
-using Mosa.Compiler.Common;
 using Mosa.Compiler.Framework;
-using Mosa.Platform.Intel;
-using System.Diagnostics;
 
 namespace Mosa.Platform.x86.Instructions
 {
