3d641d67876af5e4d43775b70918095f9c451b32
Phil Garcia <phil@thinkedge.com>
2018-12-07 19:21:25 -0800

    - Pulling korlib plugs out of the base Mosa.Runtime

---
+++ Source/Mosa.Plugin.Korlib/Plug.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+namespace Mosa.Plug.Korlib
+{
+	public static class Plug
+	{
+		public static bool Enable { get; set; }
+	}
+}
---
+++ Source/Mosa.Runtime.x86/PlugInclude.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+namespace Mosa.Runtime.x86
+{
+	internal static class PlugInclude
+	{
+		internal static bool Include = Mosa.Plug.Korlib.Plug.Enable;   // forces the plug to be included
+	}
+}
