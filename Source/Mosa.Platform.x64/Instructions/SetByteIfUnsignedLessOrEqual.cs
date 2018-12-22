9e9abb5da6c6c53967683b798c0ec45208e887b7
Phil Garcia <phil@thinkedge.com>
2018-12-21 19:32:45 -0800

    - Adding x64 support

--- Source/Mosa.Platform.x64/Instructions/SetByteIfUnsignedLessOrEqual.cs
+++ Source/Mosa.Platform.x64/Instructions/SetByteIfUnsignedLessOrEqual.cs
@@ -35,7 +35,6 @@ namespace Mosa.Platform.x64.Instructions
 			System.Diagnostics.Debug.Assert(node.ResultCount == 1);
 			System.Diagnostics.Debug.Assert(node.OperandCount == 0);
 
-			emitter.OpcodeEncoder.AppendByte(0x0F);
 			emitter.OpcodeEncoder.AppendByte(0x96);
 			emitter.OpcodeEncoder.Append2Bits(0b11);
 			emitter.OpcodeEncoder.Append3Bits(0b000);
