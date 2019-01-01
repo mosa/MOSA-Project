3a92ef97fdda83432fcbfd7bf0fe04844273ecd2
Phil Garcia <phil@thinkedge.com>
2018-12-31 23:19:49 -0800

    - WIP

--- Source/Mosa.Platform.x64/X64Instruction.cs
+++ Source/Mosa.Platform.x64/X64Instruction.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
 
-using Mosa.Compiler.Framework;
 using Mosa.Compiler.Framework.Platform;
-using System;
 
 namespace Mosa.Platform.x64
 {
