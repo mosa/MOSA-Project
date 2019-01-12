d8ac6c0a86c883343648cce04ecb3807cbe152b8
Arakis <sebastian.loncar@gmail.com>
2019-01-12 19:27:51 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,6 @@ extensions = [
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
-    'sphinxcontrib.httpdomain',
     'doc_extensions',
     'sphinx_tabs.tabs',
     'sphinx-prompt',
