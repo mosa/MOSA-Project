9eed93249cc763f60db29f82ac36e927adc9acc3
Arakis <sebastian.loncar@gmail.com>
2019-02-03 13:06:47 +0100

    WIP

--- Source/Mosa.Compiler.Extension.Dwarf/DwarfCompilerStage.cs
+++ Source/Mosa.Compiler.Extension.Dwarf/DwarfCompilerStage.cs
@@ -112,6 +112,83 @@ namespace Mosa.Compiler.Extensions.Dwarf
 
 		private void EmitDebugLine(EndianAwareBinaryWriter wr)
 		{
+			var compilationUnitSizePosition = wr.Position;
+			wr.Write((uint)0); // Placeholder for Compilation unit Size
+
+			wr.WriteByte(0x02); // DWARF Version
+			wr.WriteByte(0x00); // version (2 bytes)
+
+			var headerSizePosition = wr.Position;
+			wr.Write((uint)0); // Placeholder for header size
+
+			wr.WriteByte(0x01); // Minimum Instruction length
+			wr.WriteByte(0x01); // Default is_stmt value
+			wr.WriteByte(0xFB); // Value doesn't matter, because we are not using special op codes
+			wr.WriteByte(0x0E); // Value doesn't matter, because we are not using special op codes
+			wr.WriteByte(9 + 1); // first special op code
+
+			// wr.Write(0x00010101); // the number of arguments for the 12 standard opcodes
+			// wr.Write(0x01000000);
+			// wr.Write(0x01000001);
+
+			// the number of arguments for the 9 standard opcodes
+			wr.WriteByte(0x00); // 1
+			wr.WriteByte(0x01); // 2
+			wr.WriteByte(0x01); // ...
+			wr.WriteByte(0x01);
+
+			wr.WriteByte(0x01);
+			wr.WriteByte(0x00);
+			wr.WriteByte(0x00);
+			wr.WriteByte(0x00);
+
+			wr.WriteByte(0x01); // 9
+									  //---
+
+			var dirNum = EmitDebugLineDirectoryName(wr, "dir2");
+			wr.WriteByte(DwarfConstants.EndOfDirectories); // End of directories
+
+			EmitDebugLineFileName(wr, dirNum, "file2.cs");
+			wr.WriteByte(DwarfConstants.EndOfFiles); // End of files
+
+			// Write header size
+			uint headerSize = (uint)(wr.Position - headerSizePosition - sizeof(uint));
+			wr.Position = headerSizePosition;
+			wr.Write(headerSize);
+			wr.Position = wr.BaseStream.Length;
+
+			EmitDebugLineTypes(wr);
+
+			uint compilationUnitSize = (uint)(wr.Position - compilationUnitSizePosition - sizeof(uint));
+			wr.Position = compilationUnitSizePosition;
+			wr.Write(compilationUnitSize);
+			wr.Position = wr.BaseStream.Length;
+		}
+
+		private uint NextDirIndex = 0;
+		private uint NextFileIndex = 0;
+		uint EmitDebugLineDirectoryName(EndianAwareBinaryWriter wr, string name)
+		{
+			wr.WriteNullTerminatedString("file1.cs");
+			return NextDirIndex++;
+		}
+
+		uint EmitDebugLineFileName(EndianAwareBinaryWriter wr, uint directoryIndex, string name)
+		{
+			wr.WriteNullTerminatedString("file1.cs");
+			wr.WriteULEB128(directoryIndex);
+			wr.WriteULEB128(DwarfConstants.NullFileTime);
+			wr.WriteULEB128(DwarfConstants.NullFileLength);
+			return NextFileIndex++;
+		}
+
+		void EmitDebugLineTypes(EndianAwareBinaryWriter wr)
+		{
+			uint baseAddr = 0x00500000;
+			uint pc;
+			uint line = 1;
+			uint file = 1;
+
 			foreach (var type in TypeSystem.AllTypes)
 			{
 				if (type.IsModule)
@@ -127,76 +204,25 @@ namespace Mosa.Compiler.Extensions.Dwarf
 					if (firstInstruction == null)
 						continue;
 
-					uint pc = 0;
-					uint line = 1;
-					uint col = 0;
-					uint file = 1;
-
-					var compilationUnitSizePosition = wr.Position;
-					wr.Write((uint)0); // Placeholder for Compilation unit Size
-
-					wr.WriteByte(0x02); // DWARF Version
-					wr.WriteByte(0x00); // version (2 bytes)
-
-					var headerSizePosition = wr.Position;
-					wr.Write((uint)0); // Placeholder for header size
-
-					wr.WriteByte(0x01); // Minimum Instruction length
-					wr.WriteByte(0x01); // Default is_stmt value
-					wr.WriteByte(0xFB); // Value doesn't matter, because we are not using special op codes
-					wr.WriteByte(0x0E); // Value doesn't matter, because we are not using special op codes
-					wr.WriteByte(9 + 1); // first special op code
-
-					// wr.Write(0x00010101); // the number of arguments for the 12 standard opcodes
-					// wr.Write(0x01000000);
-					// wr.Write(0x01000001);
-
-					// the number of arguments for the 9 standard opcodes
-					wr.WriteByte(0x00);
-					wr.WriteByte(0x01);
-					wr.WriteByte(0x01);
-					wr.WriteByte(0x01);
-
-					wr.WriteByte(0x01);
-					wr.WriteByte(0x00);
-					wr.WriteByte(0x00);
-					wr.WriteByte(0x00);
-
-					wr.WriteByte(0x01);
-					//---
-
-					wr.WriteNullTerminatedString("dir1");
-					wr.WriteByte(DwarfConstants.EndOfDirectories); // End of directories
-
-					wr.WriteNullTerminatedString("file1.cs");
-					wr.WriteULEB128(0); // Dir index 0
-					wr.WriteULEB128(DwarfConstants.NullFileTime);
-					wr.WriteULEB128(DwarfConstants.NullFileLength);
-
-					wr.WriteByte(DwarfConstants.EndOfFiles); // End of files
-
-					// Write header size
-					uint headerSize = (uint)(wr.Position - headerSizePosition - sizeof(uint));
-					wr.Position = headerSizePosition;
-					wr.Write(headerSize);
-					wr.Position = wr.BaseStream.Length;
+					pc = baseAddr + (uint)firstInstruction.Offset;
 
 					wr.WriteByte(0); // signals an extended opcode
 					wr.WriteULEB128(0x05); // number of bytes after this used by the extended opcode (unsigned LEB128 encoded)
 					wr.Write(DW_LNE_set_address);
-					wr.Write(firstInstruction.Offset);
+					wr.Write(pc);
 
 					foreach (var instruction in instructions)
 					{
-						continue;
 						if (instruction.Document == null)
 							continue;
 
 						if (instruction.StartLine == 0xFEEFEE)
 							continue;
 
-						uint pcDiff = pc - (uint)instruction.Offset;
-						int lineDiff = (int)line - instruction.StartLine;
+						uint newPc = baseAddr + (uint)instruction.Offset;
+						uint pcDiff = newPc - pc;
+
+						int lineDiff = instruction.StartLine - (int)line;
 
 						wr.Write(DW_LNS_set_file);
 						wr.WriteULEB128(file);
@@ -213,13 +239,8 @@ namespace Mosa.Compiler.Extensions.Dwarf
 						wr.Write(DW_LNS_copy);
 
 						pc += pcDiff;
-						line += (uint)lineDiff;
+						line = (uint)instruction.StartLine;
 					};
-
-					uint compilationUnitSize = (uint)(wr.Position - compilationUnitSizePosition - sizeof(uint));
-					wr.Position = compilationUnitSizePosition;
-					wr.Write(compilationUnitSize);
-					wr.Position = wr.BaseStream.Length;
 				}
 
 			}
