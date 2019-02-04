c5c554cbe7c94e8d2cf1d184c5f66e356e7b71c4
Phil Garcia <phil@thinkedge.com>
2019-02-03 20:06:01 -0800

    - Added methods to return method regions to source code

--- Source/Mosa.Compiler.Framework/Source/SourceRegions.cs
+++ Source/Mosa.Compiler.Framework/Source/SourceRegions.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
 
 using Mosa.Compiler.MosaTypeSystem;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Mosa.Compiler.Framework.Source
 {
