1878ded45c5315af3d0b72e7ab4b5c5e838cf0b7
Phil Garcia <phil@thinkedge.com>
2019-02-03 20:02:51 -0800

    - Added methods to return method regions to source code

---
+++ Source/Mosa.Compiler.Framework/Source/SourceRegion.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+namespace Mosa.Compiler.Framework.Source
+{
+	public class SourceRegion
+	{
+		public int Address { get; set; }
+		public int Length { get; set; }
+
+		public int StartLine { get; set; }
+		public int EndLine { get; set; }
+		public int StartColumn { get; set; }
+		public int EndColumn { get; set; }
+
+		public string Source { get; set; }
+	}
+}
---
+++ Source/Mosa.Compiler.Framework/Source/SourceRegions.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+using Mosa.Compiler.MosaTypeSystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mosa.Compiler.Framework.Source
+{
+	public static class SourceRegions
+	{
+		public static List<SourceRegion> GetSourceRegions(MosaMethod method, MethodData data)
+		{
+			var regions = new List<SourceRegion>(data.LabelRegions.Count);
+
+			foreach (var labelRegion in data.LabelRegions)
+			{
+				foreach (var instruction in method.Code)
+				{
+					if (instruction.Offset != labelRegion.Label)
+						continue;
+
+					var region = new SourceRegion()
+					{
+						Address = labelRegion.Start,
+						Length = labelRegion.Length,
+						StartLine = instruction.StartLine,
+						EndLine = instruction.EndLine,
+						StartColumn = instruction.StartColumn,
+						EndColumn = instruction.EndColumn,
+						Source = instruction.Document
+					};
+
+					regions.Add(region);
+
+					break;
+				}
+			}
+
+			return regions;
+		}
+	}
+}
