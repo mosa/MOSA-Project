c76a5269dc98b0e48b9e6e46d7cbaa8948f88ab4
Phil Garcia <phil@thinkedge.com>
2019-03-31 11:03:00 -0700

    - WIP - More math functions

---
+++ Source/Mosa.Plug.Korlib/System/MathF.CoreCLR.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+using Mosa.Runtime.Plug;
+
+namespace Mosa.Plug.Korlib.System
+{
+	internal static class MathF
+	{
+		[Plug("System.MathF::Ceiling")]
+		internal static float Ceiling(float a)
+		{
+			return 0; // TODO
+		}
+
+		[Plug("System.MathF::Floor")]
+		internal static float Floor(float d)
+		{
+			return 0; // TODO
+		}
+
+		[Plug("System.MathF::Log")]
+		internal static float Log(float d)
+		{
+			return 0; // TODO
+		}
+
+		[Plug("System.MathF::FMod")]
+		internal static float FMod(float x, float y)
+		{
+			return 0; // TODO
+		}
+
+		[Plug("System.MathF::ModF")]
+		internal static unsafe float ModF(float x, float* intptr)
+		{
+			return 0; // TODO
+		}
+	}
+}
