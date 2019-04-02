73407ffa8527c4837293d55ead008ca9ebe4f96f
Phil Garcia <phil@thinkedge.com>
2019-04-01 20:56:58 -0700

    - WIP

--- Source/Mosa.TestWorld.x86/Boot.cs
+++ Source/Mosa.TestWorld.x86/Boot.cs
@@ -26,7 +26,9 @@ namespace Mosa.TestWorld.x86
 		public static void Main()
 		{
 			Screen.Clear();
+			Screen.BackgroundColor = ScreenColor.Blue;
 			Screen.Color = ScreenColor.Yellow;
+
 			Screen.Write('M');
 			Screen.Write('O');
 			Screen.Write('S');
@@ -68,26 +70,12 @@ namespace Mosa.TestWorld.x86
 			Screen.Write('D');
 			Console = ConsoleManager.Controller.Boot;
 			Screen.Write('E');
-
-			Console.Color = ScreenColor.Yellow;
-			Console.BackgroundColor = ScreenColor.Blue;
-			Console.WriteLine();
-			Console.WriteLine();
-
-			//ArrayLayoutTests.U8d();
+			Screen.WriteLine();
+			Screen.WriteLine();
 
 			KernelTest.RunTests();
-
-			DumpStackTrace();
-
-			Console.Goto(2, 0);
-
-			Scheduler.CreateThread(Thread1, PageFrameAllocator.PageSize);
-			Scheduler.CreateThread(Thread2, PageFrameAllocator.PageSize);
-			Scheduler.CreateThread(Thread3, PageFrameAllocator.PageSize);
-			Scheduler.CreateThread(Thread4, PageFrameAllocator.PageSize);
-
-			Scheduler.Start();
+			StackTrace();
+			StartThreadTest();
 
 			// should never get here
 			Screen.Write("!BAD!");
@@ -96,6 +84,26 @@ namespace Mosa.TestWorld.x86
 			{
 				Native.Hlt();
 			}
+		}
+
+		private static void StartThreadTest()
+		{
+			Screen.WriteLine();
+			Screen.Color = ScreenColor.Yellow;
+			Screen.Write('[');
+			Screen.Color = ScreenColor.White;
+			Screen.Write("Thread Test");
+			Screen.Color = ScreenColor.Yellow;
+			Screen.Write(']');
+			Screen.WriteLine();
+
+			Scheduler.CreateThread(Thread1, PageFrameAllocator.PageSize);
+			Scheduler.CreateThread(Thread2, PageFrameAllocator.PageSize);
+			Scheduler.CreateThread(Thread3, PageFrameAllocator.PageSize);
+			Scheduler.CreateThread(Thread4, PageFrameAllocator.PageSize);
+			Scheduler.CreateThread(Thread5, PageFrameAllocator.PageSize);
+
+			Scheduler.Start();
 		}
 
 		private static readonly object spinlock = new object();
@@ -110,8 +118,8 @@ namespace Mosa.TestWorld.x86
 			{
 				lock (spinlock)
 				{
-					Console.Goto(0, 14 + (thread * 13));
-					Console.Write("T" + thread.ToString() + ":" + ticks.ToString());
+					Screen.Goto(18 + thread, 0);
+					Screen.Write("Thread #" + thread.ToString() + ": " + ticks.ToString());
 				}
 
 				//Native.Hlt();
@@ -158,8 +166,27 @@ namespace Mosa.TestWorld.x86
 			}
 		}
 
-		public unsafe static void DumpStackTrace()
+		public static void Thread5()
 		{
+			uint ticks = 0;
+
+			while (true)
+			{
+				UpdateThreadTicks(5, ++ticks);
+			}
+		}
+
+		public unsafe static void StackTrace()
+		{
+			Screen.Color = ScreenColor.Yellow;
+			Screen.Write('[');
+			Screen.Color = ScreenColor.White;
+			Screen.Write("Stack Trace");
+			Screen.Color = ScreenColor.Yellow;
+			Screen.Write(']');
+			Screen.WriteLine();
+			Screen.WriteLine();
+
 			uint depth = 0;
 
 			while (true)
@@ -174,9 +201,9 @@ namespace Mosa.TestWorld.x86
 				if (caller == null)
 					return;
 
-				Console.Write(depth, 10, 2);
-				Console.Write(":");
-				Console.WriteLine(caller);
+				Screen.Write(depth, 10, 2);
+				Screen.Write(":");
+				Screen.WriteLine(caller);
 
 				depth++;
 			}
