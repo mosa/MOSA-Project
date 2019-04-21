01a882eb40abeedc34b64d7cfd5bd0f4401687af
Phil Garcia <phil@thinkedge.com>
2019-04-21 10:54:29 -0700

    - WIP

--- Source/Mosa.Kernel.x86/SSE.cs
+++ Source/Mosa.Kernel.x86/SSE.cs
@@ -10,7 +10,7 @@ namespace Mosa.Kernel.x86
 	/// </summary>
 	public static class SSE
 	{
-		[Plug("Mosa.Runtime.StartUp::InitalizeProcessor1")]
+		[Plug("Mosa.Runtime.StartUp::KernalInitialization")]
 		public static void Setup()
 		{
 			Native.SetCR0(Native.GetCR0() & 0xFFFB | 0x2);
