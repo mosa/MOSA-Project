848daf737f40a5170a4eae81f58bb962a61df499
Phil Garcia <phil@thinkedge.com>
2019-06-22 14:15:29 -0700

    Update appvoyer.yml (#642)

--- Source/Inno-Setup-Script/create-installer.bat
+++ Source/Inno-Setup-Script/create-installer.bat
@@ -1,3 +1,4 @@
-set INNOSETUP=C:\Program Files (x86)\Inno Setup 5\ISCC.exe
+rem set INNOSETUP=C:\Program Files (x86)\Inno Setup 5\ISCC.exe
+set INNOSETUP=C:\Program Files (x86)\Inno Setup 6\ISCC.exe
 
 if exist "%INNOSETUP%" "%INNOSETUP%" Mosa-Installer.iss
