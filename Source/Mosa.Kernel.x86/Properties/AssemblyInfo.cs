aa571a40142e7d550c7a5570fb126b13eeed862a
Phil Garcia <phil@thinkedge.com>
2019-06-22 23:15:14 -0700

    - WIP

--- Source/Mosa.Kernel.x86/Properties/AssemblyInfo.cs
+++ Source/Mosa.Kernel.x86/Properties/AssemblyInfo.cs
@@ -1,33 +1,31 @@
-﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
-
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mosa.Runtime.x86")]
+[assembly: AssemblyTitle("Mosa.Kernel.x86")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MOSA Project")]
-[assembly: AssemblyProduct("Mosa.Runtime.x86")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mosa.Kernel.x86")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9c75fcf6-5bee-42bc-89d1-6c4f61a4e9e4")]
+[assembly: Guid("2937a2ab-4d3a-4f4d-81a4-7c21c7d882f3")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
