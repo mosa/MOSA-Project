36b2786542fd8aadcb515e6f6685beb2708ce118
Phil Garcia <phil@thinkedge.com>
2019-08-25 07:46:46 -0700

    - WIP

--- Source/Mosa.Platform.x86/Intrinsic/SetSegments.cs
+++ Source/Mosa.Platform.x86/Intrinsic/SetSegments.cs
@@ -19,24 +19,17 @@ namespace Mosa.Platform.x86.Intrinsic
 			var operand4 = context.GetOperand(3);
 			var operand5 = context.GetOperand(4);
 
-			var EAX = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I4, GeneralPurposeRegister.EAX);
-
 			var ds = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I2, SegmentRegister.DS);
 			var es = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I2, SegmentRegister.ES);
 			var fs = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I2, SegmentRegister.FS);
 			var gs = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I2, SegmentRegister.GS);
 			var ss = Operand.CreateCPURegister(methodCompiler.TypeSystem.BuiltIn.I2, SegmentRegister.SS);
 
-			context.SetInstruction(X86.Mov32, EAX, operand1);
-			context.AppendInstruction(X86.MovStoreSeg32, ds, EAX);
-			context.AppendInstruction(X86.Mov32, EAX, operand2);
-			context.AppendInstruction(X86.MovStoreSeg32, es, EAX);
-			context.AppendInstruction(X86.Mov32, EAX, operand3);
-			context.AppendInstruction(X86.MovStoreSeg32, fs, EAX);
-			context.AppendInstruction(X86.Mov32, EAX, operand4);
-			context.AppendInstruction(X86.MovStoreSeg32, gs, EAX);
-			context.AppendInstruction(X86.Mov32, EAX, operand5);
-			context.AppendInstruction(X86.MovStoreSeg32, ss, EAX);
+			context.SetInstruction(X86.MovStoreSeg32, ds, operand1);
+			context.AppendInstruction(X86.MovStoreSeg32, es, operand2);
+			context.AppendInstruction(X86.MovStoreSeg32, fs, operand3);
+			context.AppendInstruction(X86.MovStoreSeg32, gs, operand4);
+			context.AppendInstruction(X86.MovStoreSeg32, ss, operand5);
 		}
 	}
 }
