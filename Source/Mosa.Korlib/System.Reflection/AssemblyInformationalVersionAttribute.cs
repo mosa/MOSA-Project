53e9e67497a8603f6add6bb751d0fed27e29bcbf
Stefan Andres Charsley <charsleysa@gmail.com>
2019-10-13 21:59:52 +1300

    Use NETSdk (WIP)

---
+++ Source/Mosa.Korlib/System.Reflection/AssemblyInformationalVersionAttribute.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+namespace System.Reflection
+{
+	[AttributeUsage(AttributeTargets.Assembly, Inherited = false)]
+	public sealed class AssemblyInformationalVersionAttribute : Attribute
+	{
+		public AssemblyInformationalVersionAttribute(string informationalVersion)
+		{
+			InformationalVersion = informationalVersion;
+		}
+
+		public string InformationalVersion { get; }
+	}
+}
