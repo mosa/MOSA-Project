309ce773ac877fe379f1728772a69a68a45d9b5e
Stefan Andres Charsley <charsleysa@gmail.com>
2019-10-31 17:33:28 +1300

    Fixes #626 (#734)
    * Fixes #626
    
    * Update FullNames

---
+++ Source/Mosa.Korlib/System.Runtime.CompilerServices/IsReadOnlyAttribute.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+namespace System.Runtime.CompilerServices
+{
+	/// <summary>
+	/// Reserved to be used by the compiler for tracking metadata.
+	/// This attribute should not be used by developers in source code.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.All, Inherited = false)]
+	public sealed class IsReadOnlyAttribute : Attribute
+	{
+		public IsReadOnlyAttribute()
+		{
+		}
+	}
+}
