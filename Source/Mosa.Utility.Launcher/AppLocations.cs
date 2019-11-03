a8a41c04080769ba4bcf34bc12afd3723cd20d8f
Sebastian Loncar <sebastian.loncar@gmail.com>
2019-11-03 12:15:10 +0100

    github actions

--- Source/Mosa.Utility.Launcher/AppLocations.cs
+++ Source/Mosa.Utility.Launcher/AppLocations.cs
@@ -239,6 +239,8 @@ namespace Mosa.Utility.Launcher
 				MsBuild = TryFind(
 					new string[] { "msbuild.exe", "msbuild" },
 					new string[] {
+						CombineParameterAndDirectory("ProgramFiles(x86)", @"Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\amd64"),
+						CombineParameterAndDirectory("ProgramFiles(x86)", @"Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin"),
 						CombineParameterAndDirectory("ProgramFiles(x86)", @"Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\amd64"),
 						CombineParameterAndDirectory("ProgramFiles(x86)", @"Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\amd64"),
 						CombineParameterAndDirectory("ProgramFiles(x86)", @"Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin"),
