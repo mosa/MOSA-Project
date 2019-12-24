6a5bae8b194eb1d5be4fc34c62d4c65ef3d1406d
Phil Garcia <phil@thinkedge.com>
2019-12-23 17:43:30 -0800

    - Monitor.Enter implementation (#750)

--- Source/Mosa.Korlib/System.Threading/Monitor.cs
+++ Source/Mosa.Korlib/System.Threading/Monitor.cs
@@ -9,7 +9,25 @@ namespace System.Threading
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
 		public static extern void Enter(Object obj);
 
+		public static void Enter(object obj, ref bool lockTaken)
+		{
+			if (lockTaken)
+				ThrowLockTakenException();
+
+			ReliableEnter(obj, ref lockTaken);
+
+			//Debug.Assert(lockTaken);
+		}
+
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
 		public static extern void Exit(Object obj);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		private static extern void ReliableEnter(Object obj, ref bool lockTaken);
+
+		private static void ThrowLockTakenException()
+		{
+			throw new ArgumentException("MustBeFalse", "lockTaken");
+		}
 	}
 }
---
+++ Source/Mosa.UnitTests/CompilerBugTests.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Mosa.UnitTests
+{
+	public static class CompilerBugTests
+	{
+		[MosaUnitTest]
+		public static bool Test()
+		{
+			ProcessManager.ProcessList = new List<Process>();
+			ProcessManager.ProcessList.Add(new Process { ProcessID = 0 });
+			ProcessManager.ProcessList.Add(new Process { ProcessID = 1 });
+			ProcessManager.ProcessList.Add(new Process { ProcessID = 2 });
+			ProcessManager.ProcessList.Add(new Process { ProcessID = 3 });
+
+			int targetProcessID = 2;
+			var proc = ProcessManager.GetProcess(targetProcessID);
+
+			return proc.ProcessID == 2;
+		}
+	}
+
+	internal static class ProcessManager
+	{
+		public static List<Process> ProcessList;
+
+		public static Process GetProcess(int processID)
+		{
+			lock (ProcessList)
+				for (var i = 0; i < ProcessList.Count; i++)
+					if (ProcessList[i].ProcessID == processID)
+						return ProcessList[i];
+			return null;
+		}
+	}
+
+	internal class Process : IDisposable
+	{
+		public int ProcessID;
+
+		public void Dispose()
+		{
+		}
+	}
+}
