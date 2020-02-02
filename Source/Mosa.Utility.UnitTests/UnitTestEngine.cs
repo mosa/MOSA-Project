54d5132911e9ba6b51a634b7e412df2a490626dd
Phil <phil@thinkedge.com>
2020-02-02 09:35:11 -0800

    - Updates

--- Source/Mosa.Utility.UnitTests/UnitTestEngine.cs
+++ Source/Mosa.Utility.UnitTests/UnitTestEngine.cs
@@ -102,7 +102,7 @@ namespace Mosa.Utility.UnitTests
 			Settings.SetValue("Emulator.Memory", 128);
 			Settings.SetValue("Emulator.Serial", "TCPServer");
 			Settings.SetValue("Emulator.Serial.Host", "127.0.0.1");
-			Settings.SetValue("Emulator.Serial.Port", new Random().Next(11111, 22222));
+			Settings.SetValue("Emulator.Serial.Port", "11110");
 			Settings.SetValue("Emulator.Serial.Pipe", "MOSA");
 			Settings.SetValue("Launcher.Start", false);
 			Settings.SetValue("Launcher.Launch", false);
