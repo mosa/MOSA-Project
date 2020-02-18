20722b4497c8fc9776f935f8eb248edc53008d89
Phil <phil@thinkedge.com>
2020-02-17 22:12:50 -0800

    - Updated Documentation

--- Source/Create-Docs.bat
+++ Source/Create-Docs.bat
@@ -1,9 +1,8 @@
 CD Docs
 CALL make html
-RMDIR /S /Q ..\..\docs
-CD build
-RENAME html docs
-MOVE docs ..\..\..\docs
-CD ..
+RMDIR /S /Q ..\..\docs\_static
+DEL /F /S /Q ..\..\docs\*.*
+DEL /F /S /Q ..\..\docs\images\*.*
+XCOPY /S build\html ..\..\docs\
 COPY /Y *.nojekyll ..\..\docs
 CD ..
