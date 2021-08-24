3f369d44d43dc24a068b3fc4830bc67efc23d0e3
AnErrupTion <ichicharka@gmail.com>
2021-08-24 23:48:55 +0200

    Fix vector tests

--- Source/Mosa.UnitTests/VectorTests.cs
+++ Source/Mosa.UnitTests/VectorTests.cs
@@ -12,85 +12,85 @@ namespace Mosa.UnitTests
 		[MosaUnitTest]
 		public static bool TestVector1()
 		{
-			_ = new Vector2(0.0, 0.0);
+			_ = new Vector2(0.0f, 0.0f);
 			return true;
 		}
 
-		[MosaUnitTest(Series = "R8R8")]
-		public static bool TestVector2(int x, int y)
+		[MosaUnitTest(Series = "R4R4")]
+		public static bool TestVector2(float x, float y)
 		{
 			var vector = new Vector2(x, y);
 
 			return vector.X == x && vector.Y == y;
 		}
 
-		[MosaUnitTest(Series = "R8R8")]
-		public static bool TestVector3(int x, int y)
+		[MosaUnitTest(Series = "R4R4")]
+		public static bool TestVector3(float x, float y)
 		{
-			var vector1 = new Vector2(x, 92.1);
-			var vector2 = new Vector2(74.0, y);
+			var vector1 = new Vector2(x, 92.1f);
+			var vector2 = new Vector2(74.0f, y);
 
 			vector2.X = vector1.Y;
 			vector1.X = vector2.Y;
 
-			return vector2.X == 92.1 && vector1.X == y;
+			return vector2.X == 92.1f && vector1.X == y;
 		}
 
 		[MosaUnitTest]
 		public static bool TestVector4()
 		{
-			_ = new Vector3(0.0, 0.0, 0.0);
+			_ = new Vector3(0.0f, 0.0f, 0.0f);
 			return true;
 		}
 
-		[MosaUnitTest(Series = "R8R8R8")]
-		public static bool TestVector5(int x, int y, int z)
+		[MosaUnitTest(Series = "R4R4R4")]
+		public static bool TestVector5(float x, float y, float z)
 		{
 			var vector = new Vector3(x, y, z);
 
 			return vector.X == x && vector.Y == y && vector.Z == z;
 		}
 
-		[MosaUnitTest(Series = "R8R8R8")]
-		public static bool TestVector6(int x, int y, int z)
+		[MosaUnitTest(Series = "R4R4R4")]
+		public static bool TestVector6(float x, float y, float z)
 		{
-			var vector1 = new Vector3(x, 92.1, z);
-			var vector2 = new Vector3(74.0, y, 36.7);
+			var vector1 = new Vector3(x, 92.1f, z);
+			var vector2 = new Vector3(74.0f, y, 36.7f);
 
 			vector2.X = vector1.Y;
 			vector1.X = vector2.Y;
 			vector2.Z = vector1.Z;
 
-			return vector2.X == 92.1 && vector1.X == y && vector2.Z == z;
+			return vector2.X == 92.1f && vector1.X == y && vector2.Z == z;
 		}
 
 		[MosaUnitTest]
 		public static bool TestVector7()
 		{
-			_ = new Vector4(0.0, 0.0, 0.0, 0.0);
+			_ = new Vector4(0.0f, 0.0f, 0.0f, 0.0f);
 			return true;
 		}
 
-		[MosaUnitTest(Series = "R8R8R8R8")]
-		public static bool TestVector8(int x, int y, int z, int w)
+		[MosaUnitTest(Series = "R4R4R4R4")]
+		public static bool TestVector8(float x, float y, float z, float w)
 		{
 			var vector = new Vector4(x, y, z, w);
 
 			return vector.X == x && vector.Y == y && vector.Z == z && vector.W == w;
 		}
 
-		[MosaUnitTest(Series = "R8R8R8R8")]
-		public static bool TestVector9(int x, int y, int z, int w)
+		[MosaUnitTest(Series = "R4R4R4R4")]
+		public static bool TestVector9(float x, float y, float z, float w)
 		{
-			var vector1 = new Vector4(x, 92.1, z, 25.8);
-			var vector2 = new Vector4(74.0, y, 36.7, w);
+			var vector1 = new Vector4(x, 92.1f, z, 25.8f);
+			var vector2 = new Vector4(74.0f, y, 36.7f, w);
 
 			vector2.X = vector1.Y;
 			vector1.X = vector2.Y;
 			vector2.Z = vector1.Z;
 			vector1.W = vector2.W;
 
-			return vector2.X == 92.1 && vector1.X == y && vector2.Z == z && vector1.W == w;
+			return vector2.X == 92.1f && vector1.X == y && vector2.Z == z && vector1.W == w;
 		}
 	}
 }
