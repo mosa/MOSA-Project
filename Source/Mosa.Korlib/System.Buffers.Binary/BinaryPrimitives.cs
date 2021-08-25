6eb2f58fa1b3361c91e7ef512bee36f80e53e9b5
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2021-08-25 02:32:56 +0200

    Add Vector2, Vector3 and Vector4 and public Pointer property for Spans (#852)
    * Add Vector2, Vector3 and Vector4
    
    * Implement Unsafe functions and MemoryMarshal
    
    * Fix vector tests
    
    * Remove Span.Pointer
    
    * Implement BinaryPrimitives and MemoryMarshal.Read()
    
    * Indent using tabs
    
    * Fix tests
    
    * Update VectorTests.cs
    
    * Update VectorTests.cs
    
    * add more R4 and R8 series
    
    * Update VectorTests.cs
    
    * Update VectorTests.cs
    
    * Update Combinations.cs
    
    Co-authored-by: Stefan Andres Charsley <charsleysa@gmail.com>

---
+++ Source/Mosa.Korlib/System.Buffers.Binary/BinaryPrimitives.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace System.Buffers.Binary
+{
+	public class BinaryPrimitives
+	{
+		public static ushort ReadUInt16BigEndian(ReadOnlySpan<byte> source)
+		{
+			ushort result = MemoryMarshal.Read<ushort>(source);
+
+			if (BitConverter.IsLittleEndian)
+				result = ReverseEndianness(result);
+
+			return result;
+		}
+
+		public static ushort ReverseEndianness(ushort value)
+		{
+			// Don't need to AND with 0xFF00 or 0x00FF since the final
+			// cast back to ushort will clear out all bits above [ 15 .. 00 ].
+			// This is normally implemented via "movzx eax, ax" on the return.
+			// Alternatively, the compiler could elide the movzx instruction
+			// entirely if it knows the caller is only going to access "ax"
+			// instead of "eax" / "rax" when the function returns.
+
+			return (ushort)((value >> 8) + (value << 8));
+		}
+	}
+}
