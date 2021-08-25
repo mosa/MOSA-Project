1bdfc4f33d7948e46ad9b7f61458dcb6d7e488c8
Stefan Andres Charsley <charsleysa@gmail.com>
2021-08-25 12:07:38 +1200

    Update VectorTests.cs

--- Source/Mosa.UnitTests/VectorTests.cs
+++ Source/Mosa.UnitTests/VectorTests.cs
@@ -43,25 +43,25 @@ namespace Mosa.UnitTests
 			return true;
 		}
 
-		[MosaUnitTest(Series = "R4R4")]
-		public static bool TestVector5(float x, float y)
+		[MosaUnitTest(Series = "R4R4R4")]
+		public static bool TestVector5(float x, float y, float z)
 		{
-			var vector = new Vector3(x, y, y);
+			var vector = new Vector3(x, y, z);
 
-			return vector.X == x && vector.Y == y && vector.Z == y;
+			return vector.X == x && vector.Y == y && vector.Z == z;
 		}
 
-		[MosaUnitTest(Series = "R4R4")]
-		public static bool TestVector6(float x, float y)
+		[MosaUnitTest(Series = "R4R4R4")]
+		public static bool TestVector6(float x, float y, float z)
 		{
-			var vector1 = new Vector3(x, 92.1f, y);
+			var vector1 = new Vector3(x, 92.1f, z);
 			var vector2 = new Vector3(74.0f, y, 36.7f);
 
 			vector2.X = vector1.Y;
 			vector1.X = vector2.Y;
 			vector2.Z = vector1.Z;
 
-			return vector2.X == 92.1f && vector1.X == y && vector2.Z == y;
+			return vector2.X == 92.1f && vector1.X == y && vector2.Z == z;
 		}
 
 		[MosaUnitTest]
@@ -71,26 +71,26 @@ namespace Mosa.UnitTests
 			return true;
 		}
 
-		[MosaUnitTest(Series = "R4R4")]
-		public static bool TestVector8(float x, float y)
+		[MosaUnitTest(Series = "R4R4R4R4")]
+		public static bool TestVector8(float x, float y, float z, float w)
 		{
-			var vector = new Vector4(x, y, x, y);
+			var vector = new Vector4(x, y, z, w);
 
-			return vector.X == x && vector.Y == y && vector.Z == x && vector.W == y;
+			return vector.X == x && vector.Y == y && vector.Z == z && vector.W == w;
 		}
 
-		[MosaUnitTest(Series = "R4R4")]
-		public static bool TestVector9(float x, float y)
+		[MosaUnitTest(Series = "R4R4R4R4")]
+		public static bool TestVector9(float x, float y, float z, float w)
 		{
-			var vector1 = new Vector4(x, 92.1f, y, 25.8f);
-			var vector2 = new Vector4(74.0f, y, 36.7f, x);
+			var vector1 = new Vector4(x, 92.1f, z, 25.8f);
+			var vector2 = new Vector4(74.0f, y, 36.7f, w);
 
 			vector2.X = vector1.Y;
 			vector1.X = vector2.Y;
 			vector2.Z = vector1.Z;
 			vector1.W = vector2.W;
 
-			return vector2.X == 92.1f && vector1.X == y && vector2.Z == y && vector1.W == x;
+			return vector2.X == 92.1f && vector1.X == y && vector2.Z == z && vector1.W == w;
 		}
 	}
 }
