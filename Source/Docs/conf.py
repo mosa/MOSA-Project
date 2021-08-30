a990b54d71cbd4485d96bfa73bd905be11705970
Phil <phil@thinkedge.com>
2021-08-29 23:13:30 -0700

    - Incorporating graphwiz into read-the-docs

--- Source/Docs/conf.py
+++ Source/Docs/conf.py
@@ -1,13 +1,13 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
+# This file only contains a selection of the most common options.  For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
+# add these directories to sys.path here.  If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 # import os
@@ -21,17 +21,16 @@ author = 'Phil Garcia'
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
+# Add any Sphinx extension module names here, as strings.  They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autosectionlabel',
+extensions = ['sphinx.ext.autosectionlabel',
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinxcontrib.httpdomain',
     'sphinx_tabs.tabs',
     'sphinx-prompt',
-]
+	'sphinx.ext.graphviz',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,7 +47,7 @@ exclude_patterns = []
 html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
+# relative to this directory.  They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
@@ -57,3 +56,6 @@ autosectionlabel_prefix_document = True
 
 def setup(app):
     app.add_css_file('css/sphinx_prompt_css.css')
+
+# -- GraphViz configuration ----------------------------------
+graphviz_output_format = 'svg'
---
+++ Source/Docs/runtime-tables.rst
@@ -0,0 +1,7 @@
+###################
+MOSA Runtime Tables
+###################
+
+This diagram represents the internal runtime tables within the MOSA virtual machine:
+
+.. graphviz:: MOSA-Runtime-Tables.dot
