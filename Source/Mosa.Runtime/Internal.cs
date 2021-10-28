e1bafb182d7f4edb5196fa723a256b37b87355b3
AnErrupTion <ichicharka@gmail.com>
2021-10-28 19:21:40 +0200

    Restore Internal.MemoryClear performance improvement

--- Source/Mosa.Runtime/Internal.cs
+++ Source/Mosa.Runtime/Internal.cs
@@ -240,8 +240,8 @@ namespace Mosa.Runtime
 		[MethodImpl(MethodImplOptions.NoInlining)]
 		public static void MemoryClear(Pointer dest, uint count, uint value = 0)
 		{
-			for (uint i = 0; i < count; i += 4)
-				dest.Store32(i, value);
+			for (uint i = 0; i < count; i++)
+				dest.Store8(i, (byte)value);
 		}
 
 		#endregion Memory Manipulation
