90e003800377c2bfb42b10917209fb28f4d8c973
Phil <phil@thinkedge.com>
2021-11-12 06:50:55 -0800

    - Minor updates to DeviceSystem

--- Source/Mosa.DeviceSystem/Bitmap.cs
+++ Source/Mosa.DeviceSystem/Bitmap.cs
@@ -6,6 +6,9 @@ namespace Mosa.DeviceSystem
 	{
 		public static Image CreateImage(byte[] data)
 		{
+			if (data[0] != (byte)'B' || data[1] != (byte)'M')
+				return null;
+
 			var bpp = data[0x1C] / 8;
 
 			if (!(bpp == 3 || bpp == 4))
