b71f7c5c4c34a3e9c6588777fbe10c8f969a777b
AnErrupTion <ichicharka@gmail.com>
2021-11-26 14:00:58 +0100

    Handle padding in Bitmap

--- Source/Mosa.DeviceSystem/Bitmap.cs
+++ Source/Mosa.DeviceSystem/Bitmap.cs
@@ -17,7 +17,7 @@ namespace Mosa.DeviceSystem
 			var stream = new ByteStream(data);
 
 			var width = stream.Read32(0x12);
-			var height = stream.Read32(0x16);
+			vardataSectionOffset height = stream.Read32(0x16);
 			var dataSectionOffset = stream.Read32(0xA);
 
 			var image = new Image(width, height, bpp);
@@ -25,9 +25,26 @@ namespace Mosa.DeviceSystem
 			uint x = 0;
 			uint y = height - 1;
 
-			for (var i = dataSectionOffset; i < dataSectionOffset + image.Pixels.Size; i += bpp)
+			var offset = dataSectionOffset + (width * height * 4);
+			for (var i = dataSectionOffset; i < offset; i += 4)
 			{
-				var color = (bpp == 4) ? stream.Read32((int)i) : (uint)(stream.Read24((int)i) | 0xFF000000); // 32 & 24 bit
+				uint color;
+
+				// This should handle padding for non 32-bit bitmaps.
+				// If i == offset, the series of bytes would look like this:
+				// 48 56 32 0 // That zero byte is the start of empty pad data
+				// If we remove, say, 1 to i (for 24-bit bitmaps), the series of bytes would now look like this:
+				// 48 56 32 // Notice how the zero byte is now gone
+				// However, we would also have to read depending if the series of bytes is now
+				// 3-byte, 2-byte of 1-byte aligned (Read24(), Read16() and Read8() respectively)
+				if (bpp < 4 && i == offset)
+				{
+					i -= 4 - bpp;
+
+					// TODO: Other color depths
+					color = (i == 1) ? (uint)(stream.Read24(i) | 0xFF000000) : 0;
+				}
+				else color = (bpp == 4) ? stream.Read32(i) : (uint)(stream.Read24(i) | 0xFF000000); // 32 & 24 bit
 
 				image.SetColor(x, y, color);
 
--- Source/Mosa.DeviceSystem/ByteStream.cs
+++ Source/Mosa.DeviceSystem/ByteStream.cs
@@ -11,22 +11,22 @@ namespace Mosa.DeviceSystem
 			this.data = data;
 		}
 
-		public byte Read8(int offset)
+		public byte Read8(uint offset)
 		{
 			return data[offset];
 		}
 
-		public ushort Read16(int offset)
+		public ushort Read16(uint offset)
 		{
 			return (ushort)(data[offset] | data[offset + 1] << 8);
 		}
 
-		public uint Read24(int offset)
+		public uint Read24(uint offset)
 		{
 			return (uint)(data[offset] | data[offset + 1] << 8 | data[offset + 2] << 16);
 		}
 
-		public uint Read32(int offset)
+		public uint Read32(uint offset)
 		{
 			return (uint)(data[offset] | data[offset + 1] << 8 | data[offset + 2] << 16 | data[offset + 3] << 24);
 		}
