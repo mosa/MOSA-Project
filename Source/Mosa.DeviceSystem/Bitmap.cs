10392cff7d21eb2f55c1e745214f835aaf6a79c0
AnErrupTion <ichicharka@gmail.com>
2021-12-03 20:20:23 +0100

    Update Bitmap logic

--- Source/Mosa.DeviceSystem/Bitmap.cs
+++ Source/Mosa.DeviceSystem/Bitmap.cs
@@ -22,39 +22,19 @@ namespace Mosa.DeviceSystem
 
 			var image = new Image(width, height, bpp);
 
-			uint x = 0;
-			uint y = height - 1;
-
-			var offset = dataSectionOffset + (width * height * 4);
-			for (var i = dataSectionOffset; i < offset; i += 4)
+			// TODO: Support for other color depths
+			for (uint y = 0; y < height - 1; y++)
 			{
-				uint color;
-
-				// This should handle padding for non 32-bit bitmaps.
-				// If i == offset, the series of bytes would look like this:
-				// 48 56 32 0 // That zero byte is the start of empty pad data
-				// If we remove, say, 1 to i (for 24-bit bitmaps), the series of bytes would now look like this:
-				// 48 56 32 // Notice how the zero byte is now gone
-				// However, we would also have to read depending if the series of bytes is now
-				// 3-byte, 2-byte of 1-byte aligned (Read24(), Read16() and Read8() respectively)
-				if (bpp < 4 && i == offset)
+				for (uint x = 0; x < width - 1; x++)
 				{
-					i -= 4 - bpp;
+					dataSectionOffset += x * bpp;
 
-					// TODO: Other color depths
-					color = (i == 1) ? (uint)(stream.Read24(i) | 0xFF000000) : 0;
+					var color = bpp == 4 ? stream.Read32(dataSectionOffset) : stream.Read24(dataSectionOffset) | 0xFF000000;
+					image.SetColor(x, y, color);
 				}
-				else color = (bpp == 4) ? stream.Read32(i) : (uint)(stream.Read24(i) | 0xFF000000); // 32 & 24 bit
 
-				image.SetColor(x, y, color);
-
-				x++;
-
-				if (x == width)
-				{
-					x = 0;
-					y--;
-				}
+				if (bpp == 3 && width * bpp % 4 > 0)
+					dataSectionOffset += 4;
 			}
 
 			return image;
