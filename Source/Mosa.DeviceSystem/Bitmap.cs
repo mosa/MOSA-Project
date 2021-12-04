ba57e61624806800dc064d8bd8ea44646b35c3eb
AnErrupTion <ichicharka@gmail.com>
2021-12-04 13:35:49 +0100

    Update Bitmap logic

--- Source/Mosa.DeviceSystem/Bitmap.cs
+++ Source/Mosa.DeviceSystem/Bitmap.cs
@@ -9,9 +9,9 @@ namespace Mosa.DeviceSystem
 			if (data[0] != (byte)'B' || data[1] != (byte)'M')
 				return null;
 
-			var bpp = (uint)(data[0x1C] / 8);
+			var bytesPerPixel = (uint)(data[0x1C] / 8);
 
-			if (bpp != 3 && bpp != 4)
+			if (bytesPerPixel != 3 && bytesPerPixel != 4)
 				return null;
 
 			var stream = new ByteStream(data);
@@ -20,20 +20,20 @@ namespace Mosa.DeviceSystem
 			var height = stream.Read32(0x16);
 			var dataSectionOffset = stream.Read32(0xA);
 
-			var image = new Image(width, height, bpp);
+			var image = new Image(width, height, bytesPerPixel);
 
 			// TODO: Support for other color depths
-			for (uint y = 0; y < height - 1; y++)
+			for (var y = height - 1; y > 0; y--)
 			{
 				for (uint x = 0; x < width - 1; x++)
 				{
-					dataSectionOffset += x * bpp;
+					dataSectionOffset += bytesPerPixel;
 
-					var color = bpp == 4 ? stream.Read32(dataSectionOffset) : stream.Read24(dataSectionOffset) | 0xFF000000;
+					var color = bytesPerPixel == 4 ? stream.Read32(dataSectionOffset) : stream.Read24(dataSectionOffset) | 0xFF000000;
 					image.SetColor(x, y, color);
 				}
 
-				if (bpp == 3 && width * bpp % 4 > 0)
+				if (bytesPerPixel == 3 && width * bytesPerPixel % 4 > 0)
 					dataSectionOffset += 4;
 			}
 
