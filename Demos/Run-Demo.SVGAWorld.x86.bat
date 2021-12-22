3fb1409c72e370734d9a8b3b2a849eb765e71d3f
AnErrupTion <anerruption@disroot.org>
2021-12-22 18:14:33 +0100

    Fix files in Demos folder

---
+++ Demos/Run-Demo.SVGAWorld.x86.bat
@@ -0,0 +1,4 @@
+cd %~dp0
+cd ..\bin
+start Mosa.Tool.Launcher.exe -autostart -vmware -output-map -output-asm -output-debug -grub2.00 -iso Mosa.Demo.SVGAWorld.x86.dll
+
--- Demos/Run-Demo.VBEWorld.x86.bat
+++
@@ -1,4 +0,0 @@
-cd %~dp0
-cd ..\bin
-start Mosa.Tool.Launcher.exe -autostart -vmware -output-map -output-asm -output-debug -grub2.00 -iso Mosa.Demo.VBEWorld.x86.dll
-
