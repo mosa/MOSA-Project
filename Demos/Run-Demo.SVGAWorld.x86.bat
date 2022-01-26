c359ea4adfb7d400ce6b499ea7878e406404b057
AnErrupTion <anerruption@disroot.org>
2022-01-26 06:50:06 +0100

    Changes

--- Demos/Run-Demo.SVGAWorld.x86.bat
+++ Demos/Run-Demo.SVGAWorld.x86.bat
@@ -1,4 +1,4 @@
 cd %~dp0
 cd ..\bin
-start Mosa.Tool.Launcher.exe -autostart -include Source/Include -vmware -output-map -output-asm -output-debug Mosa.Demo.SVGAWorld.x86.dll
+start Mosa.Tool.Launcher.exe -autostart -include Source/Mosa.Demo.SVGAWorld.x86/Include -vmware -output-map -output-asm -output-debug Mosa.Demo.SVGAWorld.x86.dll
 
