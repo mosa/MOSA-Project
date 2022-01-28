36d99f5c73833c29c85e84b4f8d59187a336ba61
Phil <phil@thinkedge.com>
2022-01-27 18:32:33 -0800

    - WIP

---
+++ Source/Mosa.UnitTests/Other/InterestingTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+namespace Mosa.UnitTests.Other
+{
+	public static class InterestingTests
+
+	{
+		public struct Struct
+		{ public int X { get; set; } }
+
+		public static int Slow()
+		{
+			Struct a = new Struct();
+
+			for (int i = 0; i < 10000000; i++)
+			{
+				a.X += 1;
+			}
+
+			return a.X;
+		}
+
+		public static int Fast()
+		{
+			Struct a = new Struct();
+
+			for (int i = 0; i < 10000000; i++)
+			{
+				a.X = a.X + 1;
+			}
+
+			return a.X;
+		}
+	}
+}
