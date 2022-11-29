c97e0ad1dc2ceb6dac03de6ead27c8a0382fffcf
AnErrupTion <anerruption@disroot.org>
2022-11-29 20:47:23 +0100

    Remove useless 'start' commands in batch files

--- Demos/Run-Demo.CoolWorld.x86.bat
+++ Demos/Run-Demo.CoolWorld.x86.bat
@@ -1,3 +1,3 @@
 cd %~dp0
 cd ..\bin
-start Mosa.Tool.Launcher.exe -autostart -output-map -output-asm -output-debug -output-inlined Mosa.Demo.CoolWorld.x86.dll
+Mosa.Tool.Launcher.exe -autostart -output-map -output-asm -output-debug -output-inlined Mosa.Demo.CoolWorld.x86.dll
