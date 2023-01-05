c6a1cdcb1a93b526d94ee56ab527713af62af59b
Phil <phil@thinkedge.com>
2023-01-04 19:38:37 -0800

    - Lower32 optimizations

---
+++ Source/Mosa.Compiler.Framework/Transforms/Optimizations/Manual/LowerTo32/ShiftLeft64By32Plus.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+namespace Mosa.Compiler.Framework.Transforms.Optimizations.Manual.LowerTo32
+{
+	public sealed class ShiftLeft64By32Plus : BaseTransform
+	{
+		public ShiftLeft64By32Plus() : base(IRInstruction.ShiftLeft64, TransformType.Manual | TransformType.Optimization)
+		{
+		}
+
+		public override bool Match(Context context, TransformContext transform)
+		{
+			return transform.LowerTo32 && context.Operand2.IsResolvedConstant && context.Operand2.ConstantUnsigned32 > 32;
+		}
+
+		public override void Transform(Context context, TransformContext transform)
+		{
+			var result = context.Result;
+			var operand1 = context.Operand1;
+			var shift = context.Operand2.ConstantUnsigned32;
+
+			var v1 = transform.AllocateVirtualRegister32();
+			var v2 = transform.AllocateVirtualRegister32();
+
+			context.SetInstruction(IRInstruction.GetLow32, v1, operand1);
+			context.AppendInstruction(IRInstruction.ShiftLeft32, v2, v1, transform.CreateConstant(shift - 32));
+			context.AppendInstruction(IRInstruction.To64, result, transform.Constant32_0, v2);
+		}
+	}
+}
---
+++ Source/Mosa.Compiler.Framework/Transforms/Optimizations/Manual/LowerTo32/ShiftRight64By32.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+namespace Mosa.Compiler.Framework.Transforms.Optimizations.Manual.LowerTo32
+{
+	public sealed class ShiftRight64By32 : BaseTransform
+	{
+		public ShiftRight64By32() : base(IRInstruction.ShiftRight64, TransformType.Manual | TransformType.Optimization)
+		{
+		}
+
+		public override bool Match(Context context, TransformContext transform)
+		{
+			return transform.LowerTo32 && context.Operand2.IsResolvedConstant && context.Operand2.ConstantUnsigned32 == 32;
+		}
+
+		public override void Transform(Context context, TransformContext transform)
+		{
+			var result = context.Result;
+			var operand1 = context.Operand1;
+
+			var v1 = transform.AllocateVirtualRegister32();
+
+			context.SetInstruction(IRInstruction.GetHigh32, v1, operand1);
+			context.AppendInstruction(IRInstruction.To64, result, v1, transform.Constant32_0);
+		}
+	}
+}
