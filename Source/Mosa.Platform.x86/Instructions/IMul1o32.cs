6f5a311807e630e6331f27b470eefd5abd7f1bb1
Phil Garcia <phil@thinkedge.com>
2023-01-14 20:45:13 -0800

    Shrink opcodes + IMul (#1004)
    * - Shrink opcodes
    
    * - Shrink opcodes
    
    * - Shrink opcodes
    
    * - Added IMul opcodes (#1002)

--- Source/Mosa.Platform.x86/Instructions/IMul1o32.cs
+++ Source/Mosa.Platform.x86/Instructions/IMul1o32.cs
@@ -7,13 +7,13 @@ using Mosa.Compiler.Framework;
 namespace Mosa.Platform.x86.Instructions
 {
 	/// <summary>
-	/// IMul32
+	/// IMul1o32
 	/// </summary>
 	/// <seealso cref="Mosa.Platform.x86.X86Instruction" />
-	public sealed class IMul32 : X86Instruction
+	public sealed class IMul1o32 : X86Instruction
 	{
-		internal IMul32()
-			: base(1, 2)
+		internal IMul1o32()
+			: base(2, 2)
 		{
 		}
 
@@ -35,18 +35,17 @@ namespace Mosa.Platform.x86.Instructions
 
 		public override void Emit(InstructionNode node, OpcodeEncoder opcodeEncoder)
 		{
-			System.Diagnostics.Debug.Assert(node.ResultCount == 1);
+			System.Diagnostics.Debug.Assert(node.ResultCount == 2);
 			System.Diagnostics.Debug.Assert(node.OperandCount == 2);
 			System.Diagnostics.Debug.Assert(node.Result.IsCPURegister);
 			System.Diagnostics.Debug.Assert(node.Operand1.IsCPURegister);
 			System.Diagnostics.Debug.Assert(node.Result.Register == node.Operand1.Register);
 
-			if (node.Operand1.IsCPURegister && node.Operand2.IsCPURegister)
+			if ((node.Operand1.IsCPURegister && node.Operand1.Register.RegisterCode == 0) && node.Operand2.IsCPURegister)
 			{
-				opcodeEncoder.Append8Bits(0x0F);
-				opcodeEncoder.Append8Bits(0xAF);
+				opcodeEncoder.Append8Bits(0xF7);
 				opcodeEncoder.Append2Bits(0b11);
-				opcodeEncoder.Append3Bits(node.Result.Register.RegisterCode);
+				opcodeEncoder.Append3Bits(0b101);
 				opcodeEncoder.Append3Bits(node.Operand2.Register.RegisterCode);
 				return;
 			}
