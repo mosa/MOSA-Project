950cd07797a938f1f372327eb01bdfc774a0e1e3
AnErrupTion <anerruption@disroot.org>
2023-01-23 16:30:31 +0100

    Add unversioned files

---
+++ Demos/Run-Demo.SVGAWorld.x86.bat
@@ -0,0 +1,3 @@
+cd %~dp0
+cd ..\bin
+Mosa.Tool.Launcher -autostart -vmdk -vmware-svga -include Include Mosa.Demo.SVGAWorld.x86.dll
---
+++ Demos/Run-Demo.SVGAWorld.x86.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ../bin
+./Mosa.Tool.Launcher.Console -autostart -vmdk -vmware-svga -include Include Mosa.Demo.SVGAWorld.x86.dll
