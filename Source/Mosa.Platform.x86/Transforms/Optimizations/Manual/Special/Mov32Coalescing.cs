0fd4f410fbb872f3fff5f1c9121665aff72cd253
Phil Garcia <phil@thinkedge.com>
2023-03-11 21:37:55 -0800

    - Fixes (#1027)

--- Source/Mosa.Platform.x86/Transforms/Optimizations/Manual/Special/Mov32Coalescing.cs
+++ Source/Mosa.Platform.x86/Transforms/Optimizations/Manual/Special/Mov32Coalescing.cs
@@ -1,13 +1,12 @@
 ﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
 
 using Mosa.Compiler.Framework;
-using Mosa.Compiler.Framework.Transforms;
 
 namespace Mosa.Platform.x86.Transforms.Optimizations.Manual.Special;
 
-public sealed class Mov32Propagation : BaseTransform
+public sealed class Mov32Coalescing : BaseTransform
 {
-	public Mov32Propagation() : base(X86.Mov32, TransformType.Manual | TransformType.Optimization)
+	public Mov32Coalescing() : base(X86.Mov32, TransformType.Manual | TransformType.Optimization)
 	{
 	}
 
