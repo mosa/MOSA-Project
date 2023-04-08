4a3da633873290e92c5dc76cc7486b813084bfaa
AnErrupTion <anerruption@disroot.org>
2023-04-08 21:17:46 +0200

    Remove Inno Setup installer

--- Source/Inno-Setup-Script/create-installer.bat
+++
@@ -1,4 +0,0 @@
-rem set INNOSETUP=C:\Program Files (x86)\Inno Setup 5\ISCC.exe
-set INNOSETUP=C:\Program Files (x86)\Inno Setup 6\ISCC.exe
-
-if exist "%INNOSETUP%" "%INNOSETUP%" /DMyAppVersion=2.3.0 Mosa-Installer.iss
