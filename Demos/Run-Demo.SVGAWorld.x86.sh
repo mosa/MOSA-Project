9cc26405d96ec5e21f799a0b4473bfa5244db85e
AnErrupTion <anerruption@disroot.org>
2023-04-08 21:09:10 +0200

    Remove -cilv2 argument (no longer needed)

--- Demos/Run-Demo.SVGAWorld.x86.sh
+++ Demos/Run-Demo.SVGAWorld.x86.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ../bin
-./Mosa.Tool.Launcher.Console -autostart -oMax -output-asm -output-debug -output-hash -vmdk -vmware-svga -include Include Mosa.Demo.SVGAWorld.x86.dll -cilv2
+./Mosa.Tool.Launcher.Console -autostart -oMax -output-asm -output-debug -output-hash -vmdk -vmware-svga -include Include Mosa.Demo.SVGAWorld.x86.dll
