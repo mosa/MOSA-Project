64563186ec3b809d9befecfbc68f7dbe1125d78f
Phil <phil@thinkedge.com>
2023-05-20 08:36:18 -0700

    -Unwind ManagedPointer changes

--- Source/Mosa.Compiler.Framework/Stages/CILDecoderStage.cs
+++ Source/Mosa.Compiler.Framework/Stages/CILDecoderStage.cs
@@ -1276,28 +1276,19 @@ public sealed class CILDecoderStage : BaseMethodCompilerStage
 					return true;
 				}
 
-			case PrimitiveType.ManagedPointer when entry2.PrimitiveType == PrimitiveType.ManagedPointer:
-				{
-					var result = MethodCompiler.VirtualRegisters.AllocateManagedPointer();
-					context.AppendInstruction(IRInstruction.AddManagedPointer, result, entry1.Operand, entry2.Operand);
-					PushStack(stack, new StackEntry(result));
-					return true;
-				}
-
 			case PrimitiveType.Int32 when entry2.PrimitiveType == PrimitiveType.ManagedPointer && Is32BitPlatform:
 			case PrimitiveType.ManagedPointer when entry2.PrimitiveType == PrimitiveType.Int32 && Is32BitPlatform:
 				{
 					var result = MethodCompiler.VirtualRegisters.AllocateManagedPointer();
-					context.AppendInstruction(IRInstruction.AddManagedPointer, result, entry2.Operand, entry1.Operand);
+					context.AppendInstruction(IRInstruction.Add32, result, entry2.Operand, entry1.Operand);
 					PushStack(stack, new StackEntry(result));
 					return true;
 				}
 
 			case PrimitiveType.Int64 when entry2.PrimitiveType == PrimitiveType.ManagedPointer && Is64BitPlatform:
-			case PrimitiveType.ManagedPointer when entry2.PrimitiveType == PrimitiveType.Int64 && Is64BitPlatform:
 				{
 					var result = MethodCompiler.VirtualRegisters.AllocateManagedPointer();
-					context.AppendInstruction(IRInstruction.AddManagedPointer, result, entry2.Operand, entry1.Operand);
+					context.AppendInstruction(IRInstruction.Add64, result, entry2.Operand, entry1.Operand);
 					PushStack(stack, new StackEntry(result));
 					return true;
 				}
@@ -1307,7 +1298,7 @@ public sealed class CILDecoderStage : BaseMethodCompilerStage
 					var result = MethodCompiler.VirtualRegisters.AllocateManagedPointer();
 					var v1 = MethodCompiler.VirtualRegisters.Allocate64();
 					context.AppendInstruction(IRInstruction.SignExtend32x64, v1, entry1.Operand);
-					context.AppendInstruction(IRInstruction.AddManagedPointer, result, v1, entry2.Operand);
+					context.AppendInstruction(IRInstruction.Add64, result, v1, entry2.Operand);
 					PushStack(stack, new StackEntry(result));
 					return true;
 				}
@@ -1317,7 +1308,7 @@ public sealed class CILDecoderStage : BaseMethodCompilerStage
 					var result = MethodCompiler.VirtualRegisters.AllocateManagedPointer();
 					var v1 = MethodCompiler.VirtualRegisters.Allocate64();
 					context.AppendInstruction(IRInstruction.SignExtend32x64, v1, entry2.Operand);
-					context.AppendInstruction(IRInstruction.AddManagedPointer, result, entry1.Operand, v1);
+					context.AppendInstruction(IRInstruction.Add64, result, entry1.Operand, v1);
 					PushStack(stack, new StackEntry(result));
 					return true;
 				}
@@ -1850,6 +1841,7 @@ public sealed class CILDecoderStage : BaseMethodCompilerStage
 			default:
 
 				// TODO: Managed Pointers
+
 				return false;
 		}
 	}
@@ -3360,10 +3352,6 @@ public sealed class CILDecoderStage : BaseMethodCompilerStage
 		if (type.IsReferenceType)
 		{
 			context.AppendInstruction(IRInstruction.StoreObject, null, entry.Operand, ConstantZero, Operand.NullObject);
-		}
-		else if (type.IsManagedPointer)
-		{
-			context.AppendInstruction(IRInstruction.StoreManagedPointer, null, entry.Operand, ConstantZero, Operand.NullObject);
 		}
 		else
 		{
@@ -4891,13 +4879,6 @@ public sealed class CILDecoderStage : BaseMethodCompilerStage
 				var staticReference = Operand.CreateLabel(symbol.Name, Is32BitPlatform);
 
 				context.AppendInstruction(IRInstruction.StoreObject, null, staticReference, ConstantZero, source);
-			}
-			else if (type.IsManagedPointer)
-			{
-				var symbol = GetStaticSymbol(field);
-				var staticReference = Operand.CreateLabel(symbol.Name, Is32BitPlatform);
-
-				context.AppendInstruction(IRInstruction.StoreManagedPointer, null, staticReference, ConstantZero, source);
 			}
 			else
 			{
