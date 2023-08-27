e5d94b4bef97ae9428c33516a3d604a5fb244721
Phil <phil@thinkedge.com>
2023-08-27 08:46:48 -0700

    - Removed legacy x86 Kernel

--- Source/Mosa.Templates/templates/Mosa.Starter.x86/Boot.cs
+++ Source/Mosa.Templates/templates/Mosa.Starter.x86/Boot.cs
@@ -1,36 +1,21 @@
 ﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
 
-using Mosa.DeviceSystem;
-using Mosa.Kernel.x86;
-using Mosa.Runtime.Plug;
+using Mosa.Kernel.BareMetal;
 
 namespace Mosa.Starter.x86;
 
 public static class Boot
 {
-	[Plug("Mosa.Runtime.StartUp::SetInitialMemory")]
-	public static void SetInitialMemory()
-	{
-		KernelMemory.SetInitialMemory(Address.GCInitialMemory, 0x01000000);
-	}
-
 	public static void Main()
 	{
-		#region Initialization
+		Debug.WriteLine("Boot::Main()");
+		Debug.WriteLine("MOSA x86 Kernel");
 
-		Kernel.x86.Kernel.Setup();
-		IDT.SetInterruptHandler(ProcessInterrupt);
-
-		#endregion
-
-		Program.Setup();
-
-		for (; ; ) Program.Loop();
+		Program.EntryPoint();
 	}
 
-	private static void ProcessInterrupt(uint interrupt, uint errorCode)
+	public static void Include()
 	{
-		if (interrupt >= 0x20 && interrupt < 0x30)
-			HAL.ProcessInterrupt((byte)(interrupt - 0x20));
+		Kernel.BareMetal.x86.Scheduler.SwitchToThread(null);
 	}
 }
