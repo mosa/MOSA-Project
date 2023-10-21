8dfa87d189caf888f6410107c667673fabd81acf
Phil <phil@thinkedge.com>
2023-10-20 21:29:43 -0700

    - WIP

--- Source/Mosa.Compiler.Framework/Transforms/LowerTo32/MulUnsigned64.cs
+++ Source/Mosa.Compiler.Framework/Transforms/LowerTo32/MulUnsigned64.cs
@@ -10,6 +10,8 @@ public sealed class MulUnsigned64 : BaseLower32Transform
 
 	public override void Transform(Context context, TransformContext transform)
 	{
+		// same code as with MulUnsigned64!
+
 		var result = context.Result;
 		var operand1 = context.Operand1;
 		var operand2 = context.Operand2;
@@ -31,28 +33,16 @@ public sealed class MulUnsigned64 : BaseLower32Transform
 		context.AppendInstruction(IRInstruction.GetLow32, op1Low, operand2);
 		context.AppendInstruction(IRInstruction.GetHigh32, op1High, operand2);
 
-		//mov eax, DWORD PTR[esp + 8]     // eax =op0Low
-		//mov edx, DWORD PTR[esp + 16]    // edx = op0High
-		//mov ecx, DWORD PTR[esp + 12]    // ecx = op1Low
-		//mov ebx, DWORD PTR[esp + 20]    // ebx = op1High
+		context.SetInstruction(IRInstruction.GetLow32, op0Low, operand1);
+		context.AppendInstruction(IRInstruction.GetHigh32, op0High, operand1);
+		context.AppendInstruction(IRInstruction.GetLow32, op1Low, operand2);
+		context.AppendInstruction(IRInstruction.GetHigh32, op1High, operand2);
 
-		// imul ebx (op1High), eax (op0Low)
 		context.AppendInstruction(IRInstruction.MulSigned32, v1, op1High, op0Low);
-
-		// imul ecx (op1Low), edx (op0High)
 		context.AppendInstruction(IRInstruction.MulSigned32, v2, op1Low, op0High);
-
-		// mul edx (op0High), eax (op0Low)
-		context.AppendInstruction(IRInstruction.MulHu32, v3, op0High, op0Low);
-		// v3 = edx
-
-		context.AppendInstruction(IRInstruction.MulUnsigned32, v6, op0High, op0Low);
-		// v6 = eax
-
-		// add ecx (v2), ebx (v1)
+		context.AppendInstruction(IRInstruction.MulHu32, v3, op1Low, op0Low);
+		context.AppendInstruction(IRInstruction.MulUnsigned32, v6, op1Low, op0Low);
 		context.AppendInstruction(IRInstruction.Add32, v4, v2, v1);
-
-		// add edx (v3), ecx (v4)
 		context.AppendInstruction(IRInstruction.Add32, v5, v3, v4);
 
 		context.AppendInstruction(IRInstruction.To64, result, v6, v5);
