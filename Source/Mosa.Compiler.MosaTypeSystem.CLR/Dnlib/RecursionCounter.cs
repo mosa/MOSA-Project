74f6cd36c81c9921d1d1b17bf37ad70b789a3e1b
Phil <phil@thinkedge.com>
2023-10-29 11:42:17 -0700

    - Cleanup

--- Source/Mosa.Compiler.MosaTypeSystem.CLR/Dnlib/RecursionCounter.cs
+++ Source/Mosa.Compiler.MosaTypeSystem.CLR/Dnlib/RecursionCounter.cs
@@ -4,6 +4,10 @@
 // "I don't plan on making that type public ever again. If you need to use it,
 // you can just copy the source code to your project, it's not a lot of code."
 
+#if DEBUG
+using Mosa.Compiler.Common.Exceptions;
+#endif
+
 namespace dnlib.DotNet;
 
 /// <summary>
