88e9c2e3698c3faecb440be21a71927609c19808
Phil <phil@thinkedge.com>
2023-11-20 01:06:11 -0800

    - WIP

--- Source/Mosa.Utility.SourceCodeGenerator/Program.cs
+++ Source/Mosa.Utility.SourceCodeGenerator/Program.cs
@@ -224,6 +224,12 @@ public static class Program
 			.Execute();
 
 		new BuildTransformations(
+			Path.Combine(dataPath, @"X64-Optimizations-ConstantMove.json"),
+			Path.Combine(targetPath, @"Mosa.Compiler.x64\Transforms\Optimizations\Auto"),
+			"Mosa.Compiler.x64.Transforms.Optimizations.Auto")
+			.Execute();
+
+		new BuildTransformations(
 			Path.Combine(dataPath, @"X64-Optimizations-Ordering.json"),
 			Path.Combine(targetPath, @"Mosa.Compiler.x64\Transforms\Optimizations\Auto"),
 			"Mosa.Compiler.x64.Transforms.Optimizations.Auto")
