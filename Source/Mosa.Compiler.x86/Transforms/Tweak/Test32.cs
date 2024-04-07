b56938ad8ed9569eeb7f741858e33c4a38f3eae7
Phil Garcia <phil@thinkedge.com>
2024-04-06 20:04:03 -0700

    - Fix (#1214)

---
+++ Source/Mosa.Compiler.x86/Transforms/Tweak/Test32.cs
@@ -0,0 +1,31 @@
+// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+using Mosa.Compiler.Framework;
+
+namespace Mosa.Compiler.x86.Transforms.Tweak;
+
+/// <summary>
+/// Test32
+/// </summary>
+public sealed class Test32 : BaseTransform
+{
+	public Test32() : base(X86.Test32, TransformType.Manual | TransformType.Transform)
+	{
+	}
+
+	public override bool Match(Context context, Transform transform)
+	{
+		if (!context.Operand1.IsConstant)
+			return false;
+
+		if (context.Operand1.IsPhysicalRegister)
+			return false;
+
+		return true;
+	}
+
+	public override void Transform(Context context, Transform transform)
+	{
+		transform.MoveOperand1ToVirtualRegister(context, X86.Mov32);
+	}
+}
