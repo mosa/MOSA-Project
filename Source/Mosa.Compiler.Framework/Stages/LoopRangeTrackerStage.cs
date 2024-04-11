08aacb3aa1472b207fbb30d6b6d7997fb2d7f173
Phil <phil@thinkedge.com>
2024-04-11 03:51:55 -0700

    - WIP

--- Source/Mosa.Compiler.Framework/Stages/LoopRangeTrackerStage.cs
+++ Source/Mosa.Compiler.Framework/Stages/LoopRangeTrackerStage.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
 
 using Mosa.Compiler.Framework.Analysis;
+using Mosa.Compiler.Framework.Instructions;
 
 namespace Mosa.Compiler.Framework.Stages;
 
@@ -109,38 +110,127 @@ public sealed class LoopRangeTrackerStage : BaseMethodCompilerStage
 
 		var d = y.Definitions[0];
 
-		// Future: determine direction base on IR.Add or IR.Sub and constant
-
-		if (!(d.Instruction == IR.Add32 || d.Instruction == IR.Add64))
+		if (!(d.Instruction == IR.Add32
+			|| d.Instruction == IR.Add64
+			|| d.Instruction == IR.Sub32
+			|| d.Instruction == IR.Sub64))
 			return;
+
+		var direction = d.Instruction == IR.Add32 || d.Instruction == IR.Add64;
+
+		var incrementValueOperand = d.Operand2;
+		var incrementVariableOperand = d.Operand1;
 
 		if (d.Result != y)
 			return;
 
-		if (d.Operand1 != result)
+		if (incrementVariableOperand != result)
 			return;
 
-		if (!d.Operand2.IsResolvedConstant)
-			return;
-
-		if (d.Operand2.ConstantUnsigned64 <= 0)
+		if (!incrementValueOperand.IsResolvedConstant)
 			return;
 
 		if (!loop.LoopBlocks.Contains(d.Block))
 			return;
 
-		result.BitValue.NarrowMin(x.ConstantUnsigned64);
+		if (incrementValueOperand.ConstantSigned64 == 0)
+			return;
 
-		trace?.Log($"{result} MinValue = {x.ConstantUnsigned64}");
-		MinDetermined.Increment();
+		var signedAnalysis = DetermineSignUsage(incrementVariableOperand);
 
-		if (DetermineMaxOut(d.Operand1, d.Operand2, loop, out var max))
+		if (signedAnalysis == SignedAnalysis.Unknown || signedAnalysis == SignedAnalysis.Both)
+			return;
+
+		bool signed = signedAnalysis == SignedAnalysis.Signed;
+
+		if ((signed && incrementValueOperand.IsNegative)
+			|| (!signed && incrementValueOperand.IsNegative))
 		{
-			result.BitValue.NarrowMax((ulong)max);
-
-			trace?.Log($"{result} MaxValue = {max}");
-			MaxDetermined.Increment();
+			direction = !direction;
 		}
+
+		var startOperand = x;
+
+		if (direction)
+		{
+			if (!signed || (signed && !startOperand.IsNegative))
+			{
+				var start = startOperand.ConstantUnsigned64;
+
+				result.BitValue.NarrowMin(start);
+
+				trace?.Log($"{result} MinValue = {start}");
+				MinDetermined.Increment();
+			}
+
+			if (DetermineMaxOut(incrementVariableOperand, incrementValueOperand, loop, out var max))
+			{
+				if (max >= 0 || !signed)
+				{
+					result.BitValue.NarrowMax((ulong)max);
+
+					trace?.Log($"{result} MaxValue = {max}");
+					MaxDetermined.Increment();
+				}
+			}
+		}
+		else
+		{
+			if (!signed || (signed && !startOperand.IsNegative))
+			{
+				var start = startOperand.ConstantUnsigned64;
+
+				result.BitValue.NarrowMax(start);
+
+				trace?.Log($"{result} MaxValue = {start}");
+				MaxDetermined.Increment();
+			}
+
+			if (DetermineMinOut(incrementVariableOperand, incrementValueOperand, loop, out var min))
+			{
+				if (min >= 0 || !signed)
+				{
+					result.BitValue.NarrowMin((ulong)min);
+
+					trace?.Log($"{result} MinValue = {min}");
+					MinDetermined.Increment();
+				}
+			}
+		}
+	}
+
+	private enum SignedAnalysis
+	{ Unknown, Signed, NotSigned, Both };
+
+	private static SignedAnalysis DetermineSignUsage(Operand value)
+	{
+		var signAnalysis = SignedAnalysis.Unknown;
+
+		foreach (var b in value.Uses)
+		{
+			if (!(b.Instruction == IR.Branch32 || b.Instruction == IR.Branch64))
+				continue;
+
+			var condition = b.ConditionCode;
+			var signed = condition.IsSigned();
+
+			if (signAnalysis == SignedAnalysis.Unknown)
+			{
+				signAnalysis = signed ? SignedAnalysis.Signed : SignedAnalysis.NotSigned;
+			}
+			else
+			{
+				if (signed && signAnalysis == SignedAnalysis.Signed)
+					continue;
+
+				if (!signed && signAnalysis != SignedAnalysis.NotSigned)
+					continue;
+
+				return SignedAnalysis.Both;
+			}
+		}
+
+		return signAnalysis;
 	}
 
 	private static bool DetermineMaxOut(Operand incrementVariable, Operand incrementValue, Loop loop, out long max)
@@ -148,52 +238,54 @@ public sealed class LoopRangeTrackerStage : BaseMethodCompilerStage
 		bool determined = false;
 		max = long.MaxValue;
 
-		foreach (var b in incrementVariable.Uses)
+		foreach (var use in incrementVariable.Uses)
 		{
-			if (!(b.Instruction == IR.Branch32 || b.Instruction == IR.Branch64))
+			if (!(use.Instruction == IR.Branch32 || use.Instruction == IR.Branch64))
 				continue;
 
-			// only that are the header or backedge (if only one)
-			if (!(b.Block == loop.Header || (loop.Backedges.Count == 1 && loop.Backedges.Contains(b.Block))))
+			// only analysis the header or backedge (if only one)
+			if (!(use.Block == loop.Header || (loop.Backedges.Count == 1 && loop.Backedges.Contains(use.Block))))
 				continue;
 
-			var x = b.Operand1;
-			var y = b.Operand2;
-			var condition = b.ConditionCode;
-			var target = b.BranchTarget1;
-			var othertarget = target != b.Block.NextBlocks[0]
-				? b.Block.NextBlocks[0]
-				: b.Block.NextBlocks[1];
+			var x = use.Operand1;
+			var y = use.Operand2;
+			var condition = use.ConditionCode;
+			var target = use.BranchTarget1;
+			var othertarget = target != use.Block.NextBlocks[0]
+				? use.Block.NextBlocks[0]
+				: use.Block.NextBlocks[1];
 
-			// form: x (variable) </<=/== y (constant) -> where branch exits the loop
-
-			// change form - on condition
-			if (condition is not (ConditionCode.Less
-				or ConditionCode.LessOrEqual
-				or ConditionCode.UnsignedLess
-				or ConditionCode.UnsignedLessOrEqual
-				or ConditionCode.Equal))
-			{
-				(x, y, condition) = (y, x, condition.GetOpposite()); // swap
-			}
+			// form: x (variable) {>,>=,==} y (constant) -> where branch exits loop
 
 			// change form - on branch
-			if (!loop.LoopBlocks.Contains(target))
+			if (loop.LoopBlocks.Contains(target))
 			{
-				(condition, target, othertarget) = (condition.GetOpposite(), othertarget, target); // swap
+				(condition, target, othertarget) = (condition.GetOpposite(), othertarget, target);
 			}
 
 			// change form - on constant to right
-			if (!y.IsResolvedConstant && condition == ConditionCode.Equal)
+			if (!y.IsResolvedConstant && (condition == ConditionCode.Equal || condition == ConditionCode.NotEqual))
 			{
 				(x, y) = (y, x);
 			}
 
-			if (condition is not (ConditionCode.Less
-				or ConditionCode.LessOrEqual
-				or ConditionCode.UnsignedLess
-				or ConditionCode.UnsignedLessOrEqual
-				or ConditionCode.Equal))
+			// change form - on condition
+			if (!y.IsResolvedConstant)
+			{
+				(x, y, condition) = (y, x, condition.GetOpposite()); // swap
+			}
+
+			if (condition is not (
+				   ConditionCode.Greater
+				or ConditionCode.GreaterOrEqual
+				or ConditionCode.UnsignedGreater
+				or ConditionCode.UnsignedGreaterOrEqual))
+				continue;
+
+			if (condition == ConditionCode.NotEqual && !incrementValue.IsConstantOne)
+				continue;
+
+			if (condition == ConditionCode.Equal)
 				continue;
 
 			if (x != incrementVariable)
@@ -202,14 +294,96 @@ public sealed class LoopRangeTrackerStage : BaseMethodCompilerStage
 			if (!y.IsResolvedConstant)
 				continue;
 
-			if (!loop.LoopBlocks.Contains(target))
+			if (loop.LoopBlocks.Contains(target))
 				continue; // exits loop
 
-			var adj = condition == ConditionCode.LessOrEqual || condition == ConditionCode.Equal ? 1 : 0;
+			var adj = condition == ConditionCode.GreaterOrEqual
+				|| condition == ConditionCode.UnsignedGreaterOrEqual ? 0 : 1;
 
-			var branchmax = y.ConstantSigned64 + incrementValue.ConstantSigned64 - 1 + adj;
+			var value = incrementValue.IsInt32
+				? y.ConstantSigned32 + incrementValue.ConstantSigned32 + adj
+				: y.ConstantSigned64 + incrementValue.ConstantSigned64 + adj;
 
-			max = Math.Min(max, branchmax);
+			max = Math.Min(max, value);
+
+			determined = true;
+		}
+
+		return determined;
+	}
+
+	private static bool DetermineMinOut(Operand incrementVariable, Operand incrementValue, Loop loop, out long min)
+	{
+		bool determined = false;
+		min = long.MinValue;
+
+		foreach (var use in incrementVariable.Uses)
+		{
+			if (!(use.Instruction == IR.Branch32 || use.Instruction == IR.Branch64))
+				continue;
+
+			// only analysis the header or backedge (if only one)
+			if (!(use.Block == loop.Header || (loop.Backedges.Count == 1 && loop.Backedges.Contains(use.Block))))
+				continue;
+
+			var x = use.Operand1;
+			var y = use.Operand2;
+			var condition = use.ConditionCode;
+			var target = use.BranchTarget1;
+			var othertarget = target != use.Block.NextBlocks[0]
+				? use.Block.NextBlocks[0]
+				: use.Block.NextBlocks[1];
+
+			// form: x (variable) {<,<=,==} y (constant) -> where branch exits the loop
+
+			// change form - on branch
+			if (loop.LoopBlocks.Contains(target))
+			{
+				(condition, target, othertarget) = (condition.GetOpposite(), othertarget, target); // swap
+			}
+
+			// change form - on constant to right
+			if (!y.IsResolvedConstant && (condition == ConditionCode.Equal || condition == ConditionCode.NotEqual))
+			{
+				(x, y) = (y, x);
+			}
+
+			// change form - on condition
+			if (!y.IsResolvedConstant)
+			{
+				(x, y, condition) = (y, x, condition.GetOpposite()); // swap
+			}
+
+			if (condition is not (
+				   ConditionCode.Less
+				or ConditionCode.LessOrEqual
+				or ConditionCode.UnsignedLess
+				or ConditionCode.UnsignedLessOrEqual))
+				continue;
+
+			if (condition == ConditionCode.NotEqual && !incrementValue.IsConstantOne)
+				continue;
+
+			if (condition == ConditionCode.Equal)
+				continue;
+
+			if (x != incrementVariable)
+				continue;
+
+			if (!y.IsResolvedConstant)
+				continue;
+
+			if (loop.LoopBlocks.Contains(target))
+				continue; // exits loop
+
+			var adj = condition == ConditionCode.LessOrEqual
+				|| condition == ConditionCode.UnsignedLessOrEqual ? 1 : 0;
+
+			var value = incrementValue.IsInt32
+				? y.ConstantSigned32 + incrementValue.ConstantSigned32 + adj
+				: y.ConstantSigned64 + incrementValue.ConstantSigned64 + adj;
+
+			min = Math.Max(min, value);
 
 			determined = true;
 		}
