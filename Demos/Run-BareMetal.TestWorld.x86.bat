98aac75abf47bf64a9fe5fdc1c2e6c9e4e75dc3d
Phil <phil@thinkedge.com>
2024-05-02 21:29:16 -0700

    - WIP

--- Demos/Run-BareMetal.TestWorld.x86.bat
+++ Demos/Run-BareMetal.TestWorld.x86.bat
@@ -1,3 +1,3 @@
 cd %~dp0
 cd ..\bin
-Mosa.Tool.Launcher -autostart -o5 -output-asm -output-debug -output-hash Mosa.BareMetal.TestWorld.x86.dll
+Mosa.Tool.Launcher -autostart -oMax -output-asm -output-debug -output-hash Mosa.BareMetal.TestWorld.x86.dll
