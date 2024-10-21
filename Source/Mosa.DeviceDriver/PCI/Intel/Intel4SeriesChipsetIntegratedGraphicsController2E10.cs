a0c8b20c66064dbb65dee81f9c7758f3c85a6bb8
Phil <phil@thinkedge.com>
2024-10-20 21:12:30 -0700

    - Updated QEMU

--- Source/Mosa.DeviceDriver/PCI/Intel/Intel4SeriesChipsetIntegratedGraphicsController2E10.cs
+++ Source/Mosa.DeviceDriver/PCI/Intel/Intel4SeriesChipsetIntegratedGraphicsController2E10.cs
@@ -6,5 +6,5 @@ namespace Mosa.DeviceDriver.PCI.Intel;
 
 public class Intel4SeriesChipsetIntegratedGraphicsController2E10 : BaseDeviceDriver
 {
-	public override void Initialize() => Device.Name = "Intel4SeriesChipsetIntegratedGraphicsController";
+	public override void Initialize() => Device.Name = "Intel4SeriesChipsetIntegratedGraphicsController2E10";
 }
