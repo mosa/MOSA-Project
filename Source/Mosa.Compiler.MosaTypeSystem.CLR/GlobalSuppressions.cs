712417c578e8094126e3753683e74687c1dfa243
Phil <phil@thinkedge.com>
2026-02-15 18:46:34 -0800

    Suppress code analysis warnings and minor code cleanup
    Added <NoWarn> entries to multiple .csproj files to suppress specific compiler and code analysis warnings. Introduced GlobalSuppressions.cs files in Mosa.Compiler.MosaTypeSystem.CLR and Mosa.Tool.Debugger to suppress CA2213. Cleaned up ArrayTest.cs by removing an unused exception variable.

---
+++ Source/Mosa.Compiler.MosaTypeSystem.CLR/GlobalSuppressions.cs
@@ -0,0 +1,15 @@
+// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+//
+// To add a suppression to this file, right-click the message in the
+// Code Analysis results, point to "Suppress Message", and click
+// "In Suppression File".
+// You do not need to add suppressions to this file manually.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "CA2213:Disposable fields should be disposed", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "~N:Mosa.Compiler.MosaTypeSystem.CLR")]
