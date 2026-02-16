736504b0d9192bf7349678ec887a8c7a77a2c98a
Phil <phil@thinkedge.com>
2026-02-15 20:02:17 -0800

    Suppress CA2213 warning at project level, not assembly
    Removed CA2213 suppression from GlobalSuppressions.cs and added it to the NoWarn property in the project file for global suppression. This centralizes warning management in the project configuration.

--- Source/Mosa.Tool.Debugger/GlobalSuppressions.cs
+++ Source/Mosa.Tool.Debugger/GlobalSuppressions.cs
@@ -9,7 +9,3 @@
 // Code Analysis results, point to "Suppress Message", and click
 // "In Suppression File".
 // You do not need to add suppressions to this file manually.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Usage", "CA2213:Disposable fields should be disposed", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "~N:Mosa.Tool.Debugger")]
