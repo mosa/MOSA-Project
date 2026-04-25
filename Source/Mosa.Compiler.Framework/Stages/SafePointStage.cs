f324c138bb6faf3b0c634f08b3c4da1e8e2c4afc
Phil <phil@thinkedge.com>
2026-04-24 18:31:20 -0700

    Refactor safepoint insertion at loop backedges
    Insert safepoints by creating new blocks between backedges and loop headers, containing a SafePoint and jump. Track (backedge, header) pairs to avoid duplicates. Move insertion logic to a helper method for clarity and maintainability.

--- Source/Mosa.Compiler.Framework/Stages/SafePointStage.cs
+++ Source/Mosa.Compiler.Framework/Stages/SafePointStage.cs
@@ -67,22 +67,33 @@ public class SafePointStage : BaseMethodCompilerStage
 
 	private void InsertSafePointsAtBackedges(List<Loop> loops)
 	{
-		var visited = new HashSet<BasicBlock>();
+		var visited = new HashSet<(BasicBlock Backedge, BasicBlock Header)>();
 
 		foreach (var loop in loops)
 		{
 			foreach (var backedge in loop.Backedges)
 			{
-				if (!visited.Add(backedge))
+				if (!visited.Add((backedge, loop.Header)))
 					continue;
 
-				backedge.ContextBeforeBranch.InsertAfter().SetInstruction(IR.SafePoint);
-
-				SafePointsInsertedCount.Increment();
-
-				Trace?.Log($"SafePoint inserted at backedge: {backedge} -> {loop.Header}");
+				InsertSafePointAtBackedge(backedge, loop.Header);
 			}
 		}
+	}
+
+	private void InsertSafePointAtBackedge(BasicBlock backedge, BasicBlock header)
+	{
+		var context = CreateNewBlockContext(header.First.Label);
+
+		context.AppendInstruction(IR.SafePoint);
+		context.AppendInstruction(IR.Jmp, header);
+		context.Label = -1;
+
+		BasicBlocks.ReplaceBranchTargets(backedge, header, context.Block);
+
+		SafePointsInsertedCount.Increment();
+
+		Trace?.Log($"SafePoint inserted at backedge block: {backedge} -> {context.Block} -> {header}");
 	}
 
 	#endregion Insertion
